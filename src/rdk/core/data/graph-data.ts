78e0179fe6ca199063ce9b8f727ea4b5bd5d1d76
陈旭10045812 <chen.xu8@zte.com.cn>
2017-03-22 17:59:13 +0800

    完善GraphData

--- src/rdk/core/data/graph-data.ts
+++ src/rdk/core/data/graph-data.ts
@@ -1,36 +1,36 @@
-
 import {TableData} from "./table-data";
-
 
 type GraphMatrixRow = Array<string|number>;
 export type GraphDataHeader = string[];
+export type GraphDataField = string[];
 export type GraphDataRowDescriptor = string[];
 export type GraphDataMatrix = GraphMatrixRow[];
 
 export abstract class AbstractGraphData extends TableData {
-    protected abstract createChartOptions():any;
+    protected abstract createChartOptions(): any;
 
     constructor(public data: GraphDataMatrix = [],
-                public header:GraphDataHeader = [],
-                public rowDescriptor:GraphDataRowDescriptor = []) {
-        super(data, [], header);
-        this._makeFakeField();
+                public header: GraphDataHeader = [],
+                public rowDescriptor: GraphDataRowDescriptor = [],
+                public field: GraphDataField) {
+        super(data, field, header);
+        this._makeFields();
 
         if (!rowDescriptor) {
             throw new Error('invalid rowDescriptor data!');
         }
     }
 
-    protected chartOptions:any;
+    protected chartOptions: any;
 
-    public get options():any {
+    public get options(): any {
         if (!this.chartOptions) {
             this.chartOptions = this.createChartOptions();
         }
         return this.chartOptions;
     }
 
-    public fromObject(data: any):AbstractGraphData {
+    public fromObject(data: any): AbstractGraphData {
         if (!data.hasOwnProperty('data') || !(data.data instanceof Array)) {
             throw new Error('need a "data" property which type is Array!');
         }
@@ -39,18 +39,23 @@ export abstract class AbstractGraphData extends TableData {
         TableData.arrayAppend(this.data, data.data);
         if (data.hasOwnProperty('header')) {
             TableData.arrayAppend(this.header, data.header);
-            this._makeFakeField();
+        }
+        if (data.hasOwnProperty('field')) {
+            TableData.arrayAppend(this.field, data.field);
         }
         if (data.hasOwnProperty('rowDescriptor')) {
             TableData.arrayAppend(this.rowDescriptor, data.rowDescriptor);
         }
+        this._makeFields();
         this.refresh();
 
         return this;
     }
 
-    private _makeFakeField():void {
-        this.header.forEach(item => this.field.push(item));
+    private _makeFields(): void {
+        if (this.field.length == 0) {
+            this.header.forEach(item => this.field.push(item));
+        }
     }
 
     protected clearData(): void {
@@ -60,7 +65,85 @@ export abstract class AbstractGraphData extends TableData {
 
 }
 
-export class AbstractPieGraphData extends AbstractGraphData {
+export class GraphTitle {
+    //TODO: 补充完整
+    constructor(public text: string = '',
+                public subtext: string = '',
+                public left: string = 'auto',
+                public right: string = 'auto',
+                public top: string = 'auto',
+                public bottom: string = 'auto',
+                public show: boolean = true) {
+    }
+}
+
+export class GraphLegend {
+    //TODO: 补充完整
+    constructor(public data: Array<{name: string, icon?: any}>,
+                public left: string = 'auto',
+                public right: string = 'auto',
+                public top: string = 'auto',
+                public bottom: string = 'auto',
+                public show: boolean = true) {
+
+    }
+}
+
+export class GraphTooltip {
+    //TODO: 补充完整
+    constructor(public show: boolean = true) {
+    }
+}
+
+export abstract class AbstractNormalGraphData extends AbstractGraphData {
+    public title: GraphTitle;
+    public legend: GraphLegend;
+    public tooltip: GraphTooltip;
+}
+
+export class OutlineMapData extends AbstractNormalGraphData {
+    protected createChartOptions(): any {
+        return undefined;
+    }
+}
+
+export class PieGraphData extends AbstractNormalGraphData {
+    protected createChartOptions(): any {
+        return undefined;
+    }
+}
+
+export class DonutGraphData extends PieGraphData {
+    protected createChartOptions(): any {
+        return undefined;
+    }
+}
+
+export class LineBarGraphData extends AbstractNormalGraphData {
+    protected createChartOptions(): any {
+        return undefined;
+    }
+}
+
+export class GaugeGraphData extends AbstractNormalGraphData {
+    protected createChartOptions(): any {
+        return undefined;
+    }
+}
+
+export class ScatterGraphData extends AbstractNormalGraphData {
+    protected createChartOptions(): any {
+        return undefined;
+    }
+}
+
+export class RadarGraphData extends AbstractNormalGraphData {
+    protected createChartOptions(): any {
+        return undefined;
+    }
+}
+
+export class HeatGraphData extends AbstractNormalGraphData {
     protected createChartOptions(): any {
         return undefined;
     }
