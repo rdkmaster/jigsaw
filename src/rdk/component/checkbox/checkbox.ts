b59ef76ba4d59c8f991c82e70f45dd08f96c5a8c
王鹏飞10177553 <wang.pengfei50@zte.com.cn>
2017-03-23 09:28:37 +0800

    # checkbox 1. 添加宽度设置: 2. 样式部分重构.(去掉一些写死的对其.)

--- src/rdk/component/checkbox/checkbox.ts
+++ src/rdk/component/checkbox/checkbox.ts
@@ -5,6 +5,7 @@
 import {
     Component, Input, EventEmitter, Output, OnInit
 } from '@angular/core';
+import {AbstractRDKComponent} from "../../core/api/component-api";
 
 export class RdkCheckboxChange {
     /** The source RdkCheckBox of the event. */
@@ -20,45 +21,52 @@ export enum CheckBoxStatus {
 @Component({
     selector: 'rdk-checkbox',
     templateUrl: './checkbox.html',
-    styleUrls: ['./checkbox.scss']
+    styleUrls: ['./checkbox.scss'],
+    host: {
+        '[style.width]': 'width',
+    }
 })
 
 /**
  * checkbox 组件
  */
-export class RdkCheckBox implements OnInit{
+export class RdkCheckBox extends AbstractRDKComponent implements OnInit{
     private _checked: CheckBoxStatus | boolean = CheckBoxStatus.unchecked;
     private _disabled: boolean = false;
-    private checkboxClass: { };
+    private _checkboxClass: { };
 
     @Input()
-    get checked(): boolean | CheckBoxStatus { return this._checked };
-    set checked(value: boolean | CheckBoxStatus) {
+    public get checked(): boolean | CheckBoxStatus { return this._checked };
+    public set checked(value: boolean | CheckBoxStatus) {
         this._checked = value;
-        this.setCheckBoxClass();
+        this._setCheckBoxClass();
     };
-    @Output() checkedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() public checkedChange: EventEmitter<boolean| CheckBoxStatus> = new EventEmitter<boolean>();
 
     /**
      * 设置checkbox 状态
      * @param value boolean 或者 CheckBoxStatus
      */
-    setCheckboxStatus(value: boolean| CheckBoxStatus) {
+    public setCheckboxStatus(value: boolean| CheckBoxStatus) {
         this._checked = value;
-        this.setCheckBoxClass();
+        this._setCheckBoxClass();
     }
 
     @Input()
-    get disabled(): boolean { return this._disabled; };
-    set disabled(value: boolean) {
+    public get disabled(): boolean { return this._disabled; };
+    public set disabled(value: boolean) {
         this._disabled = value;
-        this.setCheckBoxClass();
+        this._setCheckBoxClass();
     };
 
     /** Event emitted when the checkbox's `checked` value changes. */
-    @Output() change: EventEmitter<RdkCheckboxChange> = new EventEmitter<RdkCheckboxChange>();
+    @Output() public change = new EventEmitter<RdkCheckboxChange>();
 
-    toggle(): void {
+    public ngOnInit() {
+        this._setCheckBoxClass();
+    }
+
+    private _toggle(): void {
         this.checked = !this.checked;
         this._emitChangeEvent();
     }
@@ -69,6 +77,7 @@ export class RdkCheckBox implements OnInit{
         event.source = this;
         event.checked = this.checked;
         this.change.emit(event);
+        this.checkedChange.emit(this.checked);
     }
 
     /**
@@ -76,29 +85,25 @@ export class RdkCheckBox implements OnInit{
      * @param event
      * @private
      */
-    _onCheckboxClick(event: Event) {
+    private _onCheckboxClick(event: Event) {
         event.stopPropagation();
         event.preventDefault();
 
         if(!this.disabled) {
-            this.toggle();
-            this.setCheckBoxClass();
+            this._toggle();
+            this._setCheckBoxClass();
         }
     }
 
     /**
      * 更新checkbox的样式信息
      */
-    setCheckBoxClass() {
-        this.checkboxClass = {
+    private _setCheckBoxClass() {
+        this._checkboxClass = {
             'rdk-checkbox': 'true',
             'rdk-checkbox-checked': this.checked,
             'rdk-checkbox-indeterminate': this.checked === CheckBoxStatus.indeterminate,
             'rdk-checkbox-disabled': this.disabled
         }
-    }
-
-    ngOnInit() {
-        this.setCheckBoxClass();
     }
 }
