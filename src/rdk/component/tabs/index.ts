d074c008627af7209ee8d653f4ab5eae3aae7933
王鹏飞10177553 <wang.pengfei50@zte.com.cn>
2017-03-29 10:21:58 +0800

    # tabs 页组件, 添加

---
+++ src/rdk/component/tabs/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by 10177553 on 2017/3/29.
+ */
+
+import { NgModule } from '@angular/core';
+
+import { RdkTabs } from './tabs';
+
+@NgModule({
+    imports: [],
+    exports: [RdkTabs],
+    declarations: [RdkTabs],
+    providers: [],
+})
+export class RdkTabsModule { }
---
+++ src/rdk/component/tabs/tabs.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by 10177553 on 2017/3/29.
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'rdk-tabs',
+    templateUrl: 'tabs.html',
+    styleUrls: ['./tabs.scss']
+})
+export class RdkTabs implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
