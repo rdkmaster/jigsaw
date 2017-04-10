4c67c131c7e945288e8a328ef1992a5ff58d8af4
王鹏飞10177553 <wang.pengfei50@zte.com.cn>
2017-04-10 08:37:41 +0800

    #tab 页新添加接口,及部分接口变法实现

---
+++ src/rdk/doc/demo/component/tabs/tabApi/dynamicTab.ts
@@ -0,0 +1,72 @@
+/**
+ * Created by 10177553 on 2017/4/5.
+ */
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {RdkTabs} from "../../../../../component/tabs/tabs";
+
+@Component({
+    templateUrl: './dynamicTab.html',
+    styles:[ `
+        .container {
+            border: 1px solid #e9e9e9;
+            border-radius: 4px;
+            display: inline-block;
+            width: 100%;
+            position: relative;
+            margin: 0 0 16px;
+            -webkit-transition: all .2s;
+            transition: all .2s;
+        }
+        .tabBar {
+            margin: 10px;
+            background-color: #bbbbbb;
+        }
+    `]
+})
+export class dynamicTabDemoComponent implements OnInit {
+    @ViewChild(RdkTabs) tabs: RdkTabs;
+
+    activeIndex: number = 0;
+
+    constructor() { }
+
+    dynamicTabs = [
+        {
+            label: 'Tab 1',
+            content: 'This is the _body of the first tab'
+        }, {
+            label: 'Tab 2',
+            disabled: true,
+            content: 'This is the _body of the second tab'
+        },{
+            label: 'Tab 3',
+            disabled: false,
+            content: '<input type="text" value="123"/>'
+        }
+    ]
+
+    public deleteTab(value) {
+        this.tabs.destroyTabPane(0);
+    }
+    public deleteTabSec() {
+        this.dynamicTabs.splice(0,1);
+        this.activeIndex = this.dynamicTabs.length-1;
+    }
+    public hideTab():void {
+        this.tabs.hideTab(0);
+    }
+    public showTab():void {
+        this.tabs.showTab(0);
+    }
+
+    public addTabPane() {
+        let length = this.dynamicTabs.length-1;
+
+        this.dynamicTabs.push({
+            label: 'new Tab'+length,
+            content: 'new Tabs contents ' +length
+        })
+        this.activeIndex = this.dynamicTabs.length-1;
+    }
+    ngOnInit() { }
+}
