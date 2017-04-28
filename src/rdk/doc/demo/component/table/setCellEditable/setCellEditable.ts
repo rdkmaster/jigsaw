c65a362e1daedc2745f86c566b816c7c71757ca9
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-04-28 11:31:10 +0800

    修改editor时td宽度变化的问题

--- src/rdk/doc/demo/component/table/setCellEditable/setCellEditable.ts
+++ src/rdk/doc/demo/component/table/setCellEditable/setCellEditable.ts
@@ -17,7 +17,7 @@ export class MyTableCell extends TableCellRenderer {
  * 编辑单元格渲染器
  * */
 @Component({
-    template: `<rdk-input #input [(value)]="cellData" width="100%" [clearable]="false" (blur)="dispatchChangeEvent(cellData)"></rdk-input>`
+    template: `<rdk-input #input [(value)]="cellData" [clearable]="false" (blur)="dispatchChangeEvent(cellData)"></rdk-input>`
 })
 export class MyTableCellEditor extends TableCellRenderer implements AfterViewInit{
 
@@ -28,8 +28,6 @@ export class MyTableCellEditor extends TableCellRenderer implements AfterViewIni
     }
 
 }
-
-
 
 @Component({
   templateUrl: 'setCellEditable.html',
@@ -102,11 +100,10 @@ export class TableSetCellEditableDemoComponent {
 
     }
 
-
-
     private _columns: ColumnDefine[] = [
         {
             target: 'position',
+            width: '20%',
             group : true,
             cell :{
                 renderer: MyTableCell,
