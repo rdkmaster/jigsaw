0f8099d2017f1268bd4986a7c9d87e28484906ad
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-05-10 16:00:06 +0800

    解决loading闪现的问题

--- src/rdk/component/loading/loading.ts
+++ src/rdk/component/loading/loading.ts
@@ -7,20 +7,22 @@ import {IPopupable, PopupDisposer, PopupOptions, PopupService} from "rdk/service
     templateUrl: 'loading.html',
     styleUrls: ['loading.scss']
 })
-export class RdkLoading implements IPopupable, OnInit{
+export class RdkLoading implements IPopupable, OnInit {
     public disposer: PopupDisposer;
     public initData: any;
     public options: PopupOptions;
-    private _popupElement: HTMLElement;
-    constructor(private _renderer: Renderer2, private _elementRef: ElementRef){
+    private state: string = 'void';
 
+    constructor(private _renderer: Renderer2, private _elementRef: ElementRef) {
     }
 
-    ngOnInit(){
-        this._popupElement = this._elementRef.nativeElement;
-        if(this.options){
-            PopupService.setPopup(this.options, this._popupElement, this._renderer);
-        }
+    ngOnInit() {
+        setTimeout(() => {
+            if (this.options) {
+                PopupService.setPopup(this.options, this._elementRef.nativeElement, this._renderer);
+            }
+            this.state = 'in';
+        }, 0)
     }
 }
 
