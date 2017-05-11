0fcb6399e001631ec6ca2bce6e5888319d40b2cc
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-05-11 15:29:39 +0800

    增加barLoadingDemo

---
+++ src/rdk/doc/demo/component/loading/barLoading/loading.ts
@@ -0,0 +1,71 @@
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {PopupDisposer} from "../../../../../service/popup.service";
+import {LoadingService} from "rdk/service/loading.service";
+import {RdkBarLoading} from "../../../../../component/loading/loading";
+
+@Component({
+    templateUrl: 'loading.html',
+    styleUrls: ['loading.scss']
+})
+export class LoadingDemoComponent implements OnInit{
+    @ViewChild('block') block: ElementRef;
+
+    constructor(public loadingService: LoadingService, public renderer: Renderer2) {
+    }
+
+    disposeBlockLoading: PopupDisposer;
+    disposeGlobalLoading: PopupDisposer;
+
+    popupBlockLoading() {
+        if (!this.disposeBlockLoading) {
+            this.disposeBlockLoading = this.loadingService.show(this.block);
+        }
+    }
+
+    popupBlockBarLoading() {
+        if (!this.disposeBlockLoading) {
+            this.disposeBlockLoading = this.loadingService.show(this.block, RdkBarLoading);
+        }
+    }
+
+    closeBlockLoading() {
+        if (this.disposeBlockLoading) {
+            this.disposeBlockLoading();
+            this.disposeBlockLoading = null;
+        }
+    }
+
+    popupGlobalLoading() {
+        if (!this.disposeGlobalLoading) {
+            this.disposeGlobalLoading = this.loadingService.show();
+            setTimeout(() => {
+                this.closeGlobalLoading();
+            }, 3000)
+        }
+    }
+
+    popupGlobalBarLoading() {
+        if (!this.disposeGlobalLoading) {
+            this.disposeGlobalLoading = this.loadingService.show(RdkBarLoading);
+            setTimeout(() => {
+                this.closeGlobalLoading();
+            }, 3000)
+        }
+    }
+
+    closeGlobalLoading(){
+        if(this.disposeGlobalLoading){
+            this.disposeGlobalLoading();
+            this.disposeGlobalLoading = null;
+        }
+    }
+
+    ngOnInit(){
+        //window.history.back的监听
+        this.renderer.listen('window', 'popstate', () => {
+            this.closeBlockLoading();
+            this.closeGlobalLoading();
+        })
+    }
+
+}
