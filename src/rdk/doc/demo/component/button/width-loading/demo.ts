91f433ad92fb038e6a03089902bc45c7922ecdcf
陈旭10045812 <chen.xu8@zte.com.cn>
2017-05-23 18:28:23 +0800

    优化dmeo

--- src/rdk/doc/demo/component/button/width-loading/demo.ts
+++ src/rdk/doc/demo/component/button/width-loading/demo.ts
@@ -3,8 +3,8 @@ import {Component} from "@angular/core";
 
 @Component({
     template: `
-        <rdk-button (click)="onClick()" colortype="primary" width="120px">
-            <rdk-loading *ngIf="isLoading" width="14px" height="14px" color="blue"></rdk-loading>
+        <rdk-button (click)="onClick()" colortype="primary" width="120px" height="32px">
+            <rdk-loading *ngIf="isLoading" width="16px" height="16px" color="blue"></rdk-loading>
             <span style="padding-left: 5px">{{label}}</span>
         </rdk-button>
     `
