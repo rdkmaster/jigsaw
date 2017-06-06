8b1fa39850c3490f39bf988de14c7e4bffe9046b
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-06-06 19:24:57 +0800

    select-ArrayCollection

--- src/rdk/doc/demo/component/select/basic/basic.ts
+++ src/rdk/doc/demo/component/select/basic/basic.ts
@@ -1,18 +1,19 @@
 import {Component} from "@angular/core";
+import {ArrayCollection} from "../../../../../core/data/array-collection";
 
 @Component({
   templateUrl: 'basic.html',
   styleUrls: ['basic.css']
 })
 export class SelectBasicDemoComponent {
-    cityListForSelect = [
-    {label: "北京"},
-    {label: "上海"},
-    {label: "南京"},
-    {label: "深圳"},
-    {label: "长沙"},
-    {label: "西安"}
-    ];
+    cityListForSelect = new ArrayCollection([
+        {label: "北京"},
+        {label: "上海"},
+        {label: "南京"},
+        {label: "深圳"},
+        {label: "长沙"},
+        {label: "西安"}
+    ]);
     public selectChange(message:any){
         console.log("select city is:"+ message.label);
     }
--- src/rdk/doc/demo/component/select/scroll/scroll.ts
+++ src/rdk/doc/demo/component/select/scroll/scroll.ts
@@ -1,18 +1,19 @@
 import {Component} from "@angular/core";
+import {ArrayCollection} from "../../../../../core/data/array-collection";
 
 @Component({
   templateUrl: 'scroll.html',
   styleUrls: ['scroll.css']
 })
 export class SelectScrollDemoComponent {
-    cityListForSelect = [
-    {label: "北京"},
-    {label: "上海"},
-    {label: "南京"},
-    {label: "深圳"},
-    {label: "长沙"},
-    {label: "西安"}
-    ];
+    cityListForSelect = new ArrayCollection([
+        {label: "北京"},
+        {label: "上海"},
+        {label: "南京"},
+        {label: "深圳"},
+        {label: "长沙"},
+        {label: "西安"}
+    ]);
     public selectChange(message:any){
         console.log("select city is:"+ message.label);
     }
