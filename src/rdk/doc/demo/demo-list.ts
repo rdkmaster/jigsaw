4a881812bac098fa1c0a99f2299530d673be3f84
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-06-19 18:39:08 +0800

    demo-layout

--- src/rdk/doc/demo/demo-list.ts
+++ src/rdk/doc/demo/demo-list.ts
@@ -1,10 +1,12 @@
 import {NgModule, Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {RdkBlock, RdkBlockModule} from "../../component/block/block";
+import {RdkCollapseModule} from "../../component/collapse/collapse";
 
 @Component({
-    selector: 'rdk-demo-list', templateUrl: 'demo-list.html',
-    styles: ['li {display: inline-block; margin-right: 12px;}']
+    selector: 'rdk-demo-list',
+    templateUrl: 'demo-list.html',
+    styleUrls: ['demo-list.scss']
 })
 export class DemoListComponent {
 
@@ -13,106 +15,109 @@ export class DemoListComponent {
 // 请按照组件的字符序排列
 const demoListRoutes = [
     {
-        path: '', component: DemoListComponent
-    },
-    {
-        path: 'alert',
-        loadChildren: './component/alert/demo.module#AlertDemoModule'
-    },
-    {
-        path: 'array-collection',
-        loadChildren: './component/array-collection/demo.module#ArrayCollectionDemoModule'
-    },
-    {
-        path: 'button',
-        loadChildren: './component/button/demo.module#ButtonDemoModule'
-    },
-    {
-        path: 'checkbox',
-        loadChildren: './component/checkbox/demo.module#CheckBoxDemoModule'
-    },
-    {
-        path: 'loading',
-        loadChildren: './component/loading/demo.module#LoadingDemoModule'
-    },
-    {
-        path: 'input',
-        loadChildren: './component/input/demo.module#InputDemoModule'
-    },
-    {
-        path: 'popup',
-        loadChildren: './component/popup/demo.module#PopupDemoModule'
-    },
-    {
-        path: 'scrollbar',
-        loadChildren: './component/scrollbar/demo.module#ScrollbarDemoModule'
-    },
-    {
-        path: 'select',
-        loadChildren: './component/select/demo.module#SelectDemoModule'
-    },
-    {
-        path: 'table',
-        loadChildren: './component/table/demo.module#TableDemoModule'
-    },
-    {
-        path: 'switch',
-        loadChildren: './component/switch/switch-demo.module#SwitchDemoModule'
-    },
-    {
-        path: 'time',
-        loadChildren: './component/time/time-demo.module#TimeDemoModule'
-    },
-    {
-        path: 'radio',
-        loadChildren: './component/radio/radio-demo.module#RadioDemoModule'
-    },
-    {
-        path: 'tileselect',
-        loadChildren: './component/tileselect/tileselect-demo.module#TileSelectDemoModule'
-    },
-    {
-        path: 'graph',
-        loadChildren: './component/graph/graph-demo.module#GraphDemoModule'
-    },
-    {
-        path: 'pagination',
-        loadChildren: './component/pagination/pagination-demo.module#PaginationDemoModule'
-    },
-    {
-        path: 'tag',
-        loadChildren: './component/tag/tag-demo.module#TagDemoModule'
-    },
-    {
-        path: 'tabs',
-        loadChildren: './component/tabs/tabs-demo.module#TabsDemoModule'
-    },
-    {
-        path: 'combo-select',
-        loadChildren: './component/combo-select/combo-select-demo.module#ComboSelectDemoModule'
-    },
-    {
-        path: 'slider',
-        loadChildren: './component/slider/slider-demo.module#SliderDemoModule'
-    },
-    {
-        path:'tree',
-        loadChildren:'./component/ztree/demo.module#ZtreeDemoModule'
-    },
-    {
-        path: 'collapse',
-        loadChildren: './component/collapse/collapse-module#CollapseDemoModule'
-    },
-    {
-        path: 'dialog',
-        loadChildren: './component/dialog/demo.module#DialogDemoModule'
-    },{
-        path: 'rangeTime',
-        loadChildren: './component/range-time/range-time-demo.module#RangeTimeDemoModule'
-    },
-    {
-        path: 'tooltip',
-        loadChildren: './component/tooltip/demo.module#TooltipDemoModule'
+        path: '',
+        component: DemoListComponent,
+        children: [
+            {
+                path: 'alert',
+                loadChildren: './component/alert/demo.module#AlertDemoModule'
+            },
+            {
+                path: 'array-collection',
+                loadChildren: './component/array-collection/demo.module#ArrayCollectionDemoModule'
+            },
+            {
+                path: 'button',
+                loadChildren: './component/button/demo.module#ButtonDemoModule'
+            },
+            {
+                path: 'checkbox',
+                loadChildren: './component/checkbox/demo.module#CheckBoxDemoModule'
+            },
+            {
+                path: 'loading',
+                loadChildren: './component/loading/demo.module#LoadingDemoModule'
+            },
+            {
+                path: 'input',
+                loadChildren: './component/input/demo.module#InputDemoModule'
+            },
+            {
+                path: 'popup',
+                loadChildren: './component/popup/demo.module#PopupDemoModule'
+            },
+            {
+                path: 'scrollbar',
+                loadChildren: './component/scrollbar/demo.module#ScrollbarDemoModule'
+            },
+            {
+                path: 'select',
+                loadChildren: './component/select/demo.module#SelectDemoModule'
+            },
+            {
+                path: 'table',
+                loadChildren: './component/table/demo.module#TableDemoModule'
+            },
+            {
+                path: 'switch',
+                loadChildren: './component/switch/switch-demo.module#SwitchDemoModule'
+            },
+            {
+                path: 'time',
+                loadChildren: './component/time/time-demo.module#TimeDemoModule'
+            },
+            {
+                path: 'radio',
+                loadChildren: './component/radio/radio-demo.module#RadioDemoModule'
+            },
+            {
+                path: 'tileselect',
+                loadChildren: './component/tileselect/tileselect-demo.module#TileSelectDemoModule'
+            },
+            {
+                path: 'graph',
+                loadChildren: './component/graph/graph-demo.module#GraphDemoModule'
+            },
+            {
+                path: 'pagination',
+                loadChildren: './component/pagination/pagination-demo.module#PaginationDemoModule'
+            },
+            {
+                path: 'tag',
+                loadChildren: './component/tag/tag-demo.module#TagDemoModule'
+            },
+            {
+                path: 'tabs',
+                loadChildren: './component/tabs/tabs-demo.module#TabsDemoModule'
+            },
+            {
+                path: 'combo-select',
+                loadChildren: './component/combo-select/combo-select-demo.module#ComboSelectDemoModule'
+            },
+            {
+                path: 'slider',
+                loadChildren: './component/slider/slider-demo.module#SliderDemoModule'
+            },
+            {
+                path:'tree',
+                loadChildren:'./component/ztree/demo.module#ZtreeDemoModule'
+            },
+            {
+                path: 'collapse',
+                loadChildren: './component/collapse/collapse-module#CollapseDemoModule'
+            },
+            {
+                path: 'dialog',
+                loadChildren: './component/dialog/demo.module#DialogDemoModule'
+            },{
+                path: 'rangeTime',
+                loadChildren: './component/range-time/range-time-demo.module#RangeTimeDemoModule'
+            },
+            {
+                path: 'tooltip',
+                loadChildren: './component/tooltip/demo.module#TooltipDemoModule'
+            },
+        ]
     },
     {
         path: '**', //fallback router must in the last
@@ -123,7 +128,8 @@ const demoListRoutes = [
 @NgModule({
     imports: [
         RouterModule.forChild(demoListRoutes),
-        RdkBlockModule
+        RdkBlockModule,
+        RdkCollapseModule
     ],
     exports: [],
     declarations: [
@@ -134,7 +140,7 @@ const demoListRoutes = [
 })
 export class DemoListModule {
     constructor(){
-        Event
+
     }
 
 
