bcd53e5ff71a39dedf466957b028311ff4d2937e
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-06-21 14:46:02 +0800

    add-phantomjs-plugin

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
         frameworks: ['jasmine', '@angular/cli'],
         plugins: [
             require('karma-jasmine'),
-            require('karma-chrome-launcher'),
+            //require('karma-chrome-launcher'),
+            require('karma-phantomjs-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
             require('@angular/cli/plugins/karma')
@@ -38,7 +39,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: [/*'Chrome', */'PhantomJS'],
         singleRun: false
     });
 };
