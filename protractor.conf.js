b0f5a4f5ed3704bf9d7348f884876f17fc8a6541
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-06-21 17:37:33 +0800

    protractor-add-traivs-chrome

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,30 +1,42 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {SpecReporter} = require('jasmine-spec-reporter');
+
+var BROWSER_OPTIONS = {
+    LocalChrome: {
+        'browserName': 'chrome',
+    },
+    ChromeOnTravis: {
+        browserName: 'chrome',
+        chromeOptions: {
+            'args': ['--no-sandbox'],
+            'binary': process.env.CHROME_BIN,
+        }
+    }
+};
 
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  beforeLaunch: function() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-  },
-  onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+    allScriptsTimeout: 11000,
+    specs: [
+        './e2e/**/*.e2e-spec.ts'
+    ],
+    capabilities: process.env.TRAVIS ? BROWSER_OPTIONS.ChromeOnTravis : BROWSER_OPTIONS.LocalChrome,
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function () {
+        }
+    },
+    beforeLaunch: function () {
+        require('ts-node').register({
+            project: 'e2e/tsconfig.e2e.json'
+        });
+    },
+    onPrepare() {
+        jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    }
 };
