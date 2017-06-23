0c5ceeac11e1a1794c3505d536c6cbb195efcc51
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-06-23 14:23:08 +0800

    traivs-saucelabs-firefox

--- protractor.conf.js
+++ protractor.conf.js
@@ -33,11 +33,18 @@ let config = {
 if (process.env['TRAVIS']) {
     config.sauceUser = process.env['SAUCE_USERNAME'];
     config.sauceKey = process.env['SAUCE_ACCESS_KEY'];
-    config.capabilities = {
-        'browserName': 'chrome',
-        'tunnel-identifier': process.env['TRAVIS_JOB_NUMBER'],
-        'build': process.env['TRAVIS_BUILD_NUMBER']
-    };
+    config.multiCapabilities = [
+        {
+            'browserName': 'chrome',
+            'tunnel-identifier': process.env['TRAVIS_JOB_NUMBER'],
+            'build': process.env['TRAVIS_BUILD_NUMBER']
+        },
+        {
+            browserName: 'firefox',
+            'tunnel-identifier': process.env['TRAVIS_JOB_NUMBER'],
+            'build': process.env['TRAVIS_BUILD_NUMBER']
+        }
+    ];
 }
 
 exports.config = config;
