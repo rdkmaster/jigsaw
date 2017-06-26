f39158edaff87876e515966b258eb6a7d986d543
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-06-26 14:47:40 +0800

    button-e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,8 @@
-import {Ng2RdkPage} from './app.po';
+import {browser} from 'protractor';
 
 describe('Jigsaw App', () => {
-    let page: Ng2RdkPage;
-
-    beforeEach(() => {
-        page = new Ng2RdkPage();
-    });
-
-    it('should display message saying Jigsaw', () => {
-        page.navigateTo();
-        expect(page.getParagraphText()).toEqual('Jigsaw');
+    browser.get('/');
+    it('should have a title', () => {
+        expect(browser.getTitle()).toBe('Jigsaw');
     });
 });
--- e2e/components/button.e2e-spec.ts
+++ e2e/components/button.e2e-spec.ts
@@ -1,8 +1,11 @@
-import {browser, by, element, ExpectedConditions} from 'protractor';
+import {browser, by, element} from 'protractor';
 
 describe('button', () => {
     describe('button render', () => {
-        beforeEach(() => browser.get('/demo/button'));
+        beforeEach(() => {
+            browser.waitForAngularEnabled(true);
+            browser.get('/demo/button');
+        });
 
         it('should render button', async () => {
             expect(element(by.css('app-root .demo-wrap .right-box rdk-button')).getText()).toEqual('click me');
