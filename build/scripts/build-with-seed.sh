f9aa710dcb14ac334576c4a0234ef2ebfaf8feaf
陈旭10045812 <chen.xu8@zte.com.cn>
2017-06-26 14:33:04 +0800

    ..

--- build/scripts/build-with-seed.sh
+++ build/scripts/build-with-seed.sh
@@ -1,11 +1,25 @@
+#!/bin/bash
+home=$(cd "$(dirname "$0")"; pwd)
+workDir="/tmp/jigsaw-seed"
 
-cd src/app
-for file in `find $PWD | xargs ls -d`
-do
-	if [ -d $file ]; then
-		continue
-	fi
-	sed -i 's/from\s\+"\(\.\.\/\)\*rdk/from "@rdkmaster\/jigsaw\/typings/g' $file
-	sed -i "s/from\s\+'\(\.\.\/\)\*rdk/from '@rdkmaster\/jigsaw\/typings/g" $file
-done
+echo '##############################'
+echo $home
+echo '##############################'
+
+rm -fr $workDir
+git clone https://github.com/rdkmaster/jigsaw-seed.git $workDir
+cd $workDir
+npm install
+
+
+
+#cd src/app
+#for file in `find $PWD | xargs ls -d`
+#do
+#	if [ -d $file ]; then
+#		continue
+#	fi
+#	sed -i 's/from\s\+"\(\.\.\/\)\*rdk/from "@rdkmaster\/jigsaw\/typings/g' $file
+#	sed -i "s/from\s\+'\(\.\.\/\)\*rdk/from '@rdkmaster\/jigsaw\/typings/g" $file
+#done
 
