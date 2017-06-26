e12b1f23493ad9a72433ce1626003e69d5a9dff0
陈旭10045812 <chen.xu8@zte.com.cn>
2017-06-26 18:30:56 +0800

    ..

---
+++ build/scripts/integrate-with-seed.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+home=`pwd`
+
+echo '##############################'
+echo home=$home
+echo SEED_DIR=$SEED_DIR
+echo '##############################'
+
+mkdir -p $SEED_DIR
+git clone https://github.com/rdkmaster/jigsaw-seed.git $SEED_DIR
+cd $SEED_DIR
+npm install
+
+#update rdk npm package
+rm -fr node_modules/@rdkmaster/jigsaw
+cp $home/dist/releases/jigsaw node_modules/@rdkmaster/
+
+#copy demo source and e2e source.
+rm -fr ./e2e
+cp $home/e2e ./
+
+rm -fr src/app src/assets src/styles.scss
+cp $home/src/app $home/src/assets $home/src/styles.scss src/
+
+cd src/app
+for file in `find $PWD | xargs ls -d`
+do
+	if [ -d $file ]; then
+		continue
+	fi
+	sed -i 's/from\s\+"\(\.\.\/\)\*rdk/from "@rdkmaster\/jigsaw\/typings/g' $file
+	sed -i "s/from\s\+'\(\.\.\/\)\*rdk/from '@rdkmaster\/jigsaw\/typings/g" $file
+done
+
+cd $home
