233f2d88a7edd11f22cdc379d64e759b7444bb63
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-06-26 15:09:32 +0800

    button-e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import {browser} from 'protractor';
 
 describe('Jigsaw App', () => {
+    browser.waitForAngularEnabled(false);
     browser.get('/');
     it('should have a title', () => {
         expect(browser.getTitle()).toBe('Jigsaw');
