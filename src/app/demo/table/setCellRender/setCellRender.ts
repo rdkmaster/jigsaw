fcffb5477a4df455bd1656f6953cefd9d51ea17e
陈旭10045812 <chen.xu8@zte.com.cn>
2017-07-06 20:19:31 +0800

    修改demo错误

--- src/app/demo/table/setCellRender/setCellRender.ts
+++ src/app/demo/table/setCellRender/setCellRender.ts
@@ -1,7 +1,7 @@
-import {AfterViewInit, Component, TemplateRef, ViewChild} from "@angular/core";
+import {AfterContentInit, Component, TemplateRef, ViewChild} from "@angular/core";
 import {Http} from "@angular/http";
 import {TableData} from "jigsaw/core/data/table-data";
-import {ColumnDefine, TableCellRenderer} from "jigsaw/component/table/table-api";
+import {ColumnDefine} from "jigsaw/component/table/table-api";
 /*
  * 自定义表头渲染组件
  * */
