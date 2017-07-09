f323df6b90a7008a1de4dee3d861aa87c2089af3
陈旭 <chen.xu8@zte.com.cn>
2017-07-09 20:32:53 +0800

    将demo目录改名为e2e-testee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,11 +18,11 @@ export function HttpLoaderFactory(http: Http) {
 const appRoutes = [
     {
         path: '',
-        loadChildren: 'app/demo/demo-list#DemoListModule'
+        loadChildren: 'app/e2e-testee/demo-list#DemoListModule'
     },
     {
         path: '**',//fallback router must in the last
-        loadChildren: 'app/demo/demo-list#DemoListModule'
+        loadChildren: 'app/e2e-testee/demo-list#DemoListModule'
     }
 ];
 
