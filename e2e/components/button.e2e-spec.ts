203fd538e7a446d801f97a0f5c1be745017d2643
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-07-12 11:50:49 +0800

    button e2e

--- e2e/components/button.e2e-spec.ts
+++ e2e/components/button.e2e-spec.ts
@@ -1,35 +1,76 @@
-import {browser, by, element} from 'protractor';
+import {browser, by, element, ExpectedConditions} from 'protractor';
 
 describe('button', () => {
-    describe('button render and set disabled', () => {
+    describe('test disabled', () => {
         beforeEach(() => {
-            browser.waitForAngularEnabled(false);
             browser.get('/button/disable');
         });
 
-        it('should render a button', async () => {
-            expect(element(by.id('button')).getText()).toEqual('click me');
-            const size = await element(by.id('button')).getSize();
-            expect(size.width).toEqual(80);
-            expect(size.height).toEqual(28);
-        });
+        it('should click invalid when set disabled', () => {
+            const testButtonEl = element(by.id('test-button')),
+                toggleDisabledEl = element(by.id('toggle-disabled')),
+                clickCounterEl = element(by.id('click-counter'));
 
-        it('should the button click invalid when set disabled', () => {
-            const clickCountSpan = element(by.id('clickCount'));
+            expect(clickCounterEl.getText()).toEqual('0');
 
-            expect(clickCountSpan.getText()).toEqual('0');
-
-            element(by.id('button')).click();
-            expect(clickCountSpan.getText()).toEqual('1');
+            testButtonEl.click();
+            expect(clickCounterEl.getText()).toEqual('1');
 
             expect(element(by.css('.demo-content .jigsaw-button-disabled')).isPresent()).toBeFalsy();
-            element(by.id('toggleDisable')).click();
+            toggleDisabledEl.click();
             expect(element(by.css('.demo-content .jigsaw-button-disabled')).isPresent()).toBeTruthy();
+            expect(testButtonEl.getCssValue('pointer-events')).toEqual('none');
 
-            element(by.id('toggleDisable')).click();
-            element(by.id('button')).click();
-            expect(clickCountSpan.getText()).toEqual('2');
+            toggleDisabledEl.click();
+            testButtonEl.click();
+            expect(clickCounterEl.getText()).toEqual('2');
         })
 
     });
+
+    describe('test width and height', () => {
+        beforeEach(() => {
+            browser.get('button/width_height');
+        });
+
+        it('should change size when set width and height', async () => {
+            const size = await element(by.id('test-button')).getSize();
+            expect(size.width).toEqual(300);
+            expect(size.height).toEqual(40);
+        })
+    });
+
+    describe('test preset', () => {
+        beforeEach(() => {
+            browser.get('button/preset');
+        });
+
+        it('should change size when set presize', async () => {
+            let size;
+            const testButton = element(by.id('test-button'));
+            const presetList = element(by.id('preset-list')).all(by.tagName('jigsaw-button'));
+
+            presetList.get(0).click();
+            await browser.wait(ExpectedConditions.not(
+                ExpectedConditions.presenceOf(element(by.css('.jigsaw-button-clicked')))));
+
+            size = await testButton.getSize();
+            expect(size.width).toEqual(80);
+            expect(size.height).toEqual(20);
+
+            presetList.get(1).click();
+            await browser.wait(ExpectedConditions.not(
+                ExpectedConditions.presenceOf(element(by.css('.jigsaw-button-clicked')))));
+            size = await testButton.getSize();
+            expect(size.width).toEqual(80);
+            expect(size.height).toEqual(28);
+
+            presetList.get(2).click();
+            await browser.wait(ExpectedConditions.not(
+                ExpectedConditions.presenceOf(element(by.css('.jigsaw-button-clicked')))));
+            size = await testButton.getSize();
+            expect(size.width).toEqual(80);
+            expect(size.height).toEqual(36);
+        })
+    })
 });
