89025bd33969f6d98f7a527d0a5546b51b30d362
陈旭 <chen.xu8@zte.com.cn>
2017-07-23 09:34:37 +0800

    Time和RangeTime支持国际化

--- src/jigsaw/component/time/index.ts
+++ src/jigsaw/component/time/index.ts
@@ -1,16 +1,16 @@
 import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
+import {TranslateService} from "@ngx-translate/core";
 import {JigsawTime} from "./time";
-import {JigsawTooltipModule} from "../tooltip/tooltip";
-import {PopupService} from "../../service/popup.service";
+import {JigsawTooltipModule} from "jigsaw/component/tooltip/tooltip";
+import {PopupService} from "jigsaw/service/popup.service";
 @NgModule({
     imports: [CommonModule, JigsawTooltipModule],
     declarations: [JigsawTime],
     exports: [JigsawTime],
-    providers: [PopupService],
+    providers: [PopupService, TranslateService],
 })
 export class JigsawTimeModule {
-
 }
 
 export * from './time';
--- src/jigsaw/component/time/time.ts
+++ src/jigsaw/component/time/time.ts
@@ -4,6 +4,7 @@ import {TimeGr, TimeService, TimeUnit, TimeWeekStart} from "../../service/time.s
 import {PopupInfo, PopupPositionType, PopupService} from "../../service/popup.service";
 import {SimpleTooltipComponent} from "../tooltip/tooltip";
 import {Time, WeekTime} from "../../service/time.types";
+import {TranslateService} from "@ngx-translate/core";
 
 
 export type TimeShortcutFunction = () => [WeekTime, WeekTime]
@@ -172,7 +173,8 @@ export class JigsawTime extends AbstractJigsawComponent implements OnInit, OnDes
     //定时器Id
     private _intervalId: number;
 
-    constructor(private _el: ElementRef, private _renderer: Renderer2, private _popService: PopupService) {
+    constructor(private _el: ElementRef, private _renderer: Renderer2,
+                private _popService: PopupService, private _translateService: TranslateService) {
         super();
         this._refreshInterval = 0;
         this.weekStart = TimeWeekStart.sun;
@@ -215,7 +217,107 @@ export class JigsawTime extends AbstractJigsawComponent implements OnInit, OnDes
             }
         });
         this._timepicker = $(insert).data("DateTimePicker");
+        this._defineLocale();
+        this._timepicker.locale(this._translateService.getBrowserLang());
+
         this._handleValueChange(<Time>this.date, <TimeGr>this.gr, true);
+    }
+
+    private _defineLocale() {
+        moment.defineLocale('zh', {
+            months : '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
+            monthsShort : '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
+            weekdays : '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
+            weekdaysShort : '周日_周一_周二_周三_周四_周五_周六'.split('_'),
+            weekdaysMin : '日_一_二_三_四_五_六'.split('_'),
+            longDateFormat : {
+                LT : 'HH:mm',
+                LTS : 'HH:mm:ss',
+                L : 'YYYY年MMMD日',
+                LL : 'YYYY年MMMD日',
+                LLL : 'YYYY年MMMD日Ah点mm分',
+                LLLL : 'YYYY年MMMD日ddddAh点mm分',
+                l : 'YYYY年MMMD日',
+                ll : 'YYYY年MMMD日',
+                lll : 'YYYY年MMMD日 HH:mm',
+                llll : 'YYYY年MMMD日dddd HH:mm'
+            },
+            meridiemParse: /凌晨|早上|上午|中午|下午|晚上/,
+            meridiemHour: function (hour, meridiem) {
+                if (hour === 12) {
+                    hour = 0;
+                }
+                if (meridiem === '凌晨' || meridiem === '早上' ||
+                    meridiem === '上午') {
+                    return hour;
+                } else if (meridiem === '下午' || meridiem === '晚上') {
+                    return hour + 12;
+                } else {
+                    // '中午'
+                    return hour >= 11 ? hour : hour + 12;
+                }
+            },
+            meridiem : function (hour, minute, isLower) {
+                var hm = hour * 100 + minute;
+                if (hm < 600) {
+                    return '凌晨';
+                } else if (hm < 900) {
+                    return '早上';
+                } else if (hm < 1130) {
+                    return '上午';
+                } else if (hm < 1230) {
+                    return '中午';
+                } else if (hm < 1800) {
+                    return '下午';
+                } else {
+                    return '晚上';
+                }
+            },
+            calendar : {
+                sameDay : '[今天]LT',
+                nextDay : '[明天]LT',
+                nextWeek : '[下]ddddLT',
+                lastDay : '[昨天]LT',
+                lastWeek : '[上]ddddLT',
+                sameElse : 'L'
+            },
+            dayOfMonthOrdinalParse: /\d{1,2}(日|月|周)/,
+            ordinal : function (number, period) {
+                switch (period) {
+                    case 'd':
+                    case 'D':
+                    case 'DDD':
+                        return number + '日';
+                    case 'M':
+                        return number + '月';
+                    case 'w':
+                    case 'W':
+                        return number + '周';
+                    default:
+                        return number;
+                }
+            },
+            relativeTime : {
+                future : '%s内',
+                past : '%s前',
+                s : '几秒',
+                m : '1 分钟',
+                mm : '%d 分钟',
+                h : '1 小时',
+                hh : '%d 小时',
+                d : '1 天',
+                dd : '%d 天',
+                M : '1 个月',
+                MM : '%d 个月',
+                y : '1 年',
+                yy : '%d 年'
+            },
+            week : {
+                // GB/T 7408-1994《数据元和交换格式·信息交换·日期和时间表示法》与ISO 8601:1988等效
+                dow : 1, // Monday is the first day of the week.
+                doy : 4  // The week that contains Jan 4th is the first week of the year.
+            }
+        });
     }
 
     //设置插件选中时间值
