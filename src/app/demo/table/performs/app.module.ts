8d601590d0a47e113b0783dcb9caab39168ad979
陈旭10045812 <chen.xu8@zte.com.cn>
2017-07-24 15:23:14 +0800

    fix-demo-error

--- src/app/demo/table/performs/app.module.ts
+++ src/app/demo/table/performs/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { JigsawTableModule } from "jigsaw/component/table/table";
 import { TablePerformsDemoComponent }  from './app.component';
-import { TableHeadIcon } from "./table-renderer";
+import {TableHeadIcon, TableHeadSelect} from "./table-renderer";
 
 @NgModule({
     imports: [ JigsawTableModule ],
-    declarations: [ TablePerformsDemoComponent, TableHeadIcon ],
+    declarations: [ TablePerformsDemoComponent, TableHeadIcon, TableHeadSelect ],
     bootstrap: [ TablePerformsDemoComponent ],
     entryComponents: [ TableHeadIcon ]
 })
