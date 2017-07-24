d002046d7b7ecaba7ad301710305041dae3b9298
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-07-24 15:50:15 +0800

    CI-config

--- src/app/demo/table/performs/app.module.ts
+++ src/app/demo/table/performs/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { JigsawTableModule } from "jigsaw/component/table/table";
 import { TablePerformsDemoComponent }  from './app.component';
-import {TableHeadIcon, TableHeadSelect} from "./table-renderer";
+import {TableHeadIcon} from "./table-renderer";
 
 @NgModule({
-    imports: [ JigsawTableModule ],
-    declarations: [ TablePerformsDemoComponent, TableHeadIcon, TableHeadSelect ],
+    imports: [ JigsawTableModule],
+    declarations: [ TablePerformsDemoComponent, TableHeadIcon],
     bootstrap: [ TablePerformsDemoComponent ],
     entryComponents: [ TableHeadIcon ]
 })
