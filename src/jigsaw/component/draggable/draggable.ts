03ca54975e3cd82689a815e480906ed68a19f787
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-07-27 11:30:42 +0800

    draggable销毁优化

--- src/jigsaw/component/draggable/draggable.ts
+++ src/jigsaw/component/draggable/draggable.ts
@@ -1,15 +1,19 @@
-import {Directive, Renderer2, ElementRef, NgModule, OnInit, Input} from "@angular/core";
+import {Directive, Renderer2, ElementRef, NgModule, OnInit, Input, OnDestroy} from "@angular/core";
 import {AffixUtils} from "../../core/utils/internal-utils";
 import {CommonUtils} from "../../core/utils/common-utils";
+import {CallbackRemoval} from "../../core/data/component-data";
 
 @Directive({
     selector: '[jigsaw-draggable]'
 })
-export class JigsawDraggable implements OnInit {
+export class JigsawDraggable implements OnInit, OnDestroy {
     private _dragTarget: HTMLElement;
     private _host: HTMLElement;
     private _draging: boolean = false;
-    private _pos: number[];
+    private _position: number[];
+    private _removeHostMouseDownListener: CallbackRemoval;
+    private _removeWindowMouseMoveListener: CallbackRemoval;
+    private _removeWindowMouseUpListener: CallbackRemoval;
 
     @Input()
     public affectedSelector: string;
@@ -18,7 +22,7 @@ export class JigsawDraggable implements OnInit {
     }
 
     private _dragStart = (event) => {
-        this._pos = [event.clientX - AffixUtils.offset(this._dragTarget).left,
+        this._position = [event.clientX - AffixUtils.offset(this._dragTarget).left,
             event.clientY - AffixUtils.offset(this._dragTarget).top];
         this._draging = true;
         event.preventDefault();
@@ -27,8 +31,8 @@ export class JigsawDraggable implements OnInit {
 
     private _dragMove = (event) => {
         if (this._draging) {
-            const ox = event.clientX - this._pos[0];
-            const oy = event.clientY - this._pos[1];
+            const ox = event.clientX - this._position[0];
+            const oy = event.clientY - this._position[1];
             this._renderer.setStyle(this._dragTarget, 'left', ox + 'px');
             this._renderer.setStyle(this._dragTarget, 'top', oy + 'px');
         }
@@ -36,7 +40,7 @@ export class JigsawDraggable implements OnInit {
 
     private _dragEnd = () => {
         this._draging = false;
-        this._pos = null;
+        this._position = null;
     };
 
     ngOnInit() {
@@ -46,9 +50,9 @@ export class JigsawDraggable implements OnInit {
 
         setTimeout(() => {
             if (this._isElementAffixed(this._dragTarget)) {
-                this._renderer.listen(this._host, 'mousedown', this._dragStart);
-                this._renderer.listen(document, 'mousemove', this._dragMove);
-                this._renderer.listen(document, 'mouseup', this._dragEnd);
+                this._removeHostMouseDownListener = this._renderer.listen(this._host, 'mousedown', this._dragStart);
+                this._removeWindowMouseMoveListener = this._renderer.listen(document, 'mousemove', this._dragMove);
+                this._removeWindowMouseUpListener = this._renderer.listen(document, 'mouseup', this._dragEnd);
             }
         })
     }
@@ -56,11 +60,13 @@ export class JigsawDraggable implements OnInit {
     private _isElementAffixed(element: HTMLElement): boolean {
         if (!(element instanceof HTMLElement)) return false;
         const positionType = element.style.position;
-        if (positionType == 'fixed' || positionType == 'absolute') {
-            return true;
-        } else {
-            return false
-        }
+        return positionType == 'fixed' || positionType == 'absolute';
+    }
+
+    ngOnDestroy(){
+        this._removeHostMouseDownListener();
+        this._removeWindowMouseMoveListener();
+        this._removeWindowMouseUpListener();
     }
 }
 
