a18ce31f8eb918c46c411a92b04fd643c78eb5e0
陈旭 <chen.xu8@zte.com.cn>
2017-08-04 22:29:59 +0800

    增加ensure-url-matches-path这个task

---
+++ build/tools/gulp/tasks/ensure-url-matches-path.ts
@@ -0,0 +1,27 @@
+import {task} from 'gulp';
+import {join} from 'path';
+import {existsSync} from "fs"
+import {navInfo} from "../../../../src/app/demo/nav-info"
+
+/**
+ * 确保demo的url和它的源码路径一致，这样在demo运行时，就可以直接跳转到它对应的plunker上了。
+ */
+task('ensure-url-matches-path', () => {
+    const demoHome = join(__dirname, '../../../../src/app/demo');
+    let unmatchedUrls:string[] = [];
+    navInfo.forEach(navItem => {
+        navItem.navList.forEach(nav => {
+            // console.log(join(demoHome, nav.url));
+            if (!existsSync(join(demoHome, nav.url, 'app.module.ts'))) {
+                unmatchedUrls.push(nav.url);
+            }
+        });
+    });
+    if (unmatchedUrls.length > 0) {
+        console.log('these urls do NOT match there file path:');
+        console.log(unmatchedUrls);
+        process.exit(1);
+    } else {
+        console.log('great! all urls match their path!');
+    }
+});
