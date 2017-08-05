b7984696faa408b6e454589d800727f38b367472
陈旭10045812 <chen.xu8@zte.com.cn>
2017-08-05 16:18:29 +0800

    回退alert的修改，必须使用TranslateService

--- src/jigsaw/component/alert/alert.ts
+++ src/jigsaw/component/alert/alert.ts
@@ -1,8 +1,8 @@
-import {Component, ElementRef, Input, NgModule, Optional, Renderer2, ViewChild} from "@angular/core";
-import {AbstractDialogComponentBase, DialogBase, JigsawDialog, JigsawDialogModule} from "../dialog/dialog";
+import {Component, ElementRef, Input, NgModule, Renderer2, ViewChild} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {AbstractDialogComponentBase, DialogBase, JigsawDialog, JigsawDialogModule} from "../dialog/dialog";
 import {JigsawButtonModule} from "../button/button";
-import {TranslateService} from "@ngx-translate/core";
 import {InternalUtils} from "../../core/utils/internal-utils";
 import {TranslateHelper} from "../../core/utils/translate-helper";
 import {JigsawMovableModule} from "../../directive/movable/index";
@@ -172,55 +172,53 @@ export class JigsawErrorAlert extends JigsawCommonAlert {
 }
 
 @NgModule({
-    imports: [JigsawDialogModule, JigsawMovableModule, JigsawButtonModule, CommonModule],
+    imports: [JigsawDialogModule, JigsawMovableModule, JigsawButtonModule, CommonModule, TranslateModule.forRoot()],
     declarations: [JigsawAlert, JigsawInfoAlert, JigsawWarningAlert, JigsawErrorAlert],
     exports: [JigsawDialogModule, JigsawMovableModule, JigsawAlert, JigsawInfoAlert, JigsawWarningAlert, JigsawErrorAlert],
     providers: [TranslateService],
     entryComponents: [JigsawInfoAlert, JigsawWarningAlert, JigsawErrorAlert]
 })
 export class JigsawAlertModule {
-    constructor(@Optional() translateService: TranslateService) {
-        if (translateService) {
-            InternalUtils.initI18n(translateService, 'alert', {
-                zh: {
-                    button: {
-                        ok: "确定",
-                        cancel: "取消",
-                        yes: "是",
-                        no: "否",
-                        abort: "终止",
-                        ignore: "忽略",
-                        retry: "重试"
-                    },
-                    title: {
-                        info: "提示",
-                        warning: "警告",
-                        error: "错误",
-                        confirm: "确认"
-                    }
+    constructor(translateService: TranslateService) {
+        InternalUtils.initI18n(translateService, 'alert', {
+            zh: {
+                button: {
+                    ok: "确定",
+                    cancel: "取消",
+                    yes: "是",
+                    no: "否",
+                    abort: "终止",
+                    ignore: "忽略",
+                    retry: "重试"
                 },
-                en: {
-                    button: {
-                        ok: "OK",
-                        cancel: "Cancel",
-                        yes: "Yes",
-                        no: "No",
-                        abort: "Abort",
-                        ignore: "Ignore",
-                        retry: "Retry"
-                    },
-                    title: {
-                        info: "Information",
-                        warning: "Warning",
-                        error: "Error",
-                        confirm: "Confirm"
-                    }
+                title: {
+                    info: "提示",
+                    warning: "警告",
+                    error: "错误",
+                    confirm: "确认"
                 }
-            });
-            translateService.setDefaultLang(translateService.getBrowserLang());
-            TranslateHelper.languageChangEvent.subscribe(langInfo => {
-                translateService.use(langInfo.curLang);
-            });
-        }
+            },
+            en: {
+                button: {
+                    ok: "OK",
+                    cancel: "Cancel",
+                    yes: "Yes",
+                    no: "No",
+                    abort: "Abort",
+                    ignore: "Ignore",
+                    retry: "Retry"
+                },
+                title: {
+                    info: "Information",
+                    warning: "Warning",
+                    error: "Error",
+                    confirm: "Confirm"
+                }
+            }
+        });
+        translateService.setDefaultLang(translateService.getBrowserLang());
+        TranslateHelper.languageChangEvent.subscribe(langInfo => {
+            translateService.use(langInfo.curLang);
+        });
     }
 }
