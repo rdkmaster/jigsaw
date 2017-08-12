b7d54e714d492780413f4db90085e7a2859838aa
陈旭 <chen.xu8@zte.com.cn>
2017-08-12 22:50:02 +0800

    fix colorType refactoring error

--- src/app/demo/button/with-loading/app.component.ts
+++ src/app/demo/button/with-loading/app.component.ts
@@ -3,7 +3,7 @@ import {Component, Renderer2, ViewContainerRef} from "@angular/core";
 
 @Component({
     template: `
-        <jigsaw-button (click)="onClick()" [colorType]="primary" height="32px">
+        <jigsaw-button (click)="onClick()" colorType="primary" height="32px">
             <jigsaw-loading *ngIf="isLoading" width="16px" height="16px" color="white"></jigsaw-loading>
             <span style="padding-left: 5px">{{label}}</span>
         </jigsaw-button>
