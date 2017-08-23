a6c034c1a406984113b515bc76f98ecf512743b3
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-08-23 14:02:05 +0800

    soft-bank-theming

--- src/app/demo/tooltip/dialog/user-defined-tooltip-dialog.ts
+++ src/app/demo/tooltip/dialog/user-defined-tooltip-dialog.ts
@@ -4,9 +4,13 @@ import {JigsawTooltipDialog, TooltipBase} from "jigsaw/component/tooltip/tooltip
 
 @Component({
     templateUrl: 'user-defined-tooltip-dialog.html',
+    styles: [`.tooltip-content{line-height: 1;font-size: 12px}
+    .tooltip-content h3{margin-bottom: 8px;font-size: 14px;}
+    .tooltip-content h3 span{margin-right: 5px}
+    .tooltip-content p{line-height: 1.4;}`]
 })
 export class UserTooltipDialogComponent extends TooltipBase{
     @ViewChild(JigsawTooltipDialog) public tooltip: JigsawTooltipDialog;
-
+    
 }
 
--- src/app/demo/tooltip/in-dom/app.component.ts
+++ src/app/demo/tooltip/in-dom/app.component.ts
@@ -3,12 +3,20 @@ import {Component, Renderer2, ViewContainerRef} from "@angular/core";
 @Component({
     template: `
         <jigsaw-tooltip-dialog>
-            <span class="fa fa-thumbs-up"></span>This is a message!<br><br>
-            This is some extra messages!<br>
-            This is some extra messages!<br>
-            This is some extra messages!<br>
+            <div class="tooltip-content">
+                <h3><span class="fa fa-thumbs-up"></span>This is a message!</h3>
+                <p>
+                    This is some extra messages!<br>
+                    This is some extra messages!<br>
+                    This is some extra messages!<br>
+                </p>
+            </div>
         </jigsaw-tooltip-dialog>
-    `
+    `,
+    styles: [`.tooltip-content{line-height: 1;font-size: 12px}
+    .tooltip-content h3{margin-bottom: 8px;font-size: 14px;}
+    .tooltip-content h3 span{margin-right: 5px}
+    .tooltip-content p{line-height: 1.4;}`]
 })
 export class TooltipInDomDemoComponent {
     constructor(public viewContainerRef: ViewContainerRef,
