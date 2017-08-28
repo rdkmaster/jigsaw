8ec848db7ecd1d4c51344545b353f30729e35fff
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-08-28 10:59:26 +0800

    delete unuse gulp task

--- build/tools/gulp/gulpfile.ts
+++ build/tools/gulp/gulpfile.ts
@@ -1,26 +1,14 @@
 import {createPackageBuildTasks} from './packaging/build-tasks-gulp';
 
 // Create gulp tasks to build the different packages in the project.
-//createPackageBuildTasks('cdk');
-//createPackageBuildTasks('jigsaw', ['cdk']);
-//createPackageBuildTasks('jigsaw-examples', ['jigsaw']);
 createPackageBuildTasks('jigsaw');
 
-import './tasks/ci';
 import './tasks/clean';
 import './tasks/default';
-import './tasks/development';
-//import './tasks/docs';
-import './tasks/e2e';
-import './tasks/lint';
+//import './tasks/lint';
 import './tasks/publish';
-import './tasks/screenshots';
-//import './tasks/unit-test';
-import './tasks/aot';
-//import './tasks/payload';
-import './tasks/coverage';
+//import './tasks/aot';
 import './tasks/material-release';
-import './tasks/universal';
 import './tasks/validate-release';
 
 import './tasks/ensure-url-matches-path'
