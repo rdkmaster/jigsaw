e6b136a77d6d9da4e53725f272a3cbbf9789fd9c
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-09-04 14:51:12 +0800

    修改time-demo里面的tile

--- src/app/e2e-testee/range-time/limitEnd/app.module.ts
+++ src/app/e2e-testee/range-time/limitEnd/app.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {JigsawRangeTimeModule} from "jigsaw/component/range-time/index";
 import {JigsawTileSelectModule} from "jigsaw/component/tile-select/tile-select";
 import {RangeTimeLimitEndComponent} from "./app.component";
+import {CommonModule} from "@angular/common";
 @NgModule({
     declarations: [RangeTimeLimitEndComponent],
     bootstrap: [RangeTimeLimitEndComponent],
-    imports: [JigsawRangeTimeModule,JigsawTileSelectModule]
+    imports: [JigsawRangeTimeModule,JigsawTileSelectModule, CommonModule]
 })
 export class RangeTimeLimitEndModule{
 
--- src/app/e2e-testee/range-time/limitStart/app.module.ts
+++ src/app/e2e-testee/range-time/limitStart/app.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {JigsawRangeTimeModule} from "jigsaw/component/range-time/index";
 import {JigsawTileSelectModule} from "jigsaw/component/tile-select/tile-select";
 import {RangeTimeLimitStartComponent} from "./app.component";
+import {CommonModule} from "@angular/common";
 @NgModule({
     declarations: [RangeTimeLimitStartComponent],
     bootstrap: [RangeTimeLimitStartComponent],
-    imports: [JigsawRangeTimeModule,JigsawTileSelectModule]
+    imports: [JigsawRangeTimeModule,JigsawTileSelectModule,CommonModule]
 })
 export class RangeTimeLimitStartModule{
 
