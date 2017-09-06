82a2aed2cdb06f8c34fd92015b0151b57b43656e
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-09-06 15:38:13 +0800

    list demo modify

--- src/app/live-demo/list/app.module.ts
+++ src/app/live-demo/list/app.module.ts
@@ -3,9 +3,10 @@ import {ListBasicDemoComponent} from "./app.component";
 import {JigsawListModule} from "jigsaw/component/list-and-tile/list";
 import {CommonModule} from "@angular/common";
 import {JigsawCheckBoxModule} from "../../../jigsaw/component/checkbox/index";
+import {JigsawComboSelectModule} from "../../../jigsaw/component/combo-select/index";
 
 @NgModule({
-    imports: [JigsawListModule, CommonModule, JigsawCheckBoxModule],
+    imports: [JigsawListModule, CommonModule, JigsawCheckBoxModule, JigsawComboSelectModule],
     declarations: [ListBasicDemoComponent]
 })
 export class ListBasicDemoModule{
