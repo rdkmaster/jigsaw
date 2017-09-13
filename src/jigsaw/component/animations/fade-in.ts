1b6aec96fcf293c830836c6ae225d461d5bf6a73
尤海鹏10210844 <you.haipeng@zte.com.cn>
2017-09-13 10:24:30 +0800

    fish-bone-style

--- src/jigsaw/component/animations/fade-in.ts
+++ src/jigsaw/component/animations/fade-in.ts
@@ -3,13 +3,13 @@ import { trigger, state, style, transition, animate, keyframes, AnimationTrigger
 export const fadeIn: AnimationTriggerMetadata = trigger('fadeIn', [
   state('in', style({opacity: 1})),
   transition('void => *', [
-       animate(600, keyframes([
+       animate(400, keyframes([
         style({opacity: 0, offset: 0}),
         style({opacity: 1, offset: 1.0})
       ]))
   ]),
   transition('* => void', [
-        animate(600, keyframes([
+        animate(400, keyframes([
         style({opacity: 1, offset: 0}),
         style({opacity: 0, offset: 1.0})
       ]))
