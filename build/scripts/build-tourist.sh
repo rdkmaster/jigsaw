e02c60d96cb2ce1f24e4f976af9591277f5518c0
陈旭10045812 <chen.xu8@zte.com.cn>
2017-09-15 14:27:52 +0800

    fix-integrate-seed-error

--- build/scripts/build-tourist.sh
+++ build/scripts/build-tourist.sh
@@ -2,6 +2,12 @@
 home=`pwd`
 
 touristDir=$1
+echo "--------------------------------------------------"
+echo $touristDir
+exit
+
+
+
 mkdir -p $touristDir
 git clone https://github.com/rdkmaster/jigsaw-tourist.git $touristDir
 cd $touristDir
