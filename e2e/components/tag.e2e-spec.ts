01852d689dc4acea06ae1993136a28c8477f6c8c
朱明鹏 <zhu.mingpeng@zte.com.cn>
2017-09-18 20:07:16 +0800

    tile and tag

---
+++ e2e/components/tag.e2e-spec.ts
@@ -0,0 +1,21 @@
+import {browser, element, by, ExpectedConditions} from "protractor";
+
+describe('tile', () => {
+    beforeEach(() => {
+        browser.waitForAngularEnabled(false);
+    });
+    describe('test selectedItems', () => {
+        beforeEach(() => {
+            browser.get('/#/tag/basic');
+        });
+        it('delete tag and check the color of tag', async () => {
+            const jigsawTag = element.all(by.tagName('jigsaw-tag'));
+            expect(jigsawTag.get(0).getAttribute('ng-reflect-closable')).toBe('false');
+            expect(element(by.css('.right-box .jigsaw-tag-closable')).all(by.tagName('SPAN')).get(0).getText()).toBe('222222');
+            jigsawTag.get(1).element(by.css('.jigsaw-tag-close-bar')).click();//数量不减；
+            await browser.wait(ExpectedConditions.not(ExpectedConditions.presenceOf(element(by.css('.right-box .jigsaw-tag-closable')))));//it works!
+            expect(element(by.css('.right-box .jigsaw-tag-closable')).all(by.tagName('SPAN')).get(0).getText()).toBe('wdffrrghhttt3dd2222222fffffff55ffferrrrr');
+            expect(jigsawTag.get(2).getCssValue('background-color')).toBe('rgba(0, 143, 212, 1)')
+        })
+    });
+});
---
+++ e2e/components/tile.e2e-spec.ts
@@ -0,0 +1,37 @@
+import {browser, element, by} from "protractor";
+
+describe('tile', () => {
+    beforeEach(() => {
+        browser.waitForAngularEnabled(false);
+    });
+    describe('test selectedItems', () => {
+        beforeEach(() => {
+            browser.get('/#/tile/selectedItems');
+        });
+        it('should be active when clicked', () => {
+            const jigsawTitleEl = element(by.tagName('jigsaw-tile')),
+                tileOptionsEl = jigsawTitleEl.all(by.tagName('jigsaw-tile-option'));
+            expect(jigsawTitleEl.element(by.css('.jigsaw-tile-option-active')).getText()).toBe('深圳');
+            tileOptionsEl.get(1).click();
+            expect(jigsawTitleEl.element(by.css('.jigsaw-tile-option-active')).getText()).toBe('上海');
+        })
+    });
+    describe('test selectedItems', () => {
+        beforeEach(() => {
+            browser.get('/#/tile/multipleSelect');
+        });
+        it('should be active when clicked', () => {
+            const jigsawTitleEl = element(by.tagName('jigsaw-tile')),
+                tileOptionsEl = jigsawTitleEl.all(by.tagName('jigsaw-tile-option'));
+            expect(jigsawTitleEl.element(by.css('.jigsaw-tile-option-active')).getText()).toBe('深圳');
+            tileOptionsEl.get(0).click();
+            expect(jigsawTitleEl.all(by.css('.jigsaw-tile-option-active')).count()).toBe(2);
+            tileOptionsEl.get(3).click();
+            expect(jigsawTitleEl.all(by.css('.jigsaw-tile-option-active')).count()).toBe(1);
+            tileOptionsEl.get(2).click();
+            expect(jigsawTitleEl.all(by.css('.jigsaw-tile-option-active')).count()).toBe(2);
+            tileOptionsEl.get(3).click();
+            expect(jigsawTitleEl.all(by.css('.jigsaw-tile-option-active')).count()).toBe(3);
+        })
+    });
+});
