7a520ec6c2364cd4fe7c55931cfa9b8c9071d6b4
朱明鹏 <zhu.mingpeng@zte.com.cn>
2017-09-20 18:40:15 +0800

    time different

--- e2e/components/time.e2e-spec.ts
+++ e2e/components/time.e2e-spec.ts
@@ -1,4 +1,6 @@
 import {browser, element, by, ExpectedConditions} from "protractor";
+import {until} from "selenium-webdriver";
+import elementLocated = until.elementLocated;
 
 describe('time', () => {
     beforeEach(() => {
@@ -19,10 +21,10 @@ describe('time', () => {
             expect(element(by.css('.datepicker-days')).element(by.css('.old')).getCssValue('color')).toBe('rgba(153, 153, 153, 1)');
             expect(element(by.css('.datepicker-days')).element(by.css('.new')).getCssValue('color')).toBe('rgba(153, 153, 153, 1)');
             nextMonth.click();
-            expect(daysYearEl.getText()).toBe('十月 2017');
+            expect(daysYearEl.getText()).toBe('October 2017');
             daysYearEl.click();
             pickerMonth.all(by.tagName('SPAN')).get(5).click();
-            expect(daysYearEl.getText()).toBe('四月 2017');
+            expect(daysYearEl.getText()).toBe('April 2017');
             daysYearEl.click();
             expect(pickerMonth.element(by.css('.picker-switch')).getText()).toBe('2017');
             pickerMonth.element(by.css('.picker-switch')).click();
@@ -41,12 +43,28 @@ describe('time', () => {
         });
         it('should not be selected when choose time beyond the limit end ', () => {
             const pEl = element(by.css('.demo-content')).element(by.tagName('P')),
+                limitEndOp = element.all(by.tagName('jigsaw-tile-option')),
                 pickerDays = element(by.css('.datepicker-days'));
             expect(pEl.getText()).toBe('Selected Time : 2017-09-20');
             pickerDays.all(by.tagName('TR')).get(3).all(by.tagName('TD')).get(4).click();
             expect(pEl.getText()).toBe('Selected Time : 2017-09-07');
-            pickerDays.all(by.tagName('TR')).get(6).all(by.tagName('TD')).get(4).click();
-            expect(pEl.getText()).toBe('Selected Time : 2017-09-07');
+            limitEndOp.get(1).click();
+            pickerDays.all(by.tagName('TR')).get(5).all(by.tagName('TD')).get(4).click();
+            expect(pEl.getText()).toBe('Selected Time : 2017-09-21');
+            limitEndOp.get(0).click();
+            expect(pEl.getText()).toBe('Selected Time : 2017-09-20');
+        })
+    });
+    describe('test time', () => {
+        beforeEach(() => {
+            browser.get('/#/time/weekStart');
+        });
+        it('should be set', () => {
+            const weekStartOp = element.all(by.tagName('jigsaw-tile-option')),
+                weekStartEl = element(by.css('.datepicker-days')).all(by.tagName('TR')).get(1).all(by.tagName('TH')).get(0);
+              expect(weekStartEl.getText()).toBe('日');
+            weekStartOp.get(1).click();
+            expect(weekStartEl.getText()).toBe('一');
         })
     });
 });
