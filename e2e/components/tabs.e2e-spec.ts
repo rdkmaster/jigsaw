adccd53decc0a5328b3f24c21bcbc0dffc04a5ed
朱明鹏 <zhu.mingpeng@zte.com.cn>
2017-09-20 14:39:14 +0800

    whether it need to set wait

--- e2e/components/tabs.e2e-spec.ts
+++ e2e/components/tabs.e2e-spec.ts
@@ -21,4 +21,39 @@ describe('tabs', () => {
             expect(tabsLabelEl.get(3).getCssValue('pointer-events')).toBe('none');
         })
     });
+    describe('test tabApi', () => {
+        beforeEach(() => {
+            browser.get('/#/tabs/tabApi');
+        });
+        it('should operate tabs when click jigsaw button', async () => {
+            const jigsawButton = element(by.css('.container')).all(by.css('.jigsaw-button')),
+                tabsLabelEl = element.all(by.tagName('jigsaw-tab-label')),
+                tabContentEl = element.all(by.tagName('jigsaw-tab-content'));
+            expect(tabContentEl.get(0).getText()).toBe('This is the body of the first tab');
+            jigsawButton.get(0).click();
+            await  browser.wait(ExpectedConditions.presenceOf(tabsLabelEl.get(4)));
+            expect(tabContentEl.get(4).element(by.tagName('H3')).getText()).toBe('Hi Jerry, please input your information');
+            jigsawButton.get(3).click();
+           // await  browser.wait(ExpectedConditions.not(ExpectedConditions.textToBePresentInElement(tabContentEl.get(0), 'tab content 1')));
+            expect( tabContentEl.get(0).getCssValue('opacity')).toBe('0');
+            jigsawButton.get(4).click();
+            expect( tabContentEl.get(0).getCssValue('opacity')).toBe('1');
+            jigsawButton.get(5).click();
+            //await  browser.wait(ExpectedConditions.not(ExpectedConditions.textToBePresentInElement(tabContentEl.get(0), 'tab content 1')));
+            expect(tabsLabelEl.get(0).getText()).toBe('Tab 2');
+        })
+    });
+    describe('test tabApi', () => {
+        beforeEach(() => {
+            browser.get('/#/tabs/withInput');
+        });
+        it('should display table when click "GetTableData"',async()=>{
+            const getTableDate=element(by.css('.container')).element(by.tagName('jigsaw-button')),
+                tableEl=element(by.tagName('TABLE'));
+            expect(tableEl.getText()).toBe('');
+            getTableDate.click();
+           // await browser.wait(ExpectedConditions.presenceOf(element(by.tagName('TD'))));
+            expect(tableEl.getText()).not.toBe('');
+        })
+    });
 });
