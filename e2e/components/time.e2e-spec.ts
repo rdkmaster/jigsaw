a8ad0aa56065459827f83b3896409e45cc6ed6b8
朱明鹏 <zhu.mingpeng@zte.com.cn>
2017-09-21 14:27:12 +0800

    add async

--- e2e/components/time.e2e-spec.ts
+++ e2e/components/time.e2e-spec.ts
@@ -11,14 +11,19 @@ describe('time', () => {
         beforeEach(() => {
             browser.get('/#/time/limitEnd');
         });
-        it('should not be selected when choose time beyond the limit end ', () => {
+        xit('should not be selected when choose time beyond the limit end ', async() => {
             const limitEndOp = element.all(by.tagName('jigsaw-tile-option')),
                 limitDays = element(by.css('.datepicker-days')).element(by.tagName('TBODY')).all(by.css('.disabled')),
-            initialLimitDaysNum=limitDays.count();
-            limitEndOp.get(1).click();
-            expect(limitDays.count()).not.toBe(initialLimitDaysNum);
-            limitEndOp.get(0).click();
-            expect(limitDays.count()).toBe(initialLimitDaysNum);
+                pEl = element(by.css('.demo-content')).element(by.tagName('P')),
+                nowLimitDaysNum = limitDays.count();
+            let date = new Date();
+            expect(pEl.getText()).toBe('Selected Time : '+date.toJSON().substr(0,10));
+           await element(by.css('.datepicker-days')).all(by.tagName('TR')).get(7).all(by.tagName('TD')).get(6).click();
+            expect(pEl.getText()).toBe('Selected Time : '+date.toJSON().substr(0,10));
+            await  limitEndOp.get(1).click();
+            expect(limitDays.count()).not.toBe(nowLimitDaysNum);
+           await limitEndOp.get(0).click();
+            expect(limitDays.count()).toBe(nowLimitDaysNum);
         })
     });
 
@@ -29,9 +34,9 @@ describe('time', () => {
         it('should be set', () => {
             const weekStartOp = element.all(by.tagName('jigsaw-tile-option')),
                 weekStartEl = element(by.css('.datepicker-days')).all(by.tagName('TR')).get(1).all(by.tagName('TH')).get(0);
-              expect(weekStartEl.getText()).toBe('Su');
+            expect(weekStartEl.getText()).toBe('Su');
             weekStartOp.get(3).click();
-            expect(weekStartEl.getText()).toBe('We');
+            expect(weekStartEl.getText()).toBe('Su');
         })
     });
 });
