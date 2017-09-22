97bc56d6e45961ccb5c4bdcaae73d49b3c94b8f0
朱明鹏 <zhu.mingpeng@zte.com.cn>
2017-09-22 14:11:15 +0800

    improve check methods

--- e2e/components/slider.e2e-spec.ts
+++ e2e/components/slider.e2e-spec.ts
@@ -1,5 +1,6 @@
 import {browser, element, by} from 'protractor';
-import {ILocation} from "selenium-webdriver";
+import {ILocation, until} from "selenium-webdriver";
+import elementLocated = until.elementLocated;
 
 describe('slider', () => {
     beforeEach(() => {
@@ -12,26 +13,33 @@ describe('slider', () => {
         });
         it('shoud drag and drop slider', () => {
             const sliderHandle = element.all(by.css('.jigsaw-slider-handle')),
-                spanEl = element.all(by.tagName('ng-component span')),
+                handleTag=element.all(by.tagName('slider-handle')),
                 sliderSwitch = element(by.css('.jigsaw-switch-small')),
             sliderTrack=element.all(by.tagName('jigsaw-slider')).get(3).element(by.css('.jigsaw-slider-track'));
-            expect(spanEl.get(0).getText()).toBe('取值: 30');
+            expect(handleTag.get(0).getAttribute('ng-reflect-value')).toBe('30');
             browser.actions().dragAndDrop(sliderHandle.get(0), {x: 20000, y: 0}).perform();
-            expect(spanEl.get(0).getText()).toBe('取值: 100');
+            expect(handleTag.get(0).getAttribute('ng-reflect-value')).toBe('100');
             browser.actions().dragAndDrop(sliderHandle.get(0), {x: -20000, y: 0}).perform();
-            expect(spanEl.get(0).getText()).toBe('取值: 10');
+            expect(handleTag.get(0).getAttribute('ng-reflect-value')).toBe('10');
             sliderSwitch.click();
             browser.actions().dragAndDrop(sliderHandle.get(0), {x: 20000, y: 0}).perform();
-            expect(spanEl.get(0).getText()).toBe('取值: 10');
-            browser.actions().dragAndDrop(sliderHandle.get(1), {x: -20000, y: 0}).perform();browser.sleep(400);
-            expect(spanEl.get(1).getText()).toBe('取值: 1');
-            browser.actions().dragAndDrop(sliderHandle.get(1), {x: 20000, y: 0}).perform();browser.sleep(400);
-            expect(spanEl.get(1).getText()).toBe('取值: 20');
+            expect(handleTag.get(0).getAttribute('ng-reflect-value')).toBe('10');
+            browser.actions().dragAndDrop(sliderHandle.get(1), {x: -20000, y: 0}).perform();
+            expect(handleTag.get(1).getAttribute('ng-reflect-value')).toBe('1');
+            browser.actions().dragAndDrop(sliderHandle.get(1), {x: 20000, y: 0}).perform();
+            expect(handleTag.get(1).getAttribute('ng-reflect-value')).toBe('20');
             expect(element.all(by.tagName('jigsaw-slider')).get(2).getAttribute('step')).toBe('0.01');
-            // browser.actions().dragAndDrop(sliderHandle.get(3), {x: -200, y: 0}).perform();browser.sleep(500);
-            // expect(sliderHandle.get(3).getAttribute('left')).toBe('17%');
-            // browser.actions().dragAndDrop(sliderHandle.get(4), {x: -200, y: 0}).perform();browser.sleep(500);
-            // expect(sliderHandle.get(4).getAttribute('left')).toBe('47%');
+            browser.actions().dragAndDrop(sliderHandle.get(3), {x: -20000, y: 0}).perform();browser.sleep(1000);
+            expect(handleTag.get(3).getAttribute('ng-reflect-value')).toBe('0');
+            browser.actions().dragAndDrop(sliderHandle.get(5), {x: 20000, y: 0}).perform();browser.sleep(1000);
+            expect(handleTag.get(5).getAttribute('ng-reflect-value')).toBe('100');
+            browser.actions().dragAndDrop(sliderHandle.get(7), {x: 0, y: 10000}).perform();
+            expect(handleTag.get(7).getAttribute('ng-reflect-value')).toBe('0');
+            browser.actions().dragAndDrop(sliderHandle.get(8), {x: 0, y: -10000}).perform();
+            expect(handleTag.get(8).getAttribute('ng-reflect-value')).toBe('100');
+            browser.actions().dragAndDrop(sliderHandle.get(9), {x: 0, y: 10000}).perform();
+            expect(handleTag.get(9).getAttribute('ng-reflect-value')).toBe('20');
+
         });
 
     });
