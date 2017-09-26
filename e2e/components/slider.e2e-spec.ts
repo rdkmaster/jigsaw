fd26b4d004906921e823960a6565fc091f0dd390
朱明鹏 <zhu.mingpeng@zte.com.cn>
2017-09-26 14:45:07 +0800

    add mouseMove to mitulate drag

--- e2e/components/slider.e2e-spec.ts
+++ e2e/components/slider.e2e-spec.ts
@@ -18,37 +18,37 @@ describe('slider', () => {
                 sliderSwitch = element(by.css('.jigsaw-switch-small')),
                 sliderTrack = element.all(by.tagName('jigsaw-slider')).get(3).element(by.css('.jigsaw-slider-track'));
             await expect(handleTag.get(0).getAttribute('ng-reflect-value')).toBe('30');
-            await browser.actions().mouseDown(sliderHandle.get(0)).mouseMove({x: 1000, y: 0}).mouseUp().perform();
+            await browser.actions().mouseMove(sliderHandle.get(0)).mouseDown().mouseMove({x: 1000, y: 0}).mouseUp().perform();
             await  expect(handleTag.get(0).getAttribute('ng-reflect-value')).toBe('100');
 
-            await browser.actions().mouseDown(sliderHandle.get(0)).mouseMove({x: -5000, y: 0}).mouseUp().perform();
+            await browser.actions().mouseMove(sliderHandle.get(0)).mouseDown().mouseMove({x: -5000, y: 0}).mouseUp().perform();
 
             await    sliderSwitch.click();
-            await  browser.actions().mouseDown(sliderHandle.get(1)).mouseMove({x: 5000, y: 0}).mouseUp().perform();
+            await  browser.actions().mouseMove(sliderHandle.get(1)).mouseDown().mouseMove({x: 5000, y: 0}).mouseUp().perform();
 
             await   expect(handleTag.get(0).getAttribute('ng-reflect-value')).toBe('10');
 
-            await browser.actions().mouseDown(sliderHandle.get(1)).mouseMove({x: -5000, y: 0}).mouseUp().perform();
+            await browser.actions().mouseMove(sliderHandle.get(1)).mouseDown().mouseMove({x: -5000, y: 0}).mouseUp().perform();
 
             await   expect(handleTag.get(1).getAttribute('ng-reflect-value')).toBe('1');
-            await  browser.actions().mouseDown(sliderHandle.get(1)).mouseMove({x: 5000, y: 0}).mouseUp().perform();
+            await  browser.actions().mouseMove(sliderHandle.get(1)).mouseDown().mouseMove({x: 5000, y: 0}).mouseUp().perform();
 
             await   expect(handleTag.get(1).getAttribute('ng-reflect-value')).toBe('20');
 
             await    expect(element.all(by.tagName('jigsaw-slider')).get(2).getAttribute('step')).toBe('0.01');
 
-            await browser.actions().mouseDown(sliderHandle.get(3)).mouseMove({x: -5000, y: 0}).mouseUp().perform();
+            await browser.actions().mouseMove(sliderHandle.get(3)).mouseDown().mouseMove({x: -5000, y: 0}).mouseUp().perform();
             await expect(handleTag.get(3).getAttribute('ng-reflect-value')).toBe('0');
-            await browser.actions().mouseDown(sliderHandle.get(5)).mouseMove({x: 5000, y: 0}).mouseUp().perform();
+            await browser.actions().mouseMove(sliderHandle.get(5)).mouseDown().mouseMove({x: 5000, y: 0}).mouseUp().perform();
             await   expect(handleTag.get(5).getAttribute('ng-reflect-value')).toBe('100');
 
-            await browser.actions().mouseDown(sliderHandle.get(7)).mouseMove({x: 0, y: 5000}).mouseUp().perform();
+            await browser.actions().mouseMove(sliderHandle.get(7)).mouseDown().mouseMove({x: 0, y: 5000}).mouseUp().perform();
             await   expect(handleTag.get(7).getAttribute('ng-reflect-value')).toBe('0');
-            await  browser.actions().mouseDown(sliderHandle.get(8)).mouseMove({x: 0, y: -5000}).mouseUp().perform();
+            await  browser.actions().mouseMove(sliderHandle.get(8)).mouseDown().mouseMove({x: 0, y: -5000}).mouseUp().perform();
 
             await  expect(handleTag.get(8).getAttribute('ng-reflect-value')).toBe('100');
 
-            await browser.actions().mouseDown(sliderHandle.get(9)).mouseMove({x: 0, y: 5000}).mouseUp().perform();
+            await browser.actions().mouseMove(sliderHandle.get(9)).mouseDown().mouseMove({x: 0, y: 5000}).mouseUp().perform();
             await  expect(handleTag.get(9).getAttribute('ng-reflect-value')).toBe('20');
         });
     });
