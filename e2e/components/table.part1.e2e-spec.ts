df21fafab78248be50df2e3b018ed5964bd8d6ec
朱明鹏 <zhu.mingpeng@zte.com.cn>
2017-09-30 16:29:12 +0800

    setHeaderRender和setHeaderSort

--- e2e/components/table.part1.e2e-spec.ts
+++ e2e/components/table.part1.e2e-spec.ts
@@ -22,4 +22,31 @@ describe('table', () => {
             expect(element.all(by.css('.red-text')).get(10).getCssValue('COLOR')).toBe('rgb(255, 170, 0)');
         })
     });
+    describe('test setHeaderRender', () => {
+        beforeEach(() => {
+            browser.get('/#/table/setHeaderRender');
+        });
+        it('should set header render', () => {
+            expect(element(by.css('.fa-map-signs')).isPresent()).toBe(true);
+        })
+    });
+    describe('test setHeaderSort', () => {
+        beforeEach(() => {
+            browser.get('/#/table/setHeaderSort');
+        });
+        it('should set header sort', () => {
+            const tableSortUp = element(by.css('.jigsaw-table-sort-up')),
+                tableSortDown = element(by.css('.jigsaw-table-sort-down')),
+                columnFirstEl = element(by.css('.jigsaw-table-body')).all(by.tagName('TR')).first().all(by.tagName('TD')).get(0),
+                columnlastEl = element(by.css('.jigsaw-table-body')).all(by.tagName('TR')).last().all(by.tagName('TD')).get(0);
+            expect(columnFirstEl.getText()).toBe('Tony');
+            expect(columnlastEl.getText()).toBe('Scott');
+            tableSortUp.click();
+            expect(columnFirstEl.getText()).toBe('Aaron');
+            expect(columnlastEl.getText()).toBe('Zora');
+            tableSortDown.click();
+            expect(columnFirstEl.getText()).toBe('Zora');
+            expect(columnlastEl.getText()).toBe('Aaron');
+        })
+    });
 });
