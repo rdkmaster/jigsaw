93aad48cc3ef02a67f36f1762fd57f7a29892155
尤海鹏 <you.haipeng@zte.com.cn>
2017-10-31 16:17:41 +0800

    tree component add treeObj property

--- src/app/e2e-testee/tree/basic/app.component.ts
+++ src/app/e2e-testee/tree/basic/app.component.ts
@@ -1,12 +1,15 @@
-import {Component, Renderer2, ViewContainerRef} from "@angular/core";
+import {AfterViewInit, Component, Renderer2, ViewChild, ViewContainerRef} from "@angular/core";
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 import {TreeData} from "jigsaw/core/data/tree-data";
+import {JigsawTreeExt} from "../../../../jigsaw/component/tree/tree-ext";
 
 @Component({
     templateUrl: './app.component.html'
 })
-export class ZtreeDemoComponent {
+export class ZtreeDemoComponent implements AfterViewInit{
+    @ViewChild(JigsawTreeExt) treeExt: JigsawTreeExt;
+
     public data: TreeData;
 
     constructor(public viewContainerRef: ViewContainerRef,
@@ -78,5 +81,15 @@ export class ZtreeDemoComponent {
     public onClick(msg: any) {
         console.log("click");
         console.log(msg);
+    }
+
+    ngAfterViewInit(){
+        if(this.treeExt && this.treeExt.treeObj){
+            console.log(this.treeExt.treeObj);
+            let nodes = this.treeExt.treeObj.getNodes();
+            if (nodes.length>0) {
+                this.treeExt.treeObj.selectNode(nodes[1]);
+            }
+        }
     }
 }
