e64a319e4027b3c67e8974428ee6985a0bb120a4
陈旭10045812 <chen.xu8@zte.com.cn>
2017-11-06 20:51:51 +0800

    add install.sh

---
+++ build/install/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+scriptDir=$(cd `dirname $0`; pwd)
+#cd $scriptDir/../../
+
+if [ -d $scriptDir/../../node_modules ]; then
+	echo "directory node_modules existed!"
+	exit 1
+fi
+
+echo "Downloading dependency package..."
+rm -f node_modules.zip
+wget http://rdk.zte.com.cn:8800/misc/node_modules.zip
+
+if [ ! -e node_modules.zip ]; then
+	echo "Unable to download node_modules.zip, please check your network configuration."
+	exit 1
+fi
+
+echo "Unpackaging..."
+unzip node_modules.zip -d $scriptDir/../../ >/dev/null
+rm -f node_modules.zip
+
+echo "Compiling..."
+cd $scriptDir/../../
+chmod +x node_modules/.bin/ng
+npm start
