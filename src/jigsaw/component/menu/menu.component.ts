eb4c47f833c81306627315ecbbbd57654a96277a
10227461 <tang.cheng11@zte.com.cn>
2017-11-09 20:30:56 +0800

    feat(menu): add j-list

--- src/jigsaw/component/menu/menu.component.ts
+++ src/jigsaw/component/menu/menu.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, forwardRef, OnInit, ViewEncapsulation} from '@angular/core';
+import {NG_VALUE_ACCESSOR} from "@angular/forms";
+
+import {AbstractJigsawComponent} from "../common";
+import {PopupService} from "../../service/popup.service";
 
 @Component({
-  selector: 'jigsaw-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+    selector: 'jigsaw-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => JigsawMenuComponent),
+            multi: true
+        }
+    ],
 })
-export class JigsawMenuComponent implements OnInit {
+export class JigsawMenuComponent extends AbstractJigsawComponent implements OnInit {
+    _titles = [
+        {
+            title: 'Settings',
+            subTitle: 'Ctrl+Alt+A',
+            subMenu: false
+        },
+        {
+            title: 'Print',
+            subTitle: '',
+            subMenu: true
+        },
+        {
+            title: 'Save All',
+            subTitle: 'Ctrl+S',
+            subMenu: false
+        },
+        {
+            title: 'Exit',
+            subTitle: '',
+            subMenu: true
+        }
+    ];
+    _selectedItems1: string;
 
-  constructor() { }
+    constructor(public popupService:PopupService) {
+        super();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    _handleSelect(selectedItems) {
+        this._selectedItems1 = selectedItems.map(item => item.title).toString()
+    }
 }
--- src/jigsaw/component/menu/menu.module.ts
+++ src/jigsaw/component/menu/menu.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+
 import {JigsawMenuComponent} from './menu.component';
+import {JigsawListModule} from "../list-and-tile/list";
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        JigsawListModule
     ],
     declarations: [JigsawMenuComponent],
     exports: [JigsawMenuComponent]
