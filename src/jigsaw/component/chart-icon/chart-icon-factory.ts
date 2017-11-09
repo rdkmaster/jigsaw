f0d27ba6fa2ae6d3cc75e59aea908ce81faf8115
尤海鹏 <you.haipeng@zte.com.cn>
2017-11-09 16:41:23 +0800

    use a constant to get no data image src

--- src/jigsaw/component/chart-icon/chart-icon-factory.ts
+++ src/jigsaw/component/chart-icon/chart-icon-factory.ts
@@ -151,7 +151,7 @@ export class ChartIconFactory {
                         this.$box = $('<div class="peity-no-data"></div>');
                     }
                     this.$el.hide().after(this.$box);
-                    this.$box.empty().append(`<image width="${width}" height="${height}" src="${CommonUtils.noDataSrc}">`);
+                    this.$box.empty().append(`<image width="${width}" height="${height}" src="${CommonUtils.NoDataSrc}">`);
                     return;
                 } else {
                     if (this.$box) {
--- src/jigsaw/component/graph/graph.ts
+++ src/jigsaw/component/graph/graph.ts
@@ -35,7 +35,7 @@ export class JigsawGraph extends AbstractJigsawComponent implements OnInit, OnDe
     /**
      * internal
      */
-    public _$noDataSrc = CommonUtils.noDataSrc;
+    public _$noDataSrc = CommonUtils.NoDataSrc;
 
     // 由数据服务提供的数据.
     private _data: AbstractGraphData;
