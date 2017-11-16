c5a4e16129a5e97f89db9f7e59bb012d00834aca
tc9011 <tc9011@126.com>
2017-11-17 01:13:49 +0800

    refactor(menu):add select

--- src/jigsaw/component/menu/menu.component.ts
+++ src/jigsaw/component/menu/menu.component.ts
@@ -22,7 +22,7 @@ import {MenuData, MenuCallback} from "./menu.typings";
 export class JigsawMenuComponent extends AbstractJigsawComponent implements OnInit {
     _items: MenuData[];
     _activeItem: HTMLElement;
-    _selectedItems1: string;
+    _selectedItems1: MenuData;
     _root: boolean;
 
     @Output() public select: EventEmitter<MenuData> = new EventEmitter<MenuData>();
@@ -76,7 +76,11 @@ export class JigsawMenuComponent extends AbstractJigsawComponent implements OnIn
     }
 
     _handleSelect(selectedItems) {
-        this._selectedItems1 = selectedItems.map(item => item.label).toString();
+        this._selectedItems1 = selectedItems.map(item => item);
     }
 
+    _emitSelectedIem(e: Event) {
+        e.stopPropagation();
+        this.select.emit(this._selectedItems1);
+    }
 }
