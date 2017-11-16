0bcf1ae7390602feeecd9947eca9b10079857daf
tc9011 <tc9011@126.com>
2017-11-17 01:42:02 +0800

    test(menu):test

--- src/jigsaw/component/menu/menu.component.ts
+++ src/jigsaw/component/menu/menu.component.ts
@@ -22,7 +22,7 @@ import {MenuData, MenuCallback} from "./menu.typings";
 export class JigsawMenuComponent extends AbstractJigsawComponent implements OnInit {
     _items: MenuData[];
     _activeItem: HTMLElement;
-    _selectedItems1: MenuData;
+    _selectedItems = [];
     _root: boolean;
 
     @Output() public select: EventEmitter<MenuData> = new EventEmitter<MenuData>();
@@ -76,11 +76,11 @@ export class JigsawMenuComponent extends AbstractJigsawComponent implements OnIn
     }
 
     _handleSelect(selectedItems) {
-        this._selectedItems1 = selectedItems.map(item => item);
+        this._selectedItems.push(selectedItems.map(item => item));
     }
 
     _emitSelectedIem(e: Event) {
-        e.stopPropagation();
-        this.select.emit(this._selectedItems1);
+        // e.stopPropagation();
+        this.select.emit(this._selectedItems[0]);
     }
 }
