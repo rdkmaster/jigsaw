655cee8350cc71b7f2ac39997ccccc846470f466
10227461 <tang.cheng11@zte.com.cn>
2017-11-23 09:29:45 +0800

    fix(rate): fix style bug

--- src/jigsaw/component/rate/rate.component.ts
+++ src/jigsaw/component/rate/rate.component.ts
@@ -19,7 +19,7 @@ import {AbstractJigsawComponent} from "../common";
     ],
 })
 export class JigsawRateComponent extends AbstractJigsawComponent implements OnInit, ControlValueAccessor {
-    _prefixCls = 'rate';
+    _prefixCls = 'jigsaw-rate';
     _innerPrefixCls = `${this._prefixCls}-star`;
     _classMap;
     _starArray: Array<any> = [];
