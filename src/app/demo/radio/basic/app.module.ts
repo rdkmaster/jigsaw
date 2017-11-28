d548ba3951dc8398af95df806fc5002ad4c926de
陈旭10045812 <chen.xu8@zte.com.cn>
2017-11-28 15:45:36 +0800

    better demo code

--- src/app/demo/radio/basic/app.module.ts
+++ src/app/demo/radio/basic/app.module.ts
@@ -3,12 +3,11 @@ import {JigsawRadioModule} from "jigsaw/component/radio/radio";
 import {CommonModule} from "@angular/common";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {RadioBasicDemoComponent} from "./app.component";
-import {JigsawButtonModule} from "../../../../jigsaw/component/button/button";
 
 @NgModule({
     declarations: [RadioBasicDemoComponent],
     bootstrap: [RadioBasicDemoComponent],
-    imports: [JigsawRadioModule, CommonModule, JigsawDemoDescriptionModule, JigsawButtonModule]
+    imports: [JigsawRadioModule, CommonModule, JigsawDemoDescriptionModule]
 })
 export class RadioBasicDemoModule {
 
