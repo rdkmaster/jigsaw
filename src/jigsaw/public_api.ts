5a38185a5a743ce1013aef3cc82a1f7a0ea6492f
陈旭10045812 <chen.xu8@zte.com.cn>
2017-12-04 14:16:53 +0800

    add viewport export

--- src/jigsaw/public_api.ts
+++ src/jigsaw/public_api.ts
@@ -3,63 +3,60 @@
  * @description
  * Entry point for all public APIs of Jigsaw.
  */
-
-export * from './module';
-
-export * from './core/data/array-collection';
-export * from './core/data/component-data';
-export * from './core/data/echart-types';
-export * from './core/data/general-collection';
-export * from './core/data/graph-data';
-export * from './core/data/table-data';
-export * from './core/data/tree-data';
-
-export * from './core/utils/common-utils';
-export * from './core/utils/internal-utils';
-export * from './core/utils/translate-helper';
-
-export * from './service/popup.service';
-export * from './service/loading.service';
-export * from './service/time.service';
-export * from './service/time.types';
-
-export * from './component/common';
-export * from './component/alert/alert';
-export * from './component/block/block';
-export * from './component/button/button';
+export * from "./component/alert/alert";
+export * from "./component/block/block";
+export * from "./component/button/button";
+export * from "./component/chart-icon/chart-icon-factory";
+export * from "./component/checkbox/index";
 export * from "./component/checkbox/typings";
-export * from './component/checkbox/index';
-export * from './component/collapse/collapse';
-export * from './component/combo-select/index';
-export * from './component/dialog/dialog';
-export * from './component/graph/index';
-export * from './component/input/input';
-export * from './component/loading/loading';
-export * from './component/pagination/pagination';
-export * from './component/radio/radio';
-export * from './component/range-time/index';
-export * from './component/root/root';
-export * from './component/select/select';
-export * from './component/slider/index';
-export * from './component/switch/index';
-export * from './component/table/table';
-export * from "./component/table/table-typings";
-export * from "./component/table/table-renderer";
-export * from "./component/table/table-inner.components";
-export * from './component/tabs/index';
-export * from './component/tag/tag';
-export * from './component/list-and-tile/tile';
-export * from './component/list-and-tile/list';
+export * from "./component/collapse/collapse";
+export * from "./component/combo-select/index";
+export * from "./component/common";
+export * from "./component/dialog/dialog";
+export * from "./component/fish-bone/fish-bone";
+export * from "./component/graph/index";
+export * from "./component/input/input";
 export * from "./component/list-and-tile/group-common";
-export * from './component/time/index';
-export * from './component/tooltip/tooltip';
-export * from './component/tree/tree-ext';
-export * from './component/tree/ztree-types';
-export * from './component/fish-bone/fish-bone';
-export * from './component/chart-icon/chart-icon-factory';
-
+export * from "./component/list-and-tile/list";
+export * from "./component/list-and-tile/tile";
+export * from "./component/loading/loading";
+export * from "./component/pagination/pagination";
+export * from "./component/radio/radio";
+export * from "./component/range-time/index";
+export * from "./component/root/root";
+export * from "./component/select/select";
+export * from "./component/slider/index";
+export * from "./component/switch/index";
+export * from "./component/table/table";
+export * from "./component/table/table-inner.components";
+export * from "./component/table/table-renderer";
+export * from "./component/table/table-typings";
+export * from "./component/tabs/index";
+export * from "./component/tag/tag";
+export * from "./component/time/index";
+export * from "./component/tooltip/tooltip";
+export * from "./component/tree/tree-ext";
+export * from "./component/tree/ztree-types";
+export * from "./component/viewport/viewport";
+export * from "./core/data/array-collection";
+export * from "./core/data/component-data";
+export * from "./core/data/echart-types";
+export * from "./core/data/general-collection";
+export * from "./core/data/graph-data";
+export * from "./core/data/table-data";
+export * from "./core/data/tree-data";
+export * from "./core/utils/common-utils";
+export * from "./core/utils/internal-utils";
+export * from "./core/utils/translate-helper";
+export * from "./directive/dragdrop/index";
 export * from "./directive/dragdrop/types";
-export * from './directive/dragdrop/index';
-export * from './directive/movable/index';
-export * from './directive/trusted-html/trusted-html';
+export * from "./directive/movable/index";
+export * from "./directive/trusted-html/trusted-html";
+export * from "./module";
+export * from "./service/loading.service";
+export * from "./service/popup.service";
+export * from "./service/time.service";
+export * from "./service/time.types";
+
+
 
