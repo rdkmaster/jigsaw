db575fbebbceefaef41141841783a02fa459d305
尤海鹏 <you.haipeng@zte.com.cn>
2017-12-05 09:57:43 +0800

    table resize commend

--- src/app/demo/table/resize/app.component.ts
+++ src/app/demo/table/resize/app.component.ts
@@ -21,11 +21,14 @@ export class TableResizeDemoComponent implements OnInit {
     @ViewChild('tableCmp') tableCmp: JigsawTable;
 
     changeTableWidth() {
+        // 修改table的width，table会自动resize
+        // window resize时，table也会自动resize
         this.tableWidth = this.tableWidth == '50%' ? '100%' : '50%';
     }
 
     changeBoxWidth() {
         this.box.style.width = this.box.style.width == '50%' ? '100%' : '50%';
+        // 父元素的宽度变化（非window resize引起的），需要用户手动调一下table的resize
         this.tableCmp.resize();
     }
 
