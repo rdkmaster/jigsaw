d46bf062537e7c3da7ab1036e89025a588d58d0c
陈旭10045812 <chen.xu8@zte.com.cn>
2017-12-07 19:09:35 +0800

    add exports for resize demo

--- src/app/demo/table/resize/app.module.ts
+++ src/app/demo/table/resize/app.module.ts
@@ -7,7 +7,7 @@ import {JigsawButtonModule} from "jigsaw/component/button/button";
 @NgModule({
     imports: [JigsawTableModule, JigsawDemoDescriptionModule, JigsawButtonModule],
     declarations: [TableResizeDemoComponent],
-    bootstrap: [TableResizeDemoComponent],
+    exports: [TableResizeDemoComponent],
 })
 export class TableResizeDemoModule {
 }
