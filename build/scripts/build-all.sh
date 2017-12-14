0c60a76aee6cc0040ea7dacc2f088e72e8c4613d
陈旭 <chen.xu8@zte.com.cn>
2017-12-14 21:25:01 +0800

    add build-all.sh

--- build/scripts/build-all.sh
+++ build/scripts/build-all.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 seedDir=`pwd`/../jigsaw-seed
 
-cd `dirname $0`
-
 chmod a+x ./sauce_connect_setup.sh
 ./sauce_connect_setup.sh
 
-node make-demo-independent.js
+node buid/scripts/make-demo-independent.js
 gulp ensure-url-matches-path
 gulp jigsaw:build-release:clean
-# ng test --code-coverage --watch false
-# cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
-sh build-tourist.sh
-sh integrate-with-seed.sh
-sh sauce_connect_block.sh
+sh buid/scripts/build-tourist.sh
+sh buid/scripts/integrate-with-seed.sh
+sh buid/scripts/sauce_connect_block.sh
 cd $seedDir # run e2e in seed directory
 ng build -prod -aot
 ng e2e
