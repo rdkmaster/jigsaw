d2ea03cfa8f226ee3e20bd7eb00ab5a8d213fba8
尤海鹏 <you.haipeng@zte.com.cn>
2017-12-27 15:55:51 +0800

    update demo

--- src/app/demo/table/scroll-listen/app.component.ts
+++ src/app/demo/table/scroll-listen/app.component.ts
@@ -1,13 +1,14 @@
-import {AfterViewInit, Component, ElementRef, Renderer2, ViewChild} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, OnDestroy, Renderer2, ViewChild} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {TableData} from "jigsaw/core/data/table-data";
 import {JigsawTable} from "../../../../jigsaw/component/table/table";
+import {CallbackRemoval} from "../../../../jigsaw/core/utils/common-utils";
 
 @Component({
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class TableScrollListenDemoComponent implements AfterViewInit{
+export class TableScrollListenDemoComponent implements AfterViewInit, OnDestroy {
     tableData: TableData;
 
     constructor(http: HttpClient, private _renderer: Renderer2) {
@@ -24,15 +25,21 @@ export class TableScrollListenDemoComponent implements AfterViewInit{
 
     @ViewChild(JigsawTable) table: JigsawTable;
     @ViewChild('content') content: ElementRef;
-    _removeHorizontalScrollListener;
+    _removeHorizontalScrollListener: CallbackRemoval;
 
-    ngAfterViewInit(){
+    ngAfterViewInit() {
         this._removeHorizontalScrollListener = this._renderer.listen(
             this.table.contentScrollbar.elementRef.nativeElement, 'ps-scroll-x', () => {
                 this._renderer.setStyle(this.content.nativeElement, 'left',
                     -this.table.contentScrollbar.geometry().x + 'px');
             });
 
+    }
+
+    ngOnDestroy() {
+        if (this._removeHorizontalScrollListener) {
+            this._removeHorizontalScrollListener()
+        }
     }
 
     // ====================================================================
