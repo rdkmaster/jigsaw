40d8482fecdb906b92f66326cf88690986bb397d
尤海鹏 <you.haipeng@zte.com.cn>
2017-12-29 10:32:13 +0800

    remove unused code

--- src/app/demo/layout/form/app.component.ts
+++ src/app/demo/layout/form/app.component.ts
@@ -1,19 +1,12 @@
 import {Component} from "@angular/core";
 import {ArrayCollection} from "../../../../jigsaw/core/data/array-collection";
 import {TimeGr, TimeService} from "../../../../jigsaw/service/time.service";
-import {TableData} from "../../../../jigsaw/core/data/table-data";
-import {ColumnDefine} from "../../../../jigsaw/component/table/table-typings";
 
 @Component({
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class FormDemoComponent {
-
-    constructor() {
-        this.favoriteFruit = new ArrayCollection<any>([{label: this.fruitList.data[0][0]}]);
-    }
-
     firstName: string = 'jigsaw';
     remember: boolean = true;
     rangeTime = {
@@ -26,27 +19,11 @@ export class FormDemoComponent {
         {label: this.rangeTime.endDate, closable: false}
     ]);
     comeFrom: any = {label: 'Nan Jing'};
-    favoriteFruit: ArrayCollection<any>;
     favoriteCities: ArrayCollection<any>;
     score: number = 30;
     isGreat: boolean = true;
 
     formValue: any;
-    fruitList: TableData = new TableData(
-        [
-            ["banana", "$12.0", "The banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa.", "Southeast Asia"],
-            ["apple", "$21.0", "The apple tree is a deciduous tree in the rose family best known for its sweet, pomaceous fruit, the apple.", "Shan Dong, China"],
-            ["strawberry", "$31.0", "The garden strawberry is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries. It is cultivated worldwide for its fruit.", "Southeast Asia"],
-            ["watermelon", "$13.0", "Watermelon Citrullus lanatus var. lanatus is a scrambling and trailing vine in the flowering plant family Cucurbitaceae.", "Southeast Asia"],
-            ["pineapple", "$33.0", "The pineapple is a tropical plant with an edible multiple fruit consisting of coalesced berries, also called pineapples.", "Southeast Asia"],
-            ["pear", "$11.0", "The pear is any of several tree and shrub species of genus Pyrus, in the family Rosaceae. It is also the name of the pomaceous fruit of the trees. ", "Southeast Asia"],
-        ],
-        ["name", "price", "desc", "origin"],
-        ["Name", "Price", "Description", "Origin"]);
-    columnDef: ColumnDefine[] = [
-        {target: 'desc', width: '60%'}
-    ];
-    selectedIndex: number = 0;
 
     lastNamePattern = /^[a-z]+$/i;
 
@@ -59,17 +36,6 @@ export class FormDemoComponent {
         this.rangeTimeComboValue[0].label = this.rangeTime.beginDate;
         this.rangeTimeComboValue[1].label = this.rangeTime.endDate;
         this.rangeTimeComboValue.refresh();
-    }
-
-    onFavoriteFruitChange(selected) {
-        this.favoriteFruit = new ArrayCollection<any>([{label: this.fruitList.data[selected][0]}]);
-        this.selectedIndex = selected;
-    }
-
-    onFavoriteFruitRemoved(value) {
-        if (value.length == 0) {
-            this.selectedIndex = -1;
-        }
     }
 
     // ====================================================================
