81e328424fce126c381a071bef3aaa8eda536cac
陈旭10045812 <chen.xu8@zte.com.cn>
2018-01-02 15:46:04 +0800

    simplify box scroll demo

--- src/app/demo/layout/box-layout-scroll/app.component.ts
+++ src/app/demo/layout/box-layout-scroll/app.component.ts
@@ -7,12 +7,11 @@ import {Component} from "@angular/core";
 export class BoxLayoutScrollDemoComponent {
 
     arr1 = new Array(20);
-    arr2 = new Array(5);
 
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = '';
+    summary: string = '这个DEMO演示了在内容溢出后，j-box在滚动条方面的行为，以及如何添加自定义滚动条';
     description: string = '';
 }
 
