276e6c2e4dd7c2c158da273939df7a9a09c66b49
10222927 <30395891+10222927@users.noreply.github.com>
2018-01-02 13:54:43 +0800

    CI: fixes i18n (#442)

--- e2e/components/i18n.e2e-spec.ts
+++ e2e/components/i18n.e2e-spec.ts
@@ -15,16 +15,19 @@ describe('i18n', () => {
                 dialogContent = $('jigsaw-dialog').$('.dialog-content'),
                 rangeSwitch = $('jigsaw-range-time').$('.picker-switch'),
                 timeSwitch = $('jigsaw-time').$('.picker-switch');
+            const enMonth =['January','February','March','April','May','June','July','August','September','October','November','December'],
+                cnMonth = ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'];
+            let now = new Date();
             buttons.get(0).click();
             expect(alertContent.getText()).toBe('This is a great info alert!');
             expect(dialogContent.getText()).toBe('Create task success');
-            expect(rangeSwitch.getText()).toBe('December 2017');
-            expect(timeSwitch.getText()).toBe('December 2017');
+            expect(rangeSwitch.getText()).toBe(enMonth[now.getMonth()]+' '+now.getFullYear());
+            expect(timeSwitch.getText()).toBe(enMonth[now.getMonth()]+' '+now.getFullYear());
             buttons.get(1).click();
             expect(alertContent.getText()).toBe('这是一个非常棒的 info 提示框！');
             expect(dialogContent.getText()).toBe('创建任务成功');
-            expect(rangeSwitch.getText()).toBe('十二月 2017');
-            expect(timeSwitch.getText()).toBe('十二月 2017');
+            expect(rangeSwitch.getText()).toBe(cnMonth[now.getMonth()]+' '+now.getFullYear());
+            expect(timeSwitch.getText()).toBe(cnMonth[now.getMonth()]+' '+now.getFullYear());
         })
     })
 });
