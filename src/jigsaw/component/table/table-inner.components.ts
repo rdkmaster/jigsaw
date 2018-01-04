450cd3da29868df5c1d2cd51ee97eaa164866ac6
10222927 <zhu.mingpeng@zte.com.cn>
2018-01-04 17:14:09 +0800

    sort table when oninit

--- src/jigsaw/component/table/table-inner.components.ts
+++ src/jigsaw/component/table/table-inner.components.ts
@@ -182,6 +182,7 @@ export class JigsawTableHeaderInternalComponent extends TableInternalCellBase im
     }
 
     private _sort(order: SortOrder): void {
+        console.log(this._$sortOrderClass['jigsaw-table-des']);
         this.updateSortOrderClass(order);
         this.sort.emit({sortAs: this.sortAs, order: order, field: this.field});
         this.tableData.sort(this.sortAs, order, this.field);
@@ -190,6 +191,12 @@ export class JigsawTableHeaderInternalComponent extends TableInternalCellBase im
     ngOnInit() {
         //设置默认渲染器
         this.renderer = this.renderer ? this.renderer : DefaultCellRenderer;
+        if (this._$sortOrderClass['jigsaw-table-asc']) {
+            this._sort(SortOrder.asc);
+        }
+        if (this._$sortOrderClass['jigsaw-table-des']) {
+            this._sort(SortOrder.desc);
+        }
     }
 
     ngOnDestroy() {
