68840725c921a4df59a74e9c4ed5542322978286
10222927 <zhu.mingpeng@zte.com.cn>
2018-01-09 14:48:25 +0800

    delete

--- e2e/components/alert.e2e-spec.ts
+++ e2e/components/alert.e2e-spec.ts
@@ -65,7 +65,6 @@ describe('alert', () => {
             browser.get('/alert/customized');
         });
         it('should change color when selected ', () => {
-            browser.get('/alert/customized');
             const jigsawAlertEl = element(by.tagName('jigsaw-alert')),
                 labelCheckboxEl = jigsawAlertEl.element(by.css('.jigsaw-checkbox'));
             labelCheckboxEl.click();
