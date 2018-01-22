7f8af4b64de4961bf93e1f09ed2b98a2f95a0214
尤海鹏 <you.haipeng@zte.com.cn>
2018-01-22 12:54:33 +0800

    fixes dependency detected

--- src/jigsaw/component/view-editor/resizable.directive.ts
+++ src/jigsaw/component/view-editor/resizable.directive.ts
@@ -1,7 +1,5 @@
-
 import {Directive, ElementRef, Input, NgModule, NgZone, Renderer2} from "@angular/core";
 import {CallbackRemoval} from "../../core/utils/common-utils";
-import {JigsawViewLayout} from "./view-editor";
 import {AffixUtils} from "../../core/utils/internal-utils";
 
 @Directive({
@@ -16,11 +14,15 @@ export class JigsawResizable{
     }
 
     @Input()
-    public parent: JigsawViewLayout;
+    public movableTarget: HTMLElement;
+
+    @Input()
+    public effectBox: HTMLElement;
+
+    @Input()
+    public effectDirection: string;
 
     private _host: HTMLElement;
-
-    private _movableTarget: HTMLElement;
     private _moving: boolean = false;
     private _position: number[];
 
@@ -31,57 +33,47 @@ export class JigsawResizable{
         event.preventDefault();
         event.stopPropagation();
 
-        if (!this.parent) return;
+        if (!this.movableTarget) return;
 
-        const resizeLineEl = this._host;
-        this._movableTarget = this.parent.resizingLine.nativeElement;
-        const startOffsetX = AffixUtils.offset(resizeLineEl).left - AffixUtils.offset(this.parent.element).left;
-        const startOffsetY = AffixUtils.offset(resizeLineEl).top - AffixUtils.offset(this.parent.element).top;
-        if (this.parent.direction == 'column') {
-            this._renderer.setStyle(this._movableTarget, 'top', startOffsetY + 'px');
+        const startOffsetX = AffixUtils.offset(this._host).left - AffixUtils.offset(this.effectBox).left;
+        const startOffsetY = AffixUtils.offset(this._host).top - AffixUtils.offset(this.effectBox).top;
+        if (this.effectDirection == 'column') {
+            this._renderer.setStyle(this.movableTarget, 'top', startOffsetY + 'px');
         } else {
-            this._renderer.setStyle(this._movableTarget, 'left', startOffsetX + 'px');
+            this._renderer.setStyle(this.movableTarget, 'left', startOffsetX + 'px');
         }
-        this._renderer.setStyle(this._movableTarget, 'display', 'block');
-        //this.parent.isResizing = true;
+        this._renderer.setStyle(this.movableTarget, 'display', 'block');
 
         this._position = [event.clientX - startOffsetX, event.clientY - startOffsetY];
         this._moving = true;
 
-        if (this._removeWindowMouseMoveListener) {
-            this._removeWindowMouseMoveListener();
-        }
+        this._removeWindowListener();
+
         this._zone.runOutsideAngular(() => {
             this._removeWindowMouseMoveListener = this._renderer.listen(document, 'mousemove', this._dragMove);
         });
 
-        if (this._removeWindowMouseUpListener) {
-            this._removeWindowMouseUpListener();
-        }
         this._removeWindowMouseUpListener = this._renderer.listen(document, 'mouseup', this._dragEnd);
     };
 
     private _dragMove = (event) => {
         if (this._moving) {
-            /*const isFixed = this._movableTarget.style.position == 'fixed';
-            const ox = event.clientX - this._position[0] - (isFixed ? window.pageXOffset : 0);
-            const oy = event.clientY - this._position[1] - (isFixed ? window.pageYOffset : 0);*/
-            if (this.parent.direction == 'column') {
+            if (this.effectDirection == 'column') {
                 let oy = event.clientY - this._position[1];
                 if (oy < 0) {
                     oy = 0
-                } else if (oy > this.parent.element.offsetHeight) {
-                    oy = this.parent.element.offsetHeight - 5
+                } else if (oy > this.effectBox.offsetHeight) {
+                    oy = this.effectBox.offsetHeight - 5
                 }
-                this._renderer.setStyle(this._movableTarget, 'top', oy + 'px');
+                this._renderer.setStyle(this.movableTarget, 'top', oy + 'px');
             } else {
                 let ox = event.clientX - this._position[0];
                 if (ox < 0) {
                     ox = 0
-                } else if (ox > this.parent.element.offsetWidth) {
-                    ox = this.parent.element.offsetWidth - 5
+                } else if (ox > this.effectBox.offsetWidth) {
+                    ox = this.effectBox.offsetWidth - 5
                 }
-                this._renderer.setStyle(this._movableTarget, 'left', ox + 'px');
+                this._renderer.setStyle(this.movableTarget, 'left', ox + 'px');
             }
         }
     };
@@ -90,7 +82,7 @@ export class JigsawResizable{
         this._moving = false;
         this._position = null;
         this._removeWindowListener();
-        this._renderer.setStyle(this._movableTarget, 'display', 'none');
+        this._renderer.setStyle(this.movableTarget, 'display', 'none');
     };
 
     private _removeWindowListener() {
@@ -106,7 +98,6 @@ export class JigsawResizable{
         this._removeWindowListener();
     }
 }
-
 
 @NgModule({
     declarations: [JigsawResizable],
