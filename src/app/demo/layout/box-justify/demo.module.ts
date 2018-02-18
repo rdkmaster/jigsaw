b09123e6879abb160e9afc9367a814178bc53bc6
陈旭 <chen.xu8@zte.com.cn>
2018-02-18 09:47:43 +0800

    box-layout-demo-fix

--- src/app/demo/layout/box-justify/demo.module.ts
+++ src/app/demo/layout/box-justify/demo.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from "@angular/core";
-import {BoxJustifyDemoComponent} from "./demo.component";
-import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {CommonModule} from "@angular/common";
 import {JigsawRadioModule} from "jigsaw/component/radio/radio";
-import {JigsawBoxModule} from "../../../../jigsaw/component/box/index";
+import {JigsawBoxModule} from "jigsaw/component/box/index";
+import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
+import {BoxJustifyDemoComponent} from "./demo.component";
 
 @NgModule({
     declarations: [BoxJustifyDemoComponent],
