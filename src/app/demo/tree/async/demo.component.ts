eb6f476e00f58b5741ff61aa5f4f9d178c8948a5
陈旭 <chen.xu8@zte.com.cn>
2018-03-21 23:05:08 +0800

    close #549 (#605)

--- src/app/demo/tree/async/demo.component.ts
+++ src/app/demo/tree/async/demo.component.ts
@@ -10,17 +10,16 @@ export class ZtreeAsynDemoComponent {
         async: {
             enable: true,
             type: 'get',
-            url: function (treeId, node) {
-                //由于这个demo的运行环境比较复杂导致这个url比实际开发时的url要复杂一些
-                //实际开发时的url类似这样  /rdk/service/app/example/server/my_service?param=$param
-                let url = '/rdk/service/app/common/relay?' +
-                    'service=../doc/client/demo/comprehensive/tree/lazy_load/web/mock/data.js&param=$param';
-                let obj = {};
-                if (node) {
-                    obj = {key: node.key, name: node.name};
-                }
-
-                return encodeURI(url.replace('$param', JSON.stringify(obj)));
+            url: (treeId, node) => {
+                // 这个demo的运行对环境要求较高，无法模拟。
+                // 使用上还是比较简单的，这个函数把参数拼装好，返回一个url，ztree会去调用这个url取得数据。
+                // 服务端必须返回类似下面这样的一个数组回来：
+                // [
+                //     {key: '1', name: 'n1', label: "n1", isParent: true},
+                //     {key: '2', name: 'n2', label: "n2", isParent: true},
+                //     {key: '3', name: 'n3', label: "n3", isParent: true}
+                // ]
+                return `the-simulated-tree-data-url?key=${node.key}&name=${node.name}`;
             }
         }
     };
