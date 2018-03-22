f4194f2b0ef7ad7763e6c0a89d38fce336ccfeec
尤海鹏 <you.haipeng@zte.com.cn>
2018-03-22 15:09:35 +0800

    optimize style

--- src/app/demo/table/no-data/demo.component.ts
+++ src/app/demo/table/no-data/demo.component.ts
@@ -9,30 +9,6 @@ export class TableNoDataDemoComponent {
 
     constructor() {
         this.tableData = new TableData(
-            /*[
-                [
-                    "Tiger Nixon1",
-                    "System Architect",
-                    "$320,00",
-                    "a normal content",
-                    "2011/04/25",
-                    "Edinburgh",
-                    "542",
-                    "a normal content",
-                    "a normal content"
-                ],
-                [
-                    "Garrett Winflters1",
-                    "Accountant",
-                    "$170,7",
-                    "a normal content",
-                    "2011/07/25",
-                    "Tokyo",
-                    "8422",
-                    "a normal content",
-                    "a normal content"
-                ],
-            ],*/
             [],
             ["name", "position", "salary", "title1", "enroll-date", "office", "extn", "title2", "title3"],
             ["姓名", "职位", "薪资", "很长很长很长很长很长很长很长的用于测试的标题", "入职日期", "部门", "其他", "很长很长很长很长很长很长很长的用于测试的标题", "很长很长很长很长很长很长很长的用于测试的标题"]);
