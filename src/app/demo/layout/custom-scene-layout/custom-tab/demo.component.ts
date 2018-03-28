c5a74510f887aaddf876bd4f3e619dc4b0bc60fa
尤海鹏 <you.haipeng@zte.com.cn>
2018-03-28 15:18:41 +0800

    cache tabs metadata

--- src/app/demo/layout/custom-scene-layout/custom-tab/demo.component.ts
+++ src/app/demo/layout/custom-scene-layout/custom-tab/demo.component.ts
@@ -1,4 +1,4 @@
-import {Component, TemplateRef, Type, ViewChild} from "@angular/core";
+import {Component, Type, ViewChild} from "@angular/core";
 import {PopupEffect, PopupInfo, PopupOptions, PopupService} from "jigsaw/service/popup.service";
 import {CustomTableComponent} from "../custom-table/demo.component";
 import {CustomGraphComponent} from "../custom-graph/demo.component";
@@ -82,6 +82,16 @@ export class CustomTabComponent {
 
     @ViewChild(JigsawTab) tabs: JigsawTab;
 
+    tabsMetaData: ComponentMetaData = {
+        selector: 'j-tab',
+        component: JigsawTab,
+        panes: []
+    };
+
+    getMetaDataByComponent(component: Type<IDynamicInstantiatable>): ComponentMetaData{
+        return this.componentMetaDataList.find(metadata => metadata.component == component);
+    }
+
     public removeTab(index) {
         this.tabs.removeTab(index);
     }
@@ -91,11 +101,16 @@ export class CustomTabComponent {
     }
 
     public addComponentTab(component: Type<IDynamicInstantiatable>) {
-        this.addTab('New tab', component, 'jigsaw')
+        this.addTab('New tab', component, 'jigsaw');
+        this.tabsMetaData.panes.push({
+            tile: 'New tab',
+            content: this.getMetaDataByComponent(component)
+        })
     }
 
     public removeFirstTab(){
         this.removeTab(0);
+        this.tabsMetaData.panes.splice(0, 1);
     }
 
     selectChange(){
