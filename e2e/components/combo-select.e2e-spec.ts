54e30d070cc7a7812e1d2b31530ad358b448c5bd
10222927 <zhu.mingpeng@zte.com.cn>
2018-04-03 17:10:53 +0800

    删除多余的操作，排除CI失败原因

--- e2e/components/combo-select.e2e-spec.ts
+++ e2e/components/combo-select.e2e-spec.ts
@@ -96,8 +96,6 @@ describe('combo-select', () => {
             await browser.get('/combo-select/disable');
             const selectEl = $('jigsaw-combo-select'),
                 button = $$('jigsaw-button');
-            await browser.actions().mouseMove(selectEl).perform();
-            await waitForPresence('jigsaw-tile');
             await expectToExist('jigsaw-tile');
             await button.click();
             await browser.actions().mouseMove(selectEl).perform();
