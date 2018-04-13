ec9dc3f150410c311c809f25a91b1c7ad8f2eb5e
陈旭 <chen.xu8@zte.com.cn>
2018-04-14 00:28:34 +0800

    reverse angle on open/close; simplify demo code

--- src/app/demo/drawer/basic/demo.component.ts
+++ src/app/demo/drawer/basic/demo.component.ts
@@ -5,30 +5,18 @@ import {Component} from "@angular/core";
     styleUrls: ['./demo.component.css']
 })
 export class DrawerBasicDemoComponent {
-    positions = [
-        {label: 'left'},
-        {label: 'top'},
-        {label: 'right'},
-        {label: 'bottom'},
-    ];
-    draweropen:boolean=false;
+    isOpen: boolean = false;
+    selectedPosition = 'left';
+    touched = false;
 
-    selectedPosition = this.positions[0];
+    toggle() {
+        this.isOpen = !this.isOpen;
+        this.touched = true;
+    }
 
-    toggleClick(){
-      this.draweropen=!this.draweropen;
-      console.log("open status from outter drawer is:"+ this.draweropen);
-    }
-    public openChangelog(message:boolean){
-        console.log("open status from inner drawer is:"+ message);
-    }
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
     summary: string = '';
     description: string = '';
-    tags: string[] = [
-        'JigsawDrawer'
-    ];
-
 }
