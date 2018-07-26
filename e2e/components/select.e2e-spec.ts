a9a917b7ec2a351eca4e3d18eddb1093ff9101a1
尤海鹏10210844 <you.haipeng@zte.com.cn>
2018-07-26 17:24:04 +0800

    build CI
    Change-Id: Icfb0a7b4174bc3c8d5a694454ff9dcae3d41583b

--- e2e/components/select.e2e-spec.ts
+++ e2e/components/select.e2e-spec.ts
@@ -13,6 +13,8 @@ describe('select', () => {
         it('should drop down by hover and click option can select', async () => {
             const selectEl = $('#test-select');
 
+            await waitForPresence('#test-select .jigsaw-combo-select-host');
+
             // mouse enter
             mouseMove(selectEl);
             await waitForPresence('.jigsaw-list-option');
