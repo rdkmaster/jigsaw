a5a133f38fd92a1ca2534b418ae7b91dbe1f4704
尤海鹏10210844 <you.haipeng@zte.com.cn>
2018-08-02 17:28:52 +0800

    upload basic style
    Change-Id: I7c541a25fa188930dac7778f8ec202ad65922f69

--- src/jigsaw/component/upload/upload.ts
+++ src/jigsaw/component/upload/upload.ts
@@ -3,6 +3,7 @@ import {AbstractJigsawComponent} from "../common";
 import {JigsawBoxModule} from "../box/index";
 import {JigsawButtonModule} from "../button/button";
 import {HttpClient} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
 
 @Component({
     selector: 'jigsaw-upload, j-upload',
@@ -15,6 +16,10 @@ export class JigsawUpload extends AbstractJigsawComponent {
     constructor(private _http: HttpClient) {
         super();
     }
+
+    public _$uploadMode: 'select' | 'single' | 'multiple' = 'select';
+
+    public _$fileInfoList: {name: string, state: string}[] = [];
 
     @ViewChild('fileInput') fileInput: ElementRef;
 
@@ -29,30 +34,43 @@ export class JigsawUpload extends AbstractJigsawComponent {
     public _$upload() {
         const fileInput = this.fileInput.nativeElement;
         const files = Array.from(fileInput['files']);
-        if(!files || !files.length) {
+        if (!files || !files.length) {
             console.warn('there are no upload files');
             return;
-        } else if(files.length > 10) {
+        } else if (files.length > 10) {
             console.warn('there are too more upload files');
             return;
         }
 
         files.forEach((file: File) => {
+            const fileInfo = {name: file.name, state: 'loading'};
+            this._$fileInfoList.push(fileInfo);
+
             const formData = new FormData();
             formData.append('file', file);
             formData.append("filename", encodeURI(file.name));
             this._http.post('/rdk/service/common/upload', formData, {responseType: 'text'}).subscribe(res => {
                 console.log(res);
+                fileInfo.state = 'success';
+            }, err => {
+                fileInfo.state = 'fail'
             });
-        })
+        });
+
+        if (files.length == 1) {
+            this._$uploadMode = 'single';
+        } else {
+            this._$uploadMode = 'multiple';
+        }
+
     }
 }
 
 @NgModule({
-    imports: [JigsawBoxModule, JigsawButtonModule],
+    imports: [JigsawBoxModule, JigsawButtonModule, CommonModule],
     declarations: [JigsawUpload],
     exports: [JigsawUpload]
 })
-export class JigsawUploadModule{
+export class JigsawUploadModule {
 
 }
