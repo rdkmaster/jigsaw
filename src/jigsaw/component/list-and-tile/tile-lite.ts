044ada5e0b26b03c91af770266e3d32be571ba69
尤海鹏10210844 <you.haipeng@zte.com.cn>
2018-08-20 10:30:29 +0800

    tile lite tab style
    Change-Id: I92874e6a19fad0d96b556863bdf2da7a5f7feb52

--- src/jigsaw/component/list-and-tile/tile-lite.ts
+++ src/jigsaw/component/list-and-tile/tile-lite.ts
@@ -10,9 +10,11 @@ import {AbstractJigsawGroupLiteComponent} from "./group-lite-common";
         <j-tile [(selectedItems)]="selectedItems"
                 [trackItemBy]="trackItemBy"
                 [multipleSelect]="multipleSelect"
+                [tabStyle]="tabStyle"
                 (selectedItemsChange)="_$handleSelectChange($event)">
             <j-tile-option *ngFor="let item of data; trackBy: _$trackByFn" [value]="item"
-                           [width]="optionWidth" [height]="optionHeight" [disabled]="item?.disabled">
+                           [width]="optionWidth" [height]="optionHeight" [disabled]="item?.disabled"
+                           [backgroundStyle]="backgroundStyle">
                 {{item && item[labelField] ? item[labelField] : item}}
             </j-tile-option>
         </j-tile>`,
@@ -30,6 +32,12 @@ export class JigsawTileLite extends AbstractJigsawGroupLiteComponent {
 
     @Input()
     public optionHeight: number | string;
+
+    @Input()
+    public tabStyle: boolean;
+
+    @Input()
+    public backgroundStyle: boolean;
 }
 
 @NgModule({
