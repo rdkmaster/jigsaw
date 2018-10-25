ce05be30e9d4f505bd0f4d3115990d618a82c933
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2018-10-25 13:43:16 +0800

    [新增] input添加password属性
    fixes #774

---
+++ src/app/demo/input/password/demo.component.ts
@@ -0,0 +1,19 @@
+import {Component} from "@angular/core";
+import {JigsawInput} from "jigsaw/component/input/input";
+
+@Component({
+    templateUrl: './demo.component.html',
+    styleUrls: ['./demo.component.css']
+})
+export class InputPasswordComponent {
+
+    // ====================================================================
+    // ignore the following lines, they are not important to this demo
+    // ====================================================================
+    summary: string = '';
+    description: string = '';
+    tags: string[] = [
+        'JigsawInput',
+    ];
+}
+
