ebbcee7f463bd0f83c1cf422285cfad382a6530b
尤海鹏10210844 <you.haipeng@zte.com.cn>
2018-10-26 16:56:55 +0800

    add localPageableArray demo
    Change-Id: Ib6ca5d42892d758e6a439818168bdfaaf8918697

--- src/app/demo/transfer/basic/demo.component.ts
+++ src/app/demo/transfer/basic/demo.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {ArrayCollection, LocalPageableArray} from "../../../../jigsaw/core/data/array-collection";
 import {TableData} from "../../../../jigsaw/core/data/table-data";
+import {Local} from "protractor/built/driverProviders";
 
 @Component({
     templateUrl: './demo.component.html',
@@ -10,13 +11,27 @@ import {TableData} from "../../../../jigsaw/core/data/table-data";
 })
 export class TransferDemoComponent {
     constructor(private _http: HttpClient) {
-        this.data = new LocalPageableArray();
-        this.data.http = _http;
-        this.data.fromAjax('mock-data/countries');
-        this.data.dataReviser = (td: TableData) => TableData.toArray(td);
+        this.data1 = new ArrayCollection();
+        this.data1.http = _http;
+        this.data1.fromAjax('mock-data/countries');
+        this.data1.dataReviser = (td: TableData) => TableData.toArray(td);
+
+        this.data2 = new LocalPageableArray();
+        this.data2.http = _http;
+        this.data2.fromAjax('mock-data/countries');
+        this.data2.dataReviser = (td: TableData) => TableData.toArray(td);
     }
 
-    data;
+    data1: ArrayCollection<any>;
+    data2: LocalPageableArray<any>;
+
+    selectedCountries1: string;
+    selectedCountries2: string;
+
+    handleSelectChange($event, index) {
+        this[`selectedCountries${index}`] = $event.map(item => item.zhName).join(',');
+    }
+
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
