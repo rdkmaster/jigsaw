a1181c0128fb14f4c27dac618c88e8ff411b7056
陈旭 <chen.xu8@zte.com.cn>
2018-10-29 17:15:39 +0800

    [新增] 服务端分页功能支持自定义函数过滤

--- src/app/demo/table/pageable/demo.component.ts
+++ src/app/demo/table/pageable/demo.component.ts
@@ -32,17 +32,22 @@ export class TablePageableDemoComponent {
         }
     ];
 
+    onSearch(reg) {
+        // 这里需要特别注意，filter函数的执行是在服务端，而非在浏览器！
+        // 这里context变量是filter的执行上下文（即filter函数里的this所指向的对象），它将会一起传输给服务端，
+        // 因此这里需要注意控制context的值里只包含有用的数据，以加快前后端通信速度
+        const filter = function(item) {
+            return item[0].match(new RegExp(this.reg, 'g'));
+        };
+        const context = {reg};
+
+        this.pageable.filter(filter, context);
+    }
+
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
     summary: string = '';
     description: string = '';
-    tags: string[] = [
-        'PageableTableData',
-        'JigsawPagination',
-        'JigsawTable.columnDefines',
-        'ColumnDefine',
-        'TableHeader.sortable'
-    ];
 }
 
