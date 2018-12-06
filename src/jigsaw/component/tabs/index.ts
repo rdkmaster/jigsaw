8ceac9a86ada284252decf077b8f2169bb971fc9
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2018-12-06 23:19:27 +0800

    [优化] tab过多时，显示下拉列表供选择

--- src/jigsaw/component/tabs/index.ts
+++ src/jigsaw/component/tabs/index.ts
@@ -8,9 +8,11 @@ import {JigsawTab} from './tab';
 import {JigsawTabPane} from "./tab-pane";
 import {CommonModule} from "@angular/common";
 import {JigsawTabContent, JigsawTabLabel} from "./tab-item";
+import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
+import {JigsawListModule} from "../list-and-tile/list";
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, PerfectScrollbarModule, JigsawListModule],
     exports: [JigsawTab, JigsawTabPane],
     declarations: [JigsawTab, JigsawTabPane, JigsawTabLabel, JigsawTabContent],
     providers: [],
