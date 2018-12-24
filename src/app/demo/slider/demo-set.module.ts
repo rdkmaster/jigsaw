26f30e0ad9b0ab8ecf45e0e6ad704ea318f9e6c7
You Haipeng <you.haipeng@zte.com.cn>
2018-12-24 09:48:19 +0800

    [修复] 修复slider的value为数组时，改变value的元素不能更新视图的问题，fixes#840

--- src/app/demo/slider/demo-set.module.ts
+++ src/app/demo/slider/demo-set.module.ts
@@ -8,6 +8,8 @@ import {SliderBasicDemoModule} from "./basic/demo.module";
 
 import {SliderBasicDemoComponent} from "./basic/demo.component";
 import {SliderVerticalDemoComponent} from "./vertical/demo.component";
+import {SliderUpdateDemoComponent} from "./update/demo.component";
+import {SliderUpdateDemoModule} from "./update/demo.module";
 
 export const routerConfig = [
     {
@@ -15,13 +17,16 @@ export const routerConfig = [
     },
     {
         path: 'vertical', component: SliderVerticalDemoComponent
+    },
+    {
+        path: 'update', component: SliderUpdateDemoComponent
     }
 ];
 
 @NgModule({
     imports: [
         RouterModule.forChild(routerConfig),
-        SliderBasicDemoModule, SliderVerticalDemoModule
+        SliderBasicDemoModule, SliderVerticalDemoModule, SliderUpdateDemoModule
     ]
 })
 export class SliderDemoModule { }
