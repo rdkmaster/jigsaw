c2a099c3ffbd0b112d45937ccda6e260635ac4c6
贺小龙10238397 <he.xiaolong4@zte.com.cn>
2019-01-04 10:15:06 +0800

    多级弹出demo

---
+++ src/app/demo/float/multi-level/demo.component.ts
@@ -0,0 +1,30 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: './demo.component.html',
+    styles: [`
+               .menu{
+                     margin:100px;
+                     border:1px solid #999;
+                     padding: 2px 10px;
+                     border-radius: 3px;
+               }
+                ul {
+                    width: 100px;
+                    line-height: 20px;
+                    border-radius: 6px;
+                    text-indent: 10px;
+                }
+                ul li:hover{
+                    background:#999;
+                    color:#fff;
+                }
+    `]
+})
+export class FloatMultiLevelDemo {
+    // ====================================================================
+    // ignore the following lines, they are not important to this demo
+    // ====================================================================
+    summary: string = '本demo演示了jigsaw-float指令实现多级弹出，当弹出一个区域后，弹出区域再次弹出新的区域';
+    description: string = '';
+}
--- src/app/demo/float/multi-level/demo.module.ts
+++ src/app/demo/float/multi-level/demo.module.ts
@@ -1,16 +1,15 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
-
 import {JigsawFloatModule} from "../../../../jigsaw/directive/float";
-import {FloatPosReviserDemo} from "./demo.component";
+import {FloatMultiLevelDemo} from "./demo.component";
 
 @NgModule({
     imports: [
         CommonModule, JigsawDemoDescriptionModule, JigsawFloatModule
     ],
-    declarations: [FloatPosReviserDemo],
-    exports: [FloatPosReviserDemo]
+    declarations: [FloatMultiLevelDemo],
+    exports: [FloatMultiLevelDemo]
 })
-export class FloatPosReviserModule {
+export class FloatMultiLevelModule {
 }
