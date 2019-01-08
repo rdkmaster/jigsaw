0752e0782bb9f60c34eee70cdb3aa05d9ab7dfa3
陈旭10045812 <chen.xu8@zte.com.cn>
2019-01-08 17:12:27 +0800

    [优化] 面包屑的demo中增加live-demo必要的路由信息，优化live-demo调整逻辑

--- src/app/demo/breadcrumb/router/demo.module.ts
+++ src/app/demo/breadcrumb/router/demo.module.ts
@@ -9,11 +9,36 @@ import {BreadcrumbRouterBuy} from "./buy/buy";
 import {ProductService} from "./product.service";
 import {CommonModule} from "@angular/common";
 
+
+/* #for-live-demo-only#
+const routes = [
+    {
+        path: 'router', component: BreadcrumbRouterDemoComponent,
+        children: [
+            {
+                path: 'list/:typeId', component: BreadcrumbRouterList
+            },
+            {
+                path: 'detail/:id', component: BreadcrumbRouterDetail
+            },
+            {
+                path: 'buy/:id', component: BreadcrumbRouterBuy
+            }
+        ]
+    }
+];
+*/
+
 @NgModule({
-    declarations: [BreadcrumbRouterDemoComponent, BreadcrumbRouterList,
-        BreadcrumbRouterDetail, BreadcrumbRouterBuy],
+    declarations: [
+        BreadcrumbRouterDemoComponent, BreadcrumbRouterList,
+        BreadcrumbRouterDetail, BreadcrumbRouterBuy
+    ],
     exports: [BreadcrumbRouterDemoComponent],
-    imports: [JigsawBreadcrumbModule, JigsawDemoDescriptionModule, RouterModule, CommonModule],
+    imports: [
+        JigsawBreadcrumbModule, JigsawDemoDescriptionModule, CommonModule,
+        RouterModule/* #for-live-demo-only# .forRoot(routes) */
+    ],
     providers: [ProductService]
 })
 export class BreadcrumbRouterDemoModule {
