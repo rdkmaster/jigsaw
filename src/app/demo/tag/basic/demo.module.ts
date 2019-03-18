e73b82ce14765b00ad2f4a4529f16a2c94c8ccea
尤海鹏10210844 <you.haipeng@zte.com.cn>
2019-03-18 14:25:24 +0800

    [新增] 增加tag的显示方法，优化tag的删除操作
    Change-Id: I28ee739592933d4ac585d5dce4383a546d7749a6

--- src/app/demo/tag/basic/demo.module.ts
+++ src/app/demo/tag/basic/demo.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {JigsawTagModule} from "jigsaw/component/tag/tag";
 import {TagBasicDemoComponent} from './demo.component';
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
+import {JigsawButtonModule} from "jigsaw/component/button/button";
 
 @NgModule({
-    imports: [JigsawTagModule, JigsawDemoDescriptionModule],
+    imports: [JigsawTagModule, JigsawDemoDescriptionModule, JigsawButtonModule],
     declarations: [TagBasicDemoComponent],
     exports: [TagBasicDemoComponent]
 })
