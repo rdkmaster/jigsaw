e9654826fe21cbfa366241e8966b0aed4954a31d
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2019-03-23 16:28:44 +0800

    [修复] 修复isActive直接赋值会产生的问题 (#900)
    * 修复isActive直接赋值会产生的问题
    
    * 修改comment
    
    * optimize demo
    
    Change-Id: I5ac2d69793f9a0963f56bce73f2647ec5af1aabb

--- src/app/demo/collapse/basic/demo.module.ts
+++ src/app/demo/collapse/basic/demo.module.ts
@@ -3,11 +3,12 @@ import {JigsawCollapseModule} from "jigsaw/component/collapse/collapse";
 import {JigsawInputModule} from "jigsaw/component/input/input";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {CollapseBasicDemoComponent} from "./demo.component";
+import {JigsawButtonModule} from "jigsaw/component/button/button";
 
 @NgModule({
     declarations: [CollapseBasicDemoComponent],
     exports: [CollapseBasicDemoComponent],
-    imports: [JigsawCollapseModule, JigsawInputModule, JigsawDemoDescriptionModule]
+    imports: [JigsawCollapseModule, JigsawInputModule, JigsawDemoDescriptionModule,JigsawButtonModule]
 })
 export class CollapseBasicDemoModule {
 
