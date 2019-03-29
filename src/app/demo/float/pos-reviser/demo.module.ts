d158b876d7cb0db917b1b5b69a8a2e958b6166ce
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2019-03-29 15:32:35 +0800

    [故障] float指令弹出位置算法修正

--- src/app/demo/float/pos-reviser/demo.module.ts
+++ src/app/demo/float/pos-reviser/demo.module.ts
@@ -2,11 +2,14 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {JigsawFloatModule} from "jigsaw/directive/float";
+import {JigsawMovableModule} from "jigsaw/directive/movable";
+import {JigsawSelectModule} from "jigsaw/component/select/select";
 import {FloatPosReviserDemo} from "./demo.component";
 
 @NgModule({
     imports: [
-        CommonModule, JigsawDemoDescriptionModule, JigsawFloatModule
+        CommonModule, JigsawDemoDescriptionModule, JigsawFloatModule, JigsawMovableModule,
+        JigsawSelectModule
     ],
     declarations: [FloatPosReviserDemo],
     exports: [FloatPosReviserDemo]
