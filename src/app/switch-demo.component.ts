944010041764b016133b8e1b6986eac0e4cd29c7
贺小龙10238397 <he.xiaolong4@zte.com.cn>
2019-05-11 10:17:36 +0800

    ...

--- src/app/switch-demo.component.ts
+++ src/app/switch-demo.component.ts
@@ -4,10 +4,10 @@ import {Component} from "@angular/core";
     template: `
         <div>
             <div>
-            请点击下面的链接，选择要查看pc端还是移动端的demo
+            请点击下面的链接，选择要查看桌面端还是移动端的demo
             </div>
             <div>
-                <a [routerLink]="['/pc']" title="进入pc版本的demo列表">pc</a><a title="进入移动端版本的demo列表" [routerLink]="['/mobile']">mobile</a>
+                <a [routerLink]="['/pc']" title="进入桌面端版本的demo列表">pc</a><a title="进入移动端版本的demo列表" [routerLink]="['/mobile']">mobile</a>
             </div>
         </div>
     `,
