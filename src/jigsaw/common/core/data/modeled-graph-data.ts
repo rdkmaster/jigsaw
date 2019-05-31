56ac39f353bc72a46d8f70dda6d42340bba262e6
尤海鹏10210844 <you.haipeng@zte.com.cn>
2019-05-31 16:40:16 +0800

    ModeledMapGraphData
    Change-Id: I28216ac7fd6f1b6c5f3968d6f35b2009b694beb2

--- src/jigsaw/common/core/data/modeled-graph-data.ts
+++ src/jigsaw/common/core/data/modeled-graph-data.ts
@@ -998,3 +998,133 @@ export class ModeledScatterGraphData extends AbstractModeledGraphData {
         super.refresh();
     }
 }
+
+
+// ------------------------------------------------------------------------------------------------
+// 轮廓图相关数据对象
+export class MapSeries {
+    public dimensionField: string;
+    public dimensions: Dimension[] = [];
+    public usingAllDimensions: boolean = true;
+    public indicators: Indicator[] = [];
+    public model?: any[];
+    public more?: any;
+
+    public name?: string;
+
+    public mapType?: string;
+    public label?: string;
+    public itemStyle?: string;
+    public animation?: string;
+
+    constructor(name?: string) {
+        this.name = name;
+    }
+}
+
+export class BasicModeledMapTemplate extends ModeledRectangularTemplate {
+    getInstance(): EchartOptions {
+        return {
+            title: CommonUtils.extendObjects<EchartTitle>({}, this.title),
+            tooltip: CommonUtils.extendObjects<EchartTooltip>({}, this.tooltip),
+            visualMap: CommonUtils.extendObjects<EchartTooltip>({}, this.visualMap),
+        };
+    }
+
+    title = {
+        x: 'center',
+        textStyle: {},
+        subtextStyle: {}
+    };
+
+    tooltip = {
+        trigger: 'item',
+        formatter: ""
+    };
+
+    visualMap = {};
+
+    seriesItem = {
+        type: 'map',
+        data: null,
+        mapType: ''
+    };
+}
+
+export class ModeledMapGraphData extends AbstractModeledGraphData {
+    constructor(data: GraphDataMatrix = [], header: GraphDataHeader = [], field: GraphDataField = []) {
+        super(data, header, field);
+    }
+
+    public template: ModeledPieTemplate = new BasicModeledMapTemplate();
+    public series: MapSeries[];
+    private _options: EchartOptions;
+
+    get options(): EchartOptions {
+        if (!this._options) {
+            this._options = this.createChartOptions();
+        }
+        return this._options;
+    }
+
+    protected createChartOptions(): EchartOptions {
+        if (!this.series) {
+            return undefined;
+        }
+        const options = this.template.getInstance();
+
+        options.series = this.series
+            .filter(seriesData => {
+                if (!seriesData.dimensionField) {
+                    return false;
+                }
+                if (!seriesData.indicators || seriesData.indicators.length == 0) {
+                    return false;
+                }
+                return seriesData.usingAllDimensions || (seriesData.dimensions && seriesData.dimensions.length > 0);
+            })
+            .map((seriesData, idx) => {
+                const dimensions = this.getRealDimensions(seriesData.dimensionField, seriesData.dimensions, seriesData.usingAllDimensions);
+                const dimIndex = this.getIndex(seriesData.dimensionField);
+                seriesData.indicators.forEach(kpi => kpi.index = this.getIndex(kpi.field));
+                seriesData.indicators.forEach(kpi => kpi.name = kpi.name ? kpi.name : this.header[kpi.index]);
+
+                const seriesItem = CommonUtils.extendObjects<EchartSeriesItem>({}, this.template.seriesItem);
+                if(!dimensions.length) return {};
+
+                // 多维度单指标
+                let records;
+                if (seriesData.usingAllDimensions) {
+                    records = this.data;
+                } else {
+                    records = this.data.filter(row => dimensions.find(d => d.name == row[dimIndex]));
+                }
+                const kpiIndex = seriesData.indicators[0].index;
+                records = records.map(row => [row[dimIndex], row[kpiIndex]]);
+                const indicator: Indicator = CommonUtils.deepCopy(seriesData.indicators[0]);
+                indicator.index = 1;
+                seriesItem.data = this.pruneData(records, 0, dimensions, [indicator])
+                    .map(row => ({name: row[0], value: row[1]}));
+
+                seriesItem.name = seriesData.name ? seriesData.name : 'series' + idx;
+
+                let extendParam = ['label', 'itemStyle', 'animation'];
+
+                extendParam.forEach(param => {
+                    if (CommonUtils.isDefined(seriesData[param])) {
+                        seriesItem[param] = seriesData[param];
+                    }
+                });
+
+                return seriesItem;
+            });
+
+        return options;
+    }
+
+    public refresh(): void {
+        this._options = undefined;
+        super.refresh();
+    }
+}
+
