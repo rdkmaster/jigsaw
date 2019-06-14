ee22a19163def0a5030f5b4246af2f56d5ec458b
尤海鹏10210844 <you.haipeng@zte.com.cn>
2019-06-14 15:36:59 +0800

    [优化] combo-select的height属性改成maxHeight
    Change-Id: Id049511edeb6a0d44a26c706d962c09059afab16

--- src/app/demo/pc/combo-select/height/demo.module.ts
+++ src/app/demo/pc/combo-select/height/demo.module.ts
@@ -3,15 +3,15 @@ import {CommonModule} from "@angular/common";
 import {JigsawTileSelectModule} from "jigsaw/pc-components/list-and-tile/tile";
 import {JigsawComboSelectModule} from "jigsaw/pc-components/combo-select";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
-import {ComboSelectHeightDemo} from "./demo.component";
+import {ComboSelectMaxHeightDemo} from "./demo.component";
 
 @NgModule({
-    declarations: [ComboSelectHeightDemo],
-    exports: [ComboSelectHeightDemo],
+    declarations: [ComboSelectMaxHeightDemo],
+    exports: [ComboSelectMaxHeightDemo],
     imports: [
         JigsawComboSelectModule, JigsawTileSelectModule, CommonModule, JigsawDemoDescriptionModule
     ]
 })
-export class ComboSelectHeightDemoModule {
+export class ComboSelectMaxHeightDemoModule {
 
 }
