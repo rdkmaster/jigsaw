0724415db9ac8191c77a4e7cdde0931d8c4fa622
尤海鹏10210844 <you.haipeng@zte.com.cn>
2019-06-24 15:48:33 +0800

    range time viewChild

--- src/jigsaw/pc-components/range-time/range-time.ts
+++ src/jigsaw/pc-components/range-time/range-time.ts
@@ -34,7 +34,7 @@ export class JigsawRangeTime extends AbstractJigsawComponent implements ControlV
     @Input()
     public valid: boolean = true;
 
-    @ViewChild("timeStart", {static: false}) private _timeStart: JigsawTime;
+    @ViewChild("timeStart", {static: true}) private _timeStart: JigsawTime;
 
     /**
      * @internal
