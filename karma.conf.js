6ce54f9b1ba575643ccf96781d463b7d7abf35c5
尤海鹏10210844 <you.haipeng@zte.com.cn>
2019-07-03 14:28:56 +0800

    optimize build file

--- karma.conf.js
+++ karma.conf.js
@@ -4,37 +4,24 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine', '@angular/cli'],
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
             require('karma-jasmine'),
             //require('karma-chrome-launcher'),
             require('karma-phantomjs-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular/cli/plugins/karma')
+            require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
-        files: [
-            {pattern: './src/test.ts', watched: false}
-        ],
-        preprocessors: {
-            './src/test.ts': ['@angular/cli']
-        },
-        mime: {
-            'text/x-typescript': ['ts', 'tsx']
-        },
         coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, './coverage/jigsaw-app'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true
         },
-        angularCli: {
-            environment: 'dev'
-        },
-        reporters: config.angularCli && config.angularCli.codeCoverage
-            ? ['progress', 'coverage-istanbul']
-            : ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
--- src/typings.d.ts
+++
@@ -1,7 +0,0 @@
-/* SystemJS module definition */
-/*declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}*/
-/*declare let $: any;
-declare let moment: any;*/
