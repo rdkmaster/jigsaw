fb6fda3dc0270c05317e632e9e434ecd212fe050
尤海鹏10210844 <you.haipeng@zte.com.cn>
2019-07-04 15:59:59 +0800

    compatible with ie11

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,11 +18,29 @@
 * BROWSER POLYFILLS
 */
 
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
+/**
+ * Web Animations `@angular/platform-browser/animations`
+ * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
+ * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
+ */
 import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
@@ -30,16 +48,10 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 import 'core-js/es/reflect';
 
 
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
-//import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
-
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
 
 
 /***************************************************************************************************
