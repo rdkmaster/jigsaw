b3522f8b3b627f343b114263030a77a7db946992
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2019-08-13 09:49:16 +0800

    [新增] PageableTableData数据对象将_ajax从private改为protected，新增pagingServerUrl属性用于覆盖全局PagingInfo.pagingServerUrl

--- src/jigsaw/common/core/data/table-data.ts
+++ src/jigsaw/common/core/data/table-data.ts
@@ -372,6 +372,8 @@ export class PageableTableData extends TableData implements IServerSidePageable,
      */
     public sourceRequestOptions: HttpClientOptions;
 
+    public pagingServerUrl: string;
+
     public pagingInfo: PagingInfo = new PagingInfo();
 
     private _filterSubject = new Subject<DataFilterInfo>();
@@ -444,7 +446,7 @@ export class PageableTableData extends TableData implements IServerSidePageable,
         this._ajax();
     }
 
-    private _ajax(): void {
+    protected _ajax(): void {
         if (this._busy) {
             this.ajaxErrorHandler(null);
             return;
@@ -480,7 +482,9 @@ export class PageableTableData extends TableData implements IServerSidePageable,
             options.params = PreparedHttpClientOptions.prepareParams(options.params)
         }
 
-        this.http.request(options.method, PagingInfo.pagingServerUrl, options)
+        const pagingService = this.pagingServerUrl || PagingInfo.pagingServerUrl;
+
+        this.http.request(options.method, pagingService, options)
             .pipe(
                 map(res => this.reviseData(res)),
                 map(data => {
