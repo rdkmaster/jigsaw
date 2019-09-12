9d65d2901cda75b33cb79d93bda836506c33608b
You Haipeng <you.haipeng@zte.com.cn>
2019-09-12 15:37:45 +0800

    [故障] dialog固定高度时，内容太高被撑开，无法产生纵向滚动条

--- src/app/demo/pc/dialog/height/demo.module.ts
+++ src/app/demo/pc/dialog/height/demo.module.ts
@@ -5,11 +5,12 @@ import {JigsawInputModule} from "jigsaw/pc-components/input/input";
 import {PopupService} from "jigsaw/common/service/popup.service";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {DialogHeightDemo} from "./demo.component";
+import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
 
 @NgModule({
     declarations: [DialogHeightDemo],
     exports: [DialogHeightDemo],
-    imports: [JigsawDialogModule, JigsawButtonModule, JigsawInputModule, JigsawDemoDescriptionModule],
+    imports: [JigsawDialogModule, JigsawButtonModule, JigsawInputModule, JigsawDemoDescriptionModule, PerfectScrollbarModule],
     providers: [PopupService]
 })
 export class DialogHeightDemoModule {
