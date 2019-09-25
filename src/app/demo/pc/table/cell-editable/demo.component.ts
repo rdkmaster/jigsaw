9c2f508b935c023da475df35b871575e7f25bd0d
张晨光10198633 <zhang.chenguang@zte.com.cn>
2019-09-25 11:12:15 +0800

    修改

--- src/app/demo/pc/table/cell-editable/demo.component.ts
+++ src/app/demo/pc/table/cell-editable/demo.component.ts
@@ -21,11 +21,15 @@ export class MyTableCell extends TableCellRendererBase {
 @Component({
     template: `
         <jigsaw-input #input [(value)]="cellData" [clearable]="false"
-                      (blur)="dispatchChangeEvent(cellData)" [placeholder]="initData.placeholder"></jigsaw-input>`
+                      (blur)="dispatchChangeEvent(cellData)" [placeholder]="_$placeholder"></jigsaw-input>`
 })
 export class MyTableCellEditor extends TableCellRendererBase implements AfterViewInit {
 
     @ViewChild(JigsawInput, {static: false}) input: JigsawInput;
+
+    public get _$placeholder() {
+        return this.initData && this.initData.placeholder ? this.initData.placeholder : '';
+    }
 
     ngAfterViewInit() {
         this.input.focus();
@@ -53,11 +57,10 @@ export class TableSetCellEditableDemoComponent {
             group: true,
             cell: {
                 renderer: MyTableCell,
-                editorRendererInitData:{placeholder:"please input the position"},
                 editable: true,
-                editorRenderer: MyTableCellEditor
+                editorRenderer: MyTableCellEditor,
+                editorRendererInitData: {placeholder: ""}
             }
-
         }];
 
     changeMsg: string;
