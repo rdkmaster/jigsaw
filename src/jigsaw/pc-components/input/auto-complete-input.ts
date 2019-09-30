07062abd193dab2a470b63f58c2cbead15f27e17
张晨光10198633 <zhang.chenguang@zte.com.cn>
2019-09-30 15:16:50 +0800

    auto-input不再继承input，改为包装input

--- src/jigsaw/pc-components/input/auto-complete-input.ts
+++ src/jigsaw/pc-components/input/auto-complete-input.ts
@@ -10,14 +10,16 @@ import {
     TemplateRef,
     ViewChild,
     Output,
-    EventEmitter, AfterViewInit
+    EventEmitter,
+    AfterViewInit
 } from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {FormsModule, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR} from "@angular/forms";
 import {JigsawInput, JigsawInputModule} from "./input";
 import {PopupInfo, PopupOptions, PopupPositionValue, PopupService} from "../../common/service/popup.service";
 import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
 import {CommonUtils} from "../../common/core/utils/common-utils";
+import {AbstractJigsawComponent, IJigsawFormControl} from "../../common/common";
 
 export class DropDownValue {
     constructor(data = null) {
@@ -52,7 +54,31 @@ export class DropDownValue {
         {provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => JigsawAutoCompleteInput), multi: true},
     ]
 })
-export class JigsawAutoCompleteInput extends JigsawInput implements OnDestroy, AfterViewInit {
+export class JigsawAutoCompleteInput extends AbstractJigsawComponent implements OnDestroy, AfterViewInit, IJigsawFormControl, ControlValueAccessor {
+
+    /**
+     * 输入框的placeholder
+     *
+     */
+    @Input() public placeholder = '';
+    /**
+     * 在文本框里的文本非空时，是否显示快速清除按钮，默认为显示。用户单击了清除按钮时，文本框里的文本立即被清空。
+     *
+     */
+    @Input() public clearable: boolean = true;
+
+    /**
+     * 设置按钮不可交互状态的开关，为true则不可交互，为false则可交互。
+     *
+     */
+    @Input() public disabled: boolean = false;
+
+    /**
+     * 当用户输入非法时，组件给予样式上的提示，以提升易用性，常常和表单配合使用。
+     *
+     */
+    @Input() public valid: boolean = true;
+
     /**
      * @internal
      */
@@ -96,9 +122,6 @@ export class JigsawAutoCompleteInput extends JigsawInput implements OnDestroy, A
         [...this._bakData] = this._$data;
     }
 
-    @Input()
-    public valid: boolean = true;
-
     /**
      * 用于控制在输入框获得焦点后是否自动执行过滤
      *
@@ -124,10 +147,10 @@ export class JigsawAutoCompleteInput extends JigsawInput implements OnDestroy, A
     @Output('textSelect')
     public textSelectEvent = new EventEmitter<Event>();
 
-    constructor(protected _render2: Renderer2,
-                protected _elementRef: ElementRef,
+    constructor(private _render2: Renderer2,
+                private _elementRef: ElementRef,
                 private _popupService: PopupService) {
-        super(_render2, _elementRef);
+        super();
     }
 
     ngAfterViewInit() {
@@ -164,19 +187,106 @@ export class JigsawAutoCompleteInput extends JigsawInput implements OnDestroy, A
         });
     }
 
+    private _value: string = ''; //input表单值
+
+    /**
+     * 文本框中当前的文本
+     *
+     */
+    @Input()
+    public get value(): string {
+        return this._value;
+    }
+
+    public set value(newValue: string) {
+        if (CommonUtils.isUndefined(newValue) || this._value === newValue) {
+            return;
+        }
+        this._value = newValue;
+        this.valueChange.emit(this._value);
+        this._propagateChange(this._value);
+    }
+
+    private _propagateChange: any = () => {
+    };
+
+    public writeValue(value: any): void {
+        if (CommonUtils.isUndefined(value)) {
+            return;
+        }
+        this._value = value.toString();
+    }
+
+    public registerOnChange(fn: any): void {
+        this._propagateChange = fn;
+    }
+
+    public registerOnTouched(fn: any): void {
+    }
+
+    /**
+     * 当文本框中的文本发生变化时，组件会发出此事件。
+     *
+     * $demo = input/value-change
+     */
+    @Output()
+    public valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+    @Output('focus')
+    private _focusEmitter: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+
+    public focus() {
+        this._input.focus();
+    }
+
     /**
      * @internal
      */
     public _$handleFocus(event: FocusEvent) {
         this._getFilteredDropDownData(this.filterOnFocus);
         this._showDropdownList(event);
+        this._focusEmitter.emit(event);
     }
+
+    @Output('blur')
+    private _blurEmitter: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
+
+    private _focused: boolean = false;
+
+    /**
+     * 获取文本框是否有焦点
+     *
+     * $demo = input/focus
+     */
+    public get focused(): boolean {
+        return this._focused;
+    }
+
+    /**
+     * 在单击了清除文本按钮时，是否让文本失去焦点，默认为失去焦点。
+     * 一般来说，是否失去焦点关系不大，但是在一些特定场合，却有很大关系。`JigsawTable`的默认单元格编辑渲染就是`JigsawInput`组件，
+     * 按照`JigsawTable`的交互逻辑，单元格编辑器一旦失去焦点，就必须退回到单元格显示渲染器。
+     * 在这个情况下，用户单击了清除文本按钮时就不能让输入框失去焦点。参考[这个demo]($demo=table/update-column-define)的职位列
+     *
+     * $demo = table/update-column-define
+     */
+    @Input()
+    public blurOnClear: boolean = true;
 
     /**
      * @internal
      */
     public _$handleBlur(event: FocusEvent) {
-        super._$handleBlur(event);
+        this._focused = false;
+        if (this.blurOnClear) {
+            this._blurEmitter.emit(event);
+        } else {
+            this.callLater(() => {
+                if (!this._focused) {
+                    this._blurEmitter.emit(event);
+                }
+            }, 150);
+        }
         this._closeListPopup();
     }
 
