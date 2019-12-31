b6703832a6b921646c62ba41014462bda9dc8826
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2019-12-31 17:57:33 +0800

    [优化] 调整图片保存按钮弹出位置

--- src/jigsaw/pc-components/graph/graph-download.directive.ts
+++ src/jigsaw/pc-components/graph/graph-download.directive.ts
@@ -51,7 +51,7 @@ export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase impleme
             hideEffect: PopupEffect.fadeOut,
             pos: this._elementRef,
             posOffset: {
-                left: this._elementRef.nativeElement.offsetWidth - 10
+                left: this._elementRef.nativeElement.offsetWidth - 20
             },
             posType: PopupPositionType.absolute,
             size: {width: 15},
