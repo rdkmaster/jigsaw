65f74cdc68673bb39b94b67bb4e038dfdc2563e7
尤海鹏10210844 <you.haipeng@zte.com.cn>
2020-01-06 20:49:47 +0800

    with combo demo

--- src/app/demo/pc/range-time/week-day-start/demo.component.ts
+++ src/app/demo/pc/range-time/week-day-start/demo.component.ts
@@ -1,4 +1,6 @@
 import {AfterContentInit, Component} from "@angular/core";
+import {TimeGr, TimeService} from "../../../../../jigsaw/common/service/time.service";
+import {ArrayCollection} from "../../../../../jigsaw/common/core/data/array-collection";
 
 
 @Component({
@@ -27,6 +29,29 @@ export class RangeTimeWeekDayStartComponent implements AfterContentInit {
 
     weekStart;
     weekDayStart;
+
+    beginDate2: any = 'now-20d';
+    endDate2: any = 'now';
+    rangeTimeComboValue: any = new ArrayCollection([
+        {label: this.beginDate2, closable: false},
+        {label: this.endDate2, closable: false}
+    ]);
+
+    handleRangeDateChange() {
+        this.rangeTimeComboValue[0].label = `${this.beginDate2.year}-${this.beginDate2.week}`;
+        this.rangeTimeComboValue[1].label = `${this.endDate2.year}-${this.endDate2.week}`;
+        this.rangeTimeComboValue.refresh();
+    }
+
+    ngOnInit() {
+        let tempRangeTime = [];
+        this.rangeTimeComboValue.forEach((item, index) => {
+            let timeStr = TimeService.convertValue(item.label, TimeGr.week);
+            timeStr = TimeService.getWeekYear(timeStr) + '-' + TimeService.getWeekOfYear(timeStr);
+            tempRangeTime[index] = {label: timeStr, closable: false};
+        });
+        this.rangeTimeComboValue = tempRangeTime;
+    }
 
     ngAfterContentInit() {
         this.weekStart = [this.weekStartList[0]];
--- src/app/demo/pc/range-time/week-day-start/demo.module.ts
+++ src/app/demo/pc/range-time/week-day-start/demo.module.ts
@@ -4,9 +4,10 @@ import {JigsawTileSelectModule} from "jigsaw/pc-components/list-and-tile/tile";
 import {RangeTimeWeekDayStartComponent} from './demo.component';
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {JigsawRangeTimeModule} from "../../../../../jigsaw/pc-components/range-time";
+import {JigsawComboSelectModule} from "../../../../../jigsaw/pc-components/combo-select";
 
 @NgModule({
-    imports: [CommonModule, JigsawRangeTimeModule, JigsawTileSelectModule, JigsawDemoDescriptionModule],
+    imports: [CommonModule, JigsawRangeTimeModule, JigsawTileSelectModule, JigsawComboSelectModule, JigsawDemoDescriptionModule],
     declarations: [RangeTimeWeekDayStartComponent],
     exports: [RangeTimeWeekDayStartComponent]
 })
