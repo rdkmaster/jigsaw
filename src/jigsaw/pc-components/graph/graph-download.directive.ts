d01812a592041654dd72b0e13b01d66ae7e2223a
张晨光10198633 <zhang.chenguang@zte.com.cn>
2020-01-06 19:57:52 +0800

    修改按钮的弹出方式

--- src/jigsaw/pc-components/graph/graph-download.directive.ts
+++ src/jigsaw/pc-components/graph/graph-download.directive.ts
@@ -1,26 +1,12 @@
 import {
-    Component,
     Directive,
     ElementRef,
-    EventEmitter,
     HostListener,
     Input,
     OnDestroy,
-    QueryList,
-    Renderer2,
-    ContentChildren
+    Renderer2
 } from "@angular/core";
-import {
-    ButtonInfo,
-    IPopupable,
-    PopupEffect,
-    PopupInfo,
-    PopupOptions,
-    PopupPositionType,
-    PopupService
-} from "../../common/service/popup.service";
-import {AbstractJigsawComponent, AbstractJigsawViewBase} from "../../common/common";
-import {JigsawGraph} from "./graph";
+import {AbstractJigsawViewBase} from "../../common/common";
 import {CommonUtils} from "../../common/core/utils/common-utils";
 import * as FileSaver from 'file-saver';
 import * as JSZip from 'jszip/dist/jszip.min';
@@ -32,8 +18,7 @@ declare const echarts: any;
 })
 export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase implements OnDestroy {
     constructor(private _renderer: Renderer2,
-                private _elementRef: ElementRef,
-                private _popupService: PopupService) {
+                private _elementRef: ElementRef) {
         super();
     }
 
@@ -54,21 +39,7 @@ export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase impleme
         this._addRollOutDenouncesTimer();
     }
 
-    private _popupInfo: PopupInfo;
-
-    private _getNonModelOptions(): PopupOptions {
-        return {
-            modal: false,
-            showEffect: PopupEffect.fadeIn,
-            hideEffect: PopupEffect.fadeOut,
-            pos: this._elementRef,
-            posOffset: {
-                left: this._elementRef.nativeElement.offsetWidth - 20
-            },
-            posType: PopupPositionType.absolute,
-            size: {width: 15},
-        };
-    }
+    private addButton: HTMLElement;
 
     @Input()
     public jigsawGraphDownloadExportFileName: string = "graphs.zip";
@@ -77,10 +48,11 @@ export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase impleme
     public jigsawGraphDownloadTooltip: string = '';
 
     private _closePopup() {
-        if (this._popupInfo) {
-            this._popupInfo.dispose();
-            this._popupInfo = null;
+        if (!this.addButton) {
+            return;
         }
+        this._elementRef.nativeElement.removeChild(this.addButton);
+        this.addButton = null;
         this._closeAllListener();
     }
 
@@ -97,26 +69,28 @@ export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase impleme
 
     private _addRollInDenouncesTimer() {
         this._rollInDenouncesTimer = this.callLater(() => {
-            if (this._popupInfo) {
+            if (this.addButton) {
                 return;
             }
-            this._popupInfo = this._popupService.popup(JigsawGraphDownloadButton, this._getNonModelOptions(), {
-                jigsawGraphDownloadExportFileName: this.jigsawGraphDownloadExportFileName,
-                jigsawGraphDownloadTooltip: this.jigsawGraphDownloadTooltip,
-                dom: this._elementRef
+
+            this.addButton = window.document.createElement('div');
+            this.addButton.innerHTML = `
+                <div style="width: 100%;height: 0;position: relative;z-index: 999">
+                    <div style="width: 15px;height: 20px;background: #41addc;color: #ffffff;text-align: center;cursor: pointer;position: absolute;right: 10px"
+                        title="${this.jigsawGraphDownloadTooltip}">
+                    <span class="fa fa-download"></span>
+                    </div>
+                <div>`;
+            this.addButton.children[0].addEventListener('click', () => {
+                this._$download();
             });
-
-            if (!this._popupInfo || !this._popupInfo.element || !this._popupInfo.instance) {
-                console.error('unable to popup drop down, unknown error!');
-                return;
-            }
-
+            this._elementRef.nativeElement.insertAdjacentElement("afterbegin", this.addButton);
             this._closeAllListener();
             this._removeMouseOverHandler = this._renderer.listen(
-                this._popupInfo.element, 'mouseenter',
+                this.addButton, 'mouseenter',
                 () => this.clearCallLater(this._rollOutDenouncesTimer));
             this._removeMouseOutHandler = this._renderer.listen(
-                this._popupInfo.element, 'mouseleave', () => {
+                this.addButton, 'mouseleave', () => {
                     this._addRollOutDenouncesTimer();
                 });
         }, 100);
@@ -128,35 +102,15 @@ export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase impleme
         }, 400);
     }
 
-
     ngOnDestroy() {
         super.ngOnDestroy();
         this._closePopup();
     }
-}
-
-@Component({
-    selector: 'jigsaw-graph-download-button, j-upload-download-button',
-    template: `
-        <div style="width: 15px;height: 10px"
-             style="background: #41addc;color: #ffffff;text-align: center;cursor: pointer"
-             (click)="_$download()"
-             [title]="initData.jigsawGraphDownloadTooltip">
-            <span class="fa fa-download"></span>
-        </div>
-    `
-})
-export class JigsawGraphDownloadButton extends AbstractJigsawComponent implements IPopupable {
-    public answer: EventEmitter<ButtonInfo>;
-    @Input()
-    public initData: any;
-
-    [index: string]: any;
 
     private _graphsInDom: any[] = [];
 
     private _getGraphs() {
-        this._getChildren(this.initData.dom.nativeElement);
+        this._getChildren(this._elementRef.nativeElement);
     }
 
     private _getChildren(element: any) {
@@ -213,11 +167,11 @@ export class JigsawGraphDownloadButton extends AbstractJigsawComponent implement
         base64Codes.forEach(base64Code => {
             zip.file(`${base64Code.title}.png`, base64Code.base64, {base64: true});
         });
-        const jigsawGraphDownloadExportFileName = !!this.initData.jigsawGraphDownloadExportFileName.match(/(.+)\.(.+)/g) ?
-            this.initData.jigsawGraphDownloadExportFileName : `${this.initData.jigsawGraphDownloadExportFileName}.zip`;
+        const jigsawGraphDownloadExportFileName = !!this.jigsawGraphDownloadExportFileName.match(/(.+)\.(.+)/g) ?
+            this.jigsawGraphDownloadExportFileName : `${this.jigsawGraphDownloadExportFileName}.zip`;
         zip.generateAsync({type: "blob"}).then(content => {
             FileSaver.saveAs(content, `${jigsawGraphDownloadExportFileName}`);
         });
+        this._graphsInDom = [];
     }
 }
-
