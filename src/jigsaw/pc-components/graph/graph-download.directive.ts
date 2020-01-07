1d60ced4da9c1db189ab148cee7d4ff2df9265d2
陈旭 <chen.xu8@zte.com.cn>
2020-01-07 04:58:56 -0600

    [优化] 修改图形下载指令按钮的弹出方式，以解决在宿主有滚动条情况下的位置异常

--- src/jigsaw/pc-components/graph/graph-download.directive.ts
+++ src/jigsaw/pc-components/graph/graph-download.directive.ts
@@ -1,14 +1,11 @@
-import {Component, Directive, ElementRef, EventEmitter, HostListener, Input, OnDestroy, Renderer2} from "@angular/core";
 import {
-    ButtonInfo,
-    IPopupable,
-    PopupEffect,
-    PopupInfo,
-    PopupOptions,
-    PopupPositionType,
-    PopupService
-} from "../../common/service/popup.service";
-import {AbstractJigsawComponent, AbstractJigsawViewBase} from "../../common/common";
+    Directive,
+    ElementRef,
+    HostListener,
+    Input,
+    OnDestroy
+} from "@angular/core";
+import {AbstractJigsawViewBase} from "../../common/common";
 import {CommonUtils} from "../../common/core/utils/common-utils";
 import * as FileSaver from 'file-saver';
 import * as JSZip from 'jszip/dist/jszip.min';
@@ -19,44 +16,24 @@ declare const echarts: any;
     selector: '[j-graph-download], [jigsaw-graph-download], [jigsawGraphDownload]'
 })
 export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase implements OnDestroy {
-    constructor(private _renderer: Renderer2,
-                private _elementRef: ElementRef,
-                private _popupService: PopupService) {
+    constructor(private _elementRef: ElementRef) {
         super();
     }
 
-    private _removeMouseOverHandler: Function;
-    private _removeMouseOutHandler: Function;
     private _rollOutDenouncesTimer: any = null;
-    private _rollInDenouncesTimer: any = null;
 
     @HostListener('mouseenter', ['$event'])
     onMouseEnter() {
         this.clearCallLater(this._rollOutDenouncesTimer);
-        this._addRollInDenouncesTimer();
+        this._addSaveButton();
     }
 
     @HostListener('mouseleave', ['$event'])
     onMouseLeave() {
-        this.clearCallLater(this._rollInDenouncesTimer);
         this._addRollOutDenouncesTimer();
     }
 
-    private _popupInfo: PopupInfo;
-
-    private _getNonModelOptions(): PopupOptions {
-        return {
-            modal: false,
-            showEffect: PopupEffect.fadeIn,
-            hideEffect: PopupEffect.fadeOut,
-            pos: this._elementRef,
-            posOffset: {
-                left: this._elementRef.nativeElement.offsetWidth - 20
-            },
-            posType: PopupPositionType.absolute,
-            size: {width: 15},
-        };
-    }
+    private _addButton: HTMLElement;
 
     @Input()
     public jigsawGraphDownloadExportFileName: string = "graphs.zip";
@@ -65,49 +42,30 @@ export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase impleme
     public jigsawGraphDownloadTooltip: string = '';
 
     private _closePopup() {
-        if (this._popupInfo) {
-            this._popupInfo.dispose();
-            this._popupInfo = null;
+        if (!this._addButton) {
+            return;
         }
-        this._closeAllListener();
+        this._elementRef.nativeElement.removeChild(this._addButton);
+        this._addButton = null;
     }
 
-    private _closeAllListener() {
-        if (this._removeMouseOverHandler) {
-            this._removeMouseOverHandler();
-            this._removeMouseOverHandler = null;
+    private _addSaveButton() {
+        if (this._addButton) {
+            return;
         }
-        if (this._removeMouseOutHandler) {
-            this._removeMouseOutHandler();
-            this._removeMouseOutHandler = null;
-        }
-    }
 
-    private _addRollInDenouncesTimer() {
-        this._rollInDenouncesTimer = this.callLater(() => {
-            if (this._popupInfo) {
-                return;
-            }
-            this._popupInfo = this._popupService.popup(JigsawGraphDownloadButton, this._getNonModelOptions(), {
-                jigsawGraphDownloadExportFileName: this.jigsawGraphDownloadExportFileName,
-                jigsawGraphDownloadTooltip: this.jigsawGraphDownloadTooltip,
-                dom: this._elementRef
-            });
-
-            if (!this._popupInfo || !this._popupInfo.element || !this._popupInfo.instance) {
-                console.error('unable to popup drop down, unknown error!');
-                return;
-            }
-
-            this._closeAllListener();
-            this._removeMouseOverHandler = this._renderer.listen(
-                this._popupInfo.element, 'mouseenter',
-                () => this.clearCallLater(this._rollOutDenouncesTimer));
-            this._removeMouseOutHandler = this._renderer.listen(
-                this._popupInfo.element, 'mouseleave', () => {
-                    this._addRollOutDenouncesTimer();
-                });
-        }, 100);
+        this._addButton = window.document.createElement('div');
+        this._addButton.innerHTML = `
+                <div style="width: 100%;height: 0;position: relative;z-index: 999">
+                    <div style="width: 15px;height: 20px;background: #41addc;color: #ffffff;text-align: center;cursor: pointer;position: absolute;right: 10px;top:8px"
+                        title="${this.jigsawGraphDownloadTooltip}">
+                    <span class="fa fa-download"></span>
+                    </div>
+                <div>`;
+        this._addButton.children[0].addEventListener('click', () => {
+            this._$download();
+        });
+        this._elementRef.nativeElement.insertAdjacentElement("afterbegin", this._addButton);
     }
 
     private _addRollOutDenouncesTimer() {
@@ -116,35 +74,15 @@ export class JigsawGraphDownloadDirective extends AbstractJigsawViewBase impleme
         }, 400);
     }
 
-
     ngOnDestroy() {
         super.ngOnDestroy();
         this._closePopup();
     }
-}
-
-@Component({
-    selector: 'jigsaw-graph-download-button, j-upload-download-button',
-    template: `
-        <div style="width: 15px;height: 10px"
-             style="background: #41addc;color: #ffffff;text-align: center;cursor: pointer"
-             (click)="_$download()"
-             [title]="initData.jigsawGraphDownloadTooltip">
-            <span class="fa fa-download"></span>
-        </div>
-    `
-})
-export class JigsawGraphDownloadButton extends AbstractJigsawComponent implements IPopupable {
-    public answer: EventEmitter<ButtonInfo>;
-    @Input()
-    public initData: any;
-
-    [index: string]: any;
 
     private _graphsInDom: any[] = [];
 
     private _getGraphs() {
-        this._getChildren(this.initData.dom.nativeElement);
+        this._getChildren(this._elementRef.nativeElement);
     }
 
     private _getChildren(element: any) {
@@ -201,11 +139,11 @@ export class JigsawGraphDownloadButton extends AbstractJigsawComponent implement
         base64Codes.forEach(base64Code => {
             zip.file(`${base64Code.title}.png`, base64Code.base64, {base64: true});
         });
-        const jigsawGraphDownloadExportFileName = !!this.initData.jigsawGraphDownloadExportFileName.match(/(.+)\.(.+)/g) ?
-            this.initData.jigsawGraphDownloadExportFileName : `${this.initData.jigsawGraphDownloadExportFileName}.zip`;
+        const jigsawGraphDownloadExportFileName = !!this.jigsawGraphDownloadExportFileName.match(/(.+)\.(.+)/g) ?
+            this.jigsawGraphDownloadExportFileName : `${this.jigsawGraphDownloadExportFileName}.zip`;
         zip.generateAsync({type: "blob"}).then(content => {
             FileSaver.saveAs(content, `${jigsawGraphDownloadExportFileName}`);
         });
+        this._graphsInDom = [];
     }
 }
-
