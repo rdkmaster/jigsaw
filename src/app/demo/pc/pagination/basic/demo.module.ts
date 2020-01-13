146fc9fb9378e8014f7cb66b71d908d64f711953
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2020-01-13 09:23:45 +0800

    [新增] 分页控件的simple模式支持搜索框

--- src/app/demo/pc/pagination/basic/demo.module.ts
+++ src/app/demo/pc/pagination/basic/demo.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from "@angular/core";
 import {JigsawPaginationModule} from "jigsaw/pc-components/pagination/pagination";
+import {JigsawSwitchModule} from "jigsaw/pc-components/switch/index";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {PaginationBasicDemoComponent} from "./demo.component";
 @NgModule({
     declarations: [PaginationBasicDemoComponent],
     exports: [PaginationBasicDemoComponent],
-    imports: [JigsawPaginationModule, JigsawDemoDescriptionModule]
+    imports: [JigsawPaginationModule, JigsawSwitchModule, JigsawDemoDescriptionModule]
 })
 export class PaginationBasicDemoModule{
 
