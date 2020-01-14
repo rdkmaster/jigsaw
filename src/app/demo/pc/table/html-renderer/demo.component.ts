8983a7542fd403e100301b56f2b857a3b90f3915
You Haipeng <you.haipeng@zte.com.cn>
2020-01-14 15:14:11 +0800

    [新增] table在columnDefine中的表头现在可以设置html作为表头的渲染器

--- src/app/demo/pc/table/html-renderer/demo.component.ts
+++ src/app/demo/pc/table/html-renderer/demo.component.ts
@@ -33,6 +33,18 @@ export class TableHtmlRendererDemoComponent {
             cell: {
                 renderer: 'html'
             }
+        },
+        {
+            target: 'salary',
+            header: {
+                renderer: 'html',
+                data: (data, col) => `${data.header[col]} 
+                    <select onchange="changeUnit()" id="unitSelect">
+                        <option value="$">$</option>
+                        <option value="￥">￥</option>
+                    </select>`,
+                innerHtmlContext: this
+            }
         }
     ];
 
@@ -54,6 +66,17 @@ export class TableHtmlRendererDemoComponent {
 
     onClick(row: number, type: number) {
         console.log(`Row number ${row} testing ${type == 0 ? this.property1 : this.property2}`);
+    }
+
+    changeUnit() {
+        let unitSelect: any = document.getElementById("unitSelect");
+        let curUnit = unitSelect.options[unitSelect.selectedIndex].value;
+        console.log(curUnit);
+        this.tableData.data = this.tableData.data.concat().map(row => {
+            row[3] = curUnit == '$' ? row[3]*7 : Math.round(row[3]/7);
+            return row;
+        });
+        this.tableData.refresh();
     }
 
     // ====================================================================
