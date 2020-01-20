dcbcfd55d82fda96de680a74b5d6f78b1f2394a4
zcg8633 <44219901+zcg8633@users.noreply.github.com>
2020-01-20 14:46:48 +0800

    select组件高度不够时跟combo一样撑出滚动条

--- src/app/demo/pc/select/multiple/demo.component.ts
+++ src/app/demo/pc/select/multiple/demo.component.ts
@@ -8,14 +8,18 @@ export class SelectMultipleDemoComponent {
     selectedCity: any;
     cityList = new ArrayCollection([
         {label: "北京"},
-        {label: "这是一个上海的标题，非常长，非常长，非常长，非常长，非常长。。。。"},
         {label: "南京"},
         {label: "深圳"},
         {label: "长沙"},
+        {label: "这是一个上海的标题，非常长，非常长，非常长，非常长，非常长。。。。"},
         {label: "西安"}
     ]);
 
     selectedCityName: string;
+    enableMaxWidth: boolean = true;
+    maxWidth: number = 300;
+    enableMaxHeight: boolean = true;
+    maxHeight: number = 32;
 
     selectChange(selectedItems: any[]) {
         this.selectedCityName = selectedItems.reduce((str, item, index) => {
--- src/app/demo/pc/select/multiple/demo.module.ts
+++ src/app/demo/pc/select/multiple/demo.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {JigsawSelectModule} from "jigsaw/pc-components/select/select";
+import {JigsawCheckBoxModule} from "jigsaw/pc-components/checkbox/index";
+import {JigsawNumericInputModule} from "jigsaw/pc-components/input/numeric-input";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {SelectMultipleDemoComponent} from './demo.component';
 
 @NgModule({
-    imports: [JigsawSelectModule, JigsawDemoDescriptionModule],
+    imports: [JigsawSelectModule, JigsawCheckBoxModule, JigsawNumericInputModule, JigsawDemoDescriptionModule],
     declarations: [SelectMultipleDemoComponent],
     exports: [SelectMultipleDemoComponent]
 })
