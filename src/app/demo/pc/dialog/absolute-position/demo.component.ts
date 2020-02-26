eaaf6f87f1fbe23e52d029db5256f32e89d9311a
陈旭 <chen.xu8@zte.com.cn>
2020-02-26 09:12:40 +0800

    [新增] 弹出选项新增可以配置 top / right / left / bottom 四个绝对位置

---
+++ src/app/demo/pc/dialog/absolute-position/demo.component.ts
@@ -0,0 +1,62 @@
+import {Component, TemplateRef, ViewChild, ViewEncapsulation} from "@angular/core";
+import {PopupEffect, PopupInfo, PopupOptions, PopupService} from "jigsaw/common/service/popup.service";
+
+@Component({
+    templateUrl: './demo.component.html',
+    styleUrls: ['./demo.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class DialogAbsolutePositionDemoComponent {
+    public _$offset: number = 16;
+    public _$isModal: boolean = false;
+    public _$popPositionTypes: string[] = ['top', 'left', 'right', 'bottom', 'center'];
+    public _$selectedPositionType: any = 'center';
+    public _$popupInfo: PopupInfo;
+
+    @ViewChild('tpDialog')
+    private _tpDialog: TemplateRef<any>;
+
+    constructor(private _popupService: PopupService) {
+    }
+
+    public _$onModalChange() {
+        if (!this._$popupInfo) {
+            return;
+        }
+        this._$closeDialog();
+        this._$popupComponentDialog();
+    }
+
+    public _$closeDialog() {
+        if (this._$popupInfo) {
+            this._$popupInfo.dispose();
+            this._$popupInfo = null;
+        }
+    }
+
+    public _$popupComponentDialog() {
+        const options: PopupOptions = {
+            modal: !!this._$isModal,
+            showEffect: PopupEffect.bubbleIn,
+            hideEffect: PopupEffect.bubbleOut,
+            posOffset: {}
+        };
+        if (this._$selectedPositionType != 'center') {
+            options.pos = this._$selectedPositionType;
+            options.posOffset[this._$selectedPositionType] = this._$offset;
+        }
+
+        if (this._$popupInfo) {
+            this._popupService.setPosition(options, this._$popupInfo.element);
+        } else {
+            this._$popupInfo = this._popupService.popup(this._tpDialog, options);
+        }
+    }
+
+    // ====================================================================
+    // ignore the following lines, they are not important to this demo
+    // ====================================================================
+    summary: string = '这个demo介绍了弹出对话框四个方位弹出的绝对位置：靠上、靠左、靠右、考下';
+    description: string = '[这里详细介绍了`PopupService`，请仔细阅读](/#/pc/popup/introduce)。';
+}
+
