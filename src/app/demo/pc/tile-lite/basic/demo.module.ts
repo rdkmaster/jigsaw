4b4913538bb5b2095727dbe21a0796c7cacbb4f8
张晨光10198633 <zhang.chenguang@zte.com.cn>
2020-03-19 09:54:41 +0800

    [优化]tile-lite添加showBoder属性

--- src/app/demo/pc/tile-lite/basic/demo.module.ts
+++ src/app/demo/pc/tile-lite/basic/demo.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {TileLiteBasicDemoComponent} from './demo.component';
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {JigsawTileLiteModule} from "jigsaw/pc-components/list-and-tile/tile-lite";
+import {JigsawButtonModule} from "jigsaw/pc-components/button/button";
 
 @NgModule({
-    imports: [JigsawTileLiteModule, JigsawDemoDescriptionModule],
+    imports: [JigsawTileLiteModule, JigsawDemoDescriptionModule, JigsawButtonModule],
     declarations: [TileLiteBasicDemoComponent],
     exports: [TileLiteBasicDemoComponent]
 })
