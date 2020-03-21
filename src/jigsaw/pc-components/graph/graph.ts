2b72d5f009ecf1a886af154f736dd3f7ab942f9a
You Haipeng <you.haipeng@zte.com.cn>
2020-03-21 16:32:56 +0630

    [修复] 修复window获取不到echarts对象的破坏性，fixes #1111

--- src/jigsaw/pc-components/graph/graph.ts
+++ src/jigsaw/pc-components/graph/graph.ts
@@ -24,6 +24,8 @@ import {JigsawTheme} from "../../common/core/theming/theme";
 
 import echarts from "echarts";
 
+window['echarts'] = window['echarts'] ? window['echarts'] : echarts;
+
 @Component({
     selector: 'jigsaw-graph, j-graph',
     templateUrl: 'graph.html',
