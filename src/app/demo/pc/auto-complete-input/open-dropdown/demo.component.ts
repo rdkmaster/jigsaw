16dac4e0f9f7ac4bd69c1ef6e48d5d70aaf017aa
陈旭10045812 <chen.xu8@zte.com.cn>
2020-05-13 16:40:56 +0800

    [新增] float指令暴露openFloat和closeFloat方法，auto-complete-input支持编程方式开关提示列表

---
+++ src/app/demo/pc/auto-complete-input/open-dropdown/demo.component.ts
@@ -0,0 +1,31 @@
+import {Component, ViewChild} from '@angular/core';
+import {DropDownValue, JigsawAutoCompleteInput} from "jigsaw/pc-components/input/auto-complete-input";
+
+@Component({
+    templateUrl: './demo.component.html'
+})
+
+export class AutoCompleteInputOpenDropdownDemoComponent {
+    @ViewChild('input', {static: false})
+    public autoInput: JigsawAutoCompleteInput;
+
+    public _$dropdownData: string[] | DropDownValue[] = [{
+        category: '事件与数据',
+        items: ['发送事件到事件总线', '更新变量']
+    }, {
+        category: '动画',
+        items: ['隐藏/显示元素', '滚动页面']
+    }, {
+        category: '弹出',
+        items: ['对话框', '提醒', '警示', '等待弹出关闭']
+    }, {
+        category: '高级',
+        items: ['自定义代码块']
+    }];
+
+    // ====================================================================
+    // ignore the following lines, they are not important to this demo
+    // ====================================================================
+    summary: string = '这个DEMO详细演示如何通过编程方式打开下拉提示列表的功能';
+    description: string = '';
+}
