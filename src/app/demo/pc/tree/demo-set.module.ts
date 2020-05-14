60859ff598ae499a3964f37a5f7cc1a7ff4da452
陈旭 <chen.xu8@zte.com.cn>
2020-05-14 14:53:03 +0800

    [新增] SimpleTreeData支持xml作为数据源格式

--- src/app/demo/pc/tree/demo-set.module.ts
+++ src/app/demo/pc/tree/demo-set.module.ts
@@ -4,13 +4,14 @@ import {TreeBasicDemoModule} from "./basic/demo.module";
 import {TreeAsyncDemoModule} from "./async/demo.module";
 import {TreeAjaxDataDemoModule} from "./data-from-ajax/demo.module";
 import {TreeEditableDemoModule} from "./editable/demo.module";
-
 import {ZtreeDemoComponent} from "./basic/demo.component";
 import {ZtreeDemoDataFromAjaxComponent} from "./data-from-ajax/demo.component";
 import {ZtreeDemoEditableComponent} from "./editable/demo.component";
 import {ZtreeAsynDemoComponent} from "./async/demo.component";
 import {ZtreeCustomSettingCallbackDemoComponent} from "./custom-settings-callback/demo.component";
 import {TreeCustomSettingCallbackDemoModule} from "./custom-settings-callback/demo.module";
+import {ZtreeXMLDataDemoComponent} from "./xml-data/demo.component";
+import {TreeXMLDataDemoModule} from "./xml-data/demo.module";
 
 export const routerConfig = [
     {
@@ -28,12 +29,16 @@ export const routerConfig = [
     {
         path: 'custom-settings-callback', component: ZtreeCustomSettingCallbackDemoComponent
     },
+    {
+        path: 'xml-data', component: ZtreeXMLDataDemoComponent
+    },
 ];
 
 @NgModule({
     imports: [
         RouterModule.forChild(routerConfig),
-        TreeAsyncDemoModule, TreeBasicDemoModule, TreeAjaxDataDemoModule, TreeEditableDemoModule,TreeCustomSettingCallbackDemoModule
+        TreeAsyncDemoModule, TreeBasicDemoModule, TreeAjaxDataDemoModule, TreeEditableDemoModule,
+        TreeCustomSettingCallbackDemoModule, TreeXMLDataDemoModule
     ],
     exports: [
     ],
