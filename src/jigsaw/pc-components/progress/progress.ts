cc1b6d0d79d9beea64f5d142416918543f70b78e
尤海鹏10210844 <you.haipeng@zte.com.cn>
2020-05-28 17:20:23 +0800

    status
    Change-Id: Ib312a69ae69ec199f1843a5127bc57833b0d606e

--- src/jigsaw/pc-components/progress/progress.ts
+++ src/jigsaw/pc-components/progress/progress.ts
@@ -7,9 +7,10 @@ import {CommonModule} from '@angular/common';
     templateUrl: './progress.html',
     host: {
         '[class.jigsaw-progress]': 'true',
-        '[class.jigsaw-progress-primary]': 'colorType == "primary"',
-        '[class.jigsaw-progress-warning]': 'colorType == "warning"',
-        '[class.jigsaw-progress-error]': 'colorType == "error" || colorType == "danger"',
+        '[class.jigsaw-progress-processing]': 'status == "processing"',
+        '[class.jigsaw-progress-block]': 'status == "block"',
+        '[class.jigsaw-progress-error]': 'status == "error"',
+        '[class.jigsaw-progress-success]': 'status == "success"',
         '[class.jigsaw-progress-default]': 'preSize == "default"',
         '[class.jigsaw-progress-small]': 'preSize == "small"',
         '[class.jigsaw-progress-large]': 'preSize == "large"',
@@ -28,7 +29,7 @@ export class JigsawProgress extends AbstractJigsawComponent {
     @Input()
     public labelPosition: 'left' | 'right' | 'top' |'followLeft' | 'followRight' = 'right';
 
-    @Input() public colorType: 'primary' | 'warning' | 'error' | 'danger' = 'primary';
+    @Input() public status: 'processing' | 'block' | 'error' | 'success' = 'processing';
 
     @Input() public preSize: 'default' | 'small' | 'large' = 'default';
 
