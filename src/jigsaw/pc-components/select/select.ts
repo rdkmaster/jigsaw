4c7c892b7f43d0c91a4d2eb9e81b9ca09b63d482
陈旭 <chen.xu8@zte.com.cn>
2020-05-28 20:36:55 +0800

    [故障] select设置多选和初始值时，页面首次加载显示异常的问题

--- src/jigsaw/pc-components/select/select.ts
+++ src/jigsaw/pc-components/select/select.ts
@@ -1,5 +1,5 @@
 import {
-    NgModule, Component, Input, forwardRef, Output, EventEmitter, ViewChild
+    NgModule, Component, Input, forwardRef, Output, EventEmitter, ViewChild, OnInit
 } from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {AbstractJigsawComponent} from "../../common/common";
@@ -33,7 +33,8 @@ import {CommonUtils} from "../../common/core/utils/common-utils";
         {provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => JigsawSelect), multi: true},
     ]
 })
-export class JigsawSelect extends AbstractJigsawComponent implements ControlValueAccessor {
+export class JigsawSelect extends AbstractJigsawComponent implements ControlValueAccessor, OnInit {
+
     @Input()
     public valid: boolean = true;
 
@@ -178,10 +179,14 @@ export class JigsawSelect extends AbstractJigsawComponent implements ControlValu
     }
 
     public set value(newValue: any) {
-        if (this._value != newValue) {
-            this._propagateChange(newValue);
+        if (this._value == newValue) {
+            return;
         }
-        this.writeValue(newValue);
+        this._propagateChange(newValue);
+        this._value = newValue;
+        if (this.initialized) {
+            this.writeValue(newValue);
+        }
     }
 
     /**
@@ -236,10 +241,6 @@ export class JigsawSelect extends AbstractJigsawComponent implements ControlValu
     };
 
     public writeValue(value: any): void {
-        if (this._value == value) {
-            return;
-        }
-        this._value = value;
         if (CommonUtils.isDefined(value)) {
             this._$selectedItems = this.multipleSelect ? value : [value];
         } else {
@@ -255,6 +256,14 @@ export class JigsawSelect extends AbstractJigsawComponent implements ControlValu
     }
 
     public registerOnTouched(fn: any): void {
+    }
+
+    ngOnInit() {
+        super.ngOnInit();
+        // 设置默认选中的初始值
+        if (CommonUtils.isDefined(this.value)) {
+            this.writeValue(this.value);
+        }
     }
 }
 
