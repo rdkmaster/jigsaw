d31f8f7031bbf04f47bed8a59c1d5ef7ba42282b
陈旭 <chen.xu8@zte.com.cn>
2020-05-29 17:14:56 +0800

    在jigsaw演示网站内部直接打开demo源码，避免由于网络原因导致无法看到代码

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {RouterModule} from "@angular/router";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {JigsawRootModule} from "jigsaw/pc-components/root/root";
+import {
+    JigsawBoxModule, JigsawFloatModule, JigsawListLiteModule, JigsawRootModule, JigsawTreeExtModule
+} from "jigsaw/public_api";
 import {AppComponent} from './app.component';
 import {AjaxInterceptor} from './app.interceptor';
 import {routerConfig} from "./router-config";
 import {PCDemoListComponent} from "./pc-demo-list.component";
 import {MobileDemoListComponent} from "./mobile-demo-list.component";
 import {SwitchDemoComponent} from "./switch-demo.component";
+import {DemoCodeComponent} from "./demo-code.component";
 
 @NgModule({
     declarations: [
-        AppComponent, PCDemoListComponent, MobileDemoListComponent, SwitchDemoComponent
+        AppComponent, PCDemoListComponent, MobileDemoListComponent, SwitchDemoComponent, DemoCodeComponent
     ],
     imports: [
         BrowserModule, BrowserAnimationsModule, HttpClientModule,
@@ -23,9 +26,12 @@ import {SwitchDemoComponent} from "./switch-demo.component";
             {path: '', component: SwitchDemoComponent},
             {path: 'pc', component: PCDemoListComponent},
             {path: 'mobile', component: MobileDemoListComponent},
+            {path: 'demo-code', component: DemoCodeComponent, children: [
+                    {path: '**', component: DemoCodeComponent}
+                ]},
             {path: '**', redirectTo: '/'}
         ], {useHash: true}),
-        JigsawRootModule
+        JigsawRootModule, JigsawFloatModule, JigsawListLiteModule, JigsawBoxModule, JigsawTreeExtModule
     ],
     providers: [
         {
