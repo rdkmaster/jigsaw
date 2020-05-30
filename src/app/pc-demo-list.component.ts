870d65c5467f741a95737211319af92eab58af85
尤海鹏10210844 <you.haipeng@zte.com.cn>
2020-05-30 17:36:30 +0800

    fix demo bug
    Change-Id: I7ebc642ce318fef062555988fe11208cd77b0090

--- src/app/pc-demo-list.component.ts
+++ src/app/pc-demo-list.component.ts
@@ -128,7 +128,7 @@ export class PCDemoListComponent implements OnInit {
 
     ngOnInit(): void {
         const stored: string[] = JSON.parse(localStorage.getItem('jigsaw-demo-show-list'));
-        this.selectedItems = this.routes.filter(item => !item.hidden && stored.indexOf(item.path) != -1);
+        this.selectedItems = this.routes.filter(item => !item.hidden && stored && stored.indexOf(item.path) != -1);
         if (this.selectedItems.length == this.routes.length) {
             // 全显示表示这是第一次打开此页面
             this.selectedItems = null;
