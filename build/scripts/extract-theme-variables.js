c52be2603cef788dc715993c2fecb84f3586cdc4
陈旭10045812 <chen.xu8@zte.com.cn>
2020-06-02 09:52:34 +0800

    ...

--- build/scripts/extract-theme-variables.js
+++ build/scripts/extract-theme-variables.js
@@ -5,18 +5,19 @@ const fs = require('fs');
 const file = `d:/Codes/jigsaw/src/jigsaw/pc-components/theming/prebuilt/zte-purple.scss`;
 const scssSource = fs.readFileSync(file).toString()//.replace(/^\s*@import\s+.+$/mg, '');
 
-let spySource = '';
-scssSource.replace(/^\$(.+?)\s*:\s*(.+?);?$/gm, (found, varName, value) => {
-    value = value.replace(/\s*\/\/.*/, '');
-    spySource += `.a { ${varName}: ${value} }\n`;
-});
+// let spySource = '';
+// scssSource.replace(/^\$(.+?)\s*:\s*(.+?);?$/gm, (found, varName, value) => {
+//     value = value.replace(/\s*\/\/.*/, '');
+//     value = value.replace(/\/\*[\s\S]*?\*\//, '');
+//     spySource += `.a { ${varName}: ${value} }\n`;
+// });
 
 // console.log(spySource)
 
 const result = sass.renderSync({
-    data: scssSource + '\n' + spySource,
+    data: scssSource,
     importer: (url, prev) => {
-        let path = `d:/Codes/jigsaw/src/jigsaw/pc-components/theming/prebuilt/${url}.scss`;
+        let path = `src/jigsaw/pc-components/theming/prebuilt/${url}.scss`;
         if (prev != 'stdin') {
             url = url.replace(/(.+)\.scss$/i, '$1');
             prev = prev.replace(/(.+\/).+/, '$1');
@@ -28,10 +29,21 @@ const result = sass.renderSync({
         }
         console.log(path);
         return {
-            file: path,
-            contents: fs.readFileSync(path).toString()
+            file: path, contents: addSpy(path)
         }
     }
 });
 
+function addSpy(path) {
+    const source = fs.readFileSync(path).toString();
+    let spySource = '';
+    return source.replace(/^\$(.+?)\s*:\s*(.+?);?$/gm, (found, varName, value) => {
+            value = value.replace(/\s*\/\/.*/, '');
+            value = value.replace(/\/\*[\s\S]*?\*\//, '');
+            spySource += `.css-spy-mark { ${varName}: ${value} }\n`;
+        });
+    return source + '\n' + spySource;
+}
+
 // console.log(result.css.toString());
+fs.writeFileSync('d:/temp/22/aa.css', result.css.toString())
