60db78296a6101d72e8f7f518c90bd1befa020b4
陈旭 <chen.xu8@zte.com.cn>
2020-06-18 19:36:42 +0800

    将PopupService里的几个私有静态变量移到InternalUtils里去

--- src/jigsaw/mobile-components/root/root.ts
+++ src/jigsaw/mobile-components/root/root.ts
@@ -1,10 +1,6 @@
 import {Component, NgModule, NgZone, Renderer2, ViewContainerRef} from "@angular/core";
 import {PopupService} from "../../common/service/popup.service";
-import {JigsawBlock, JigsawBlockModule} from "../../common/components/block/block";
-import {
-    JigsawBallLoading, JigsawBubbleLoading, JigsawFontLoading, JigsawLoading,
-    JigsawLoadingModule
-} from "../../common/components/loading/loading";
+import {InternalUtils} from "../../common/core/utils/internal-utils";
 
 @Component({
     selector: 'jigsaw-mobile-root, jm-root',
@@ -13,17 +9,15 @@ import {
 export class JigsawRoot {
     constructor(viewContainerRef: ViewContainerRef, renderer: Renderer2, zone: NgZone,
                 ps:PopupService /* do not remove this line, need for global PopupService instantiate! */) {
-        PopupService._viewContainerRef = viewContainerRef;
-        PopupService._renderer = renderer;
+        InternalUtils.viewContainerRef = viewContainerRef;
+        InternalUtils.renderer = renderer;
     }
 }
 
 @NgModule({
     declarations: [JigsawRoot],
     exports: [JigsawRoot],
-    imports: [
-        JigsawBlockModule, JigsawLoadingModule
-    ],
+    imports: [],
     providers: [PopupService]
 })
 export class JigsawMobileRootModule {
