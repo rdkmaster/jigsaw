16a510ccd9b705dd81e470a44ee02530330668d0
尤海鹏10210844 <you.haipeng@zte.com.cn>
2020-07-27 19:56:09 +0800

    [故障] 修改date-time-select不支持同时修改多个属性
    Change-Id: I95b9a5f343290cecb2627ff0e7a30aa63211255f

--- src/app/demo/pc/date-time-picker/date-time-select/demo.module.ts
+++ src/app/demo/pc/date-time-picker/date-time-select/demo.module.ts
@@ -1,13 +1,19 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {JigsawButtonBarModule, JigsawDateTimeSelectModule, JigsawRadioModule, JigsawSwitchModule} from "jigsaw/public_api";
+import {
+    JigsawButtonBarModule,
+    JigsawDateTimeSelectModule,
+    JigsawRadioModule,
+    JigsawSelectModule,
+    JigsawSwitchModule
+} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {DateTimeSelectDemoComponent} from "./demo.component";
 
 @NgModule({
     declarations: [DateTimeSelectDemoComponent],
     exports: [ DateTimeSelectDemoComponent ],
-    imports: [CommonModule, JigsawDemoDescriptionModule, JigsawDateTimeSelectModule, JigsawButtonBarModule, JigsawRadioModule, JigsawSwitchModule]
+    imports: [CommonModule, JigsawDemoDescriptionModule, JigsawDateTimeSelectModule, JigsawButtonBarModule, JigsawRadioModule, JigsawSwitchModule, JigsawSelectModule]
 })
 export class DateTimeSelectDemoModule{
 
