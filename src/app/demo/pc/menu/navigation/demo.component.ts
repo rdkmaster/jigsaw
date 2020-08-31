3b5d3de9ec6610062b2ede2af3ca6b1fea484f86
ZhouQing <42016535+10229428@users.noreply.github.com>
2020-08-31 10:15:31 +0800

    [故障] 导航菜单多次更新数据无效的问题

--- src/app/demo/pc/menu/navigation/demo.component.ts
+++ src/app/demo/pc/menu/navigation/demo.component.ts
@@ -12,23 +12,12 @@ import {SimpleTreeData} from "jigsaw/public_api";
     `]
 })
 export class NavigationMenuNavDemo {
-
     public data1: SimpleTreeData = new SimpleTreeData();
     public data2: SimpleTreeData = new SimpleTreeData();
     public data3: SimpleTreeData = new SimpleTreeData();
 
     constructor() {
-        const xmlData = `
-            <node>
-                <node label="当前告警" icon="fa fa-bell-o" selected="true"></node>
-                <node label="历史告警" icon="fa fa-bell"></node>
-                <node label="通知" icon="fa fa-bullhorn"></node>
-                <node label="告警设置" icon="fa fa-cog"></node>
-            </node>
-        `;
-        this.data1.fromXML(xmlData);
-        this.data2.fromXML(xmlData);
-        this.data3.fromXML(`
+        this.data1.fromXML(`
             <node>
                 <node label="当前告警" icon="fa fa-bell-o" isActive="true" selected="true">
                     <node label="告警监控" selected="true"></node>
@@ -44,6 +33,37 @@ export class NavigationMenuNavDemo {
                     <node label="通知监控"></node>
                 </node>
                 <node label="告警设置" icon="fa fa-cog"></node>
+            </node>
+        `);
+        const xmlData = `
+            <node>
+                <node label="当前告警" icon="fa fa-bell-o" selected="true"></node>
+                <node label="历史告警" icon="fa fa-bell"></node>
+                <node label="通知" icon="fa fa-bullhorn"></node>
+                <node label="告警设置" icon="fa fa-cog"></node>
+            </node>
+        `;
+        this.data2.fromXML(xmlData);
+        this.data3.fromXML(xmlData);
+    }
+
+    updateMenu() {
+        this.data1.fromXML(`
+            <node>
+                <node label="一级菜单1" icon="fa fa-bell-o" isActive="true" selected="true">
+                    <node label="二级菜单1" selected="true"></node>
+                    <node label="二级菜单2"></node>
+                    <node label="二级菜单3"></node>
+                    <node label="二级菜单4"></node>
+                    <node label="二级菜单5"></node>
+                </node>
+                <node label="一级菜单2" icon="fa fa-bell">
+                    <node label="二级菜单"></node>
+                </node>
+                <node label="一级菜单3" icon="fa fa-bullhorn">
+                    <node label="二级菜单"></node>
+                </node>
+                <node label="一级菜单4" icon="fa fa-cog"></node>
             </node>
         `);
     }
