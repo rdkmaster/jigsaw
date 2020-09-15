fc80ab03fdbc0cf04b7029c978270edde0da565e
陈旭10045812 <chen.xu8@zte.com.cn>
2020-09-15 11:31:57 +0800

    ...

--- src/app/demo/pc/notification/basic/demo.module.ts
+++ src/app/demo/pc/notification/basic/demo.module.ts
@@ -1,16 +1,12 @@
 import {NgModule} from "@angular/core";
-import {
-    JigsawButtonModule, JigsawNotificationModule, PopupService, JigsawInputModule,
-    JigsawRadioModule, JigsawSliderModule
-} from "jigsaw/public_api";
+import {JigsawButtonModule, JigsawNotificationModule, PopupService} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {NotificationBasicDemoComponent} from "./demo.component";
 
 @NgModule({
     declarations: [NotificationBasicDemoComponent],
     imports: [
-        JigsawButtonModule, JigsawNotificationModule, JigsawInputModule,
-        JigsawRadioModule, JigsawSliderModule, JigsawDemoDescriptionModule
+        JigsawNotificationModule, JigsawDemoDescriptionModule, JigsawButtonModule
     ],
     providers: [PopupService],
     exports: [NotificationBasicDemoComponent]
