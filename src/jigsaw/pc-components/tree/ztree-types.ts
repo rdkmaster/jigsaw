fcc2dc19821c22b452ca1c25a3e34b8d6d065aa5
陈旭10045812 <chen.xu8@zte.com.cn>
2020-09-15 15:15:21 +0800

    ...

--- src/jigsaw/pc-components/tree/ztree-types.ts
+++ src/jigsaw/pc-components/tree/ztree-types.ts
@@ -19,10 +19,10 @@ export class ZTreeSettingView {
     autoCancelSelected?: boolean;
     dblClickExpand?: boolean;
     expandSpeed?: ZTreeSettingViewSpeedLevel;
-    fontCss?: object;
+    fontCss?: (id: string, node: any) => boolean | object;
     nameIsHTML?: boolean;
     selectedMulti?: boolean;
-    showIcon?: object;
+    showIcon?: (id: string, node: any) => boolean | boolean;
     showLine?: boolean;
     showTitle?: boolean;
     txtSelectedEnable?: boolean;
