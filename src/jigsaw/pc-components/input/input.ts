faf4bba3cb3932eeb980ac99e05bf48c8deac156
陈旭10045812 <chen.xu8@zte.com.cn>
2021-01-12 15:11:44 +0800

    当input的password属性为true的时候，给自动增加一个autocomplete属性，并且值设置为“new-password”

--- src/jigsaw/pc-components/input/input.ts
+++ src/jigsaw/pc-components/input/input.ts
@@ -81,9 +81,11 @@ export abstract class JigsawInputBase extends AbstractJigsawComponent implements
 
     /**
      * 文本框中当前的文本
+     *
+     * @NoMarkForCheckRequired
+     *
      * $demo = input/valid
      */
-    @RequireMarkForCheck()
     @Input()
     public get value(): string {
         return this._value;
@@ -265,6 +267,11 @@ export class JigsawInput extends JigsawInputBase {
     @Input()
     public get type(): string {
         return this.password ? "password" : "text";
+    }
+
+    @Input()
+    public get autocomplete():string{
+        return this.password ? "new-password" : "off" ;
     }
 
     @ViewChild('input')
