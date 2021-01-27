14bca1f3d705506fc2d4860ac2dec2662e916ef4
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-01-27 17:50:25 +0800

    add tab renderer demo
    Change-Id: I500948b16325a82c40e31ee2741df9526d158936

--- src/app/demo/pc/tab/api/demo.component.ts
+++ src/app/demo/pc/tab/api/demo.component.ts
@@ -1,10 +1,10 @@
-import {Component, ViewChild} from '@angular/core';
-import {JigsawTab} from "jigsaw/public_api";
-import {TabContentDefine} from "./tabContent/tabContent";
+import { Component, ViewChild } from "@angular/core";
+import { JigsawTab, JigsawEditableTabTitleRenderer } from "jigsaw/public_api";
+import { TabContentDefine } from "./tabContent/tabContent";
 
 @Component({
-    templateUrl: './demo.component.html',
-    styleUrls: ['./demo.component.css']
+    templateUrl: "./demo.component.html",
+    styleUrls: ["./demo.component.css"]
 })
 export class DynamicTabDemoComponent {
     @ViewChild(JigsawTab) tabs: JigsawTab;
@@ -22,38 +22,42 @@ export class DynamicTabDemoComponent {
     }
 
     public addTab(tabTitle, tabContent, initData?: Object) {
-        this.tabs.addTab(tabTitle, tabContent, initData)
+        this.tabs.addTab(tabTitle, tabContent, initData);
     }
 
     public addTabWidthTemplateTitle(tabTitle, tabContent) {
         this.addTab(tabTitle, tabContent, {
             userInfo: {
-                username: 'Jerry',
-                email: 'ddd@qq.com',
-                phoneNumber: '1347559375',
-                address: '南京市雨花区软件大道1号'
+                username: "Jerry",
+                email: "ddd@qq.com",
+                phoneNumber: "1347559375",
+                address: "南京市雨花区软件大道1号"
             }
-        })
+        });
     }
 
     public addTabWidthStringTitle(tabContent) {
-        this.addTab('template tab', tabContent, {
+        this.addTab("template tab", tabContent, {
             userInfo: {
-                username: 'Martin',
-                email: 'fff@163.com',
-                phoneNumber: '1733994499',
-                address: '南京市雨花区花神大道6号'
+                username: "Martin",
+                email: "fff@163.com",
+                phoneNumber: "1733994499",
+                address: "南京市雨花区花神大道6号"
             }
-        })
+        });
+    }
+
+    public addTabWithEditableTitle(tabContent) {
+        this.addTab(JigsawEditableTabTitleRenderer, tabContent);
     }
 
     public addComponentTab() {
-        this.addTab('component tab', TabContentDefine, 'jigsaw')
+        this.addTab("component tab", TabContentDefine, "jigsaw");
     }
 
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = '';
-    description: string = '';
+    summary: string = "";
+    description: string = "";
 }
