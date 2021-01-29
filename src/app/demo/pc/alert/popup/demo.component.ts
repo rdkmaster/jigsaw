3a2a8cc3947272ac8c050944f3da8f50032efb4a
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-01-29 15:53:11 +0800

    fix compile error
    Change-Id: I1bcf3df49002d5c5fe5b1a3835f286ddd1faeb37

--- src/app/demo/pc/alert/popup/demo.component.ts
+++ src/app/demo/pc/alert/popup/demo.component.ts
@@ -10,28 +10,28 @@ export class AlertPopupDemoComponent {
 
     commonInfoAlert() {
         this.answer = 'waiting for an answer';
-        JigsawInfoAlert.show('this is a great info alert!', answer => {
+        JigsawInfoAlert.show('this is a great info alert!', '', answer => {
             this.answer = answer ? 'great! your answer is: ' + answer.label : 'you closed the alert with the close button';
         });
     }
 
     commonWarningAlert() {
         this.answer = 'waiting for an answer';
-        JigsawWarningAlert.show('this is a great warning alert!', answer => {
+        JigsawWarningAlert.show('this is a great warning alert!', '', answer => {
             this.answer = answer ? 'great! your answer is: ' + answer.label : 'you closed the alert with the close button';
         });
     }
 
     commonErrorAlert() {
         this.answer = 'waiting for an answer';
-        JigsawErrorAlert.show('this is a great error alert!', answer => {
+        JigsawErrorAlert.show('this is a great error alert!', '', answer => {
             this.answer = answer ? 'great! your answer is: ' + answer.label : 'you closed the alert with the close button';
         }, null, null, false);
     }
 
     commonConfirmAlert() {
         this.answer = 'waiting for an answer';
-        JigsawConfirmAlert.show('Jigsaw is great, do you agree?', answer => {
+        JigsawConfirmAlert.show('Jigsaw is great, do you agree?', '', answer => {
                 this.answer = answer ? 'great! your answer is: ' + answer.label : 'you closed the alert with the close button';
             },
             /* custom your own buttons*/
