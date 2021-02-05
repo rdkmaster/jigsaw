0bcd91eb41c00dd936a428104ed49ffb2ffef2d4
陈旭 <chen.xu8@zte.com.cn>
2021-02-05 03:22:35 +0000

    ...

--- src/app/demo/pc/badge/mask/demo.component.ts
+++ src/app/demo/pc/badge/mask/demo.component.ts
@@ -9,6 +9,6 @@ export class BadgeMaskDemoComponent {
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = '这个DEMO详细演示了`jigsaw-badge`指令显示角标的用法';
+    summary: string = '这个DEMO详细演示了`jigsaw-badge`指令角标加背景色的用法和效果，注意区分深浅色';
     description: string = '';
 }
