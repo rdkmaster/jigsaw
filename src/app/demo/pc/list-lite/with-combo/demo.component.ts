22323e4a63bff9871c20d521b01e2bcc60bfa938
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2021-02-08 19:13:26 +0800

    [修复] list-lite组件的图标与文本未垂直居中

--- src/app/demo/pc/list-lite/with-combo/demo.component.ts
+++ src/app/demo/pc/list-lite/with-combo/demo.component.ts
@@ -8,35 +8,35 @@ import {ArrayCollection, GroupOptionValue, JigsawListLite} from "jigsaw/public_a
 export class ListLiteWithComboDemoComponent {
     goodsList: GroupOptionValue[] = [
         {
-            logo: 'e187',
+            icon: 'iconfont iconfont-e187',
             name: 'bicycle',
             desc: 'A bicycle, also called a cycle or bike, is a human-powered, pedal-driven, single-track vehicle, having two wheels attached to a frame, one behind the other.'
         },
         JigsawListLite.SEPARATOR,
         {
-            logo: 'e2e7',
+            icon: 'iconfont iconfont-e2e7',
             name: 'camera',
             desc: 'A camera is an optical instrument for recording or capturing images, which may be stored locally, transmitted to another location, or both.'
         },
         {
-            logo: 'e18a',
+            icon: 'iconfont iconfont-e18a',
             name: 'car',
             desc: 'A car (or automobile) is a wheeled motor vehicle used for transportation.',
             disabled: true
         },
         {
-            logo: 'e534',
+            icon: 'iconfont iconfont-e534',
             name: 'football',
             desc: 'Football is a family of team sports that involve, to varying degrees, kicking a ball with the foot to score a goal. '
         },
         JigsawListLite.SEPARATOR,
         {
-            logo: 'e565',
+            icon: 'iconfont iconfont-e565',
             name: 'book',
             desc: 'A book is a set of sheets of paper, parchment, or similar materials that are fastened together to hinge at one side.'
         },
         {
-            logo: 'e6ca',
+            icon: 'iconfont iconfont-e6ca',
             name: 'puzzle-piece',
             desc: 'A puzzle is a game, problem, or toy that tests a person\'s ingenuity or knowledge.'
         },
@@ -44,7 +44,7 @@ export class ListLiteWithComboDemoComponent {
 
     selectedItems6 = new ArrayCollection([
         {
-            logo: 'e565',
+            icon: 'iconfont iconfont-e565',
             name: 'book',
             desc: 'A book is a set of sheets of paper, parchment, or similar materials that are fastened together to hinge at one side.'
         },
--- src/jigsaw/pc-components/list-and-tile/list-lite.ts
+++ src/jigsaw/pc-components/list-and-tile/list-lite.ts
@@ -39,8 +39,11 @@ import {CallbackRemoval} from "../../common/core/utils/common-utils";
                                [disabled]="item?.disabled">
                     <p j-title class="jigsaw-list-lite-text" title="{{item && item[labelField] ? item[labelField] : item}}">
                         <span *ngIf="item?.icon" class="{{item?.icon}}" style="font-size:12px; margin-right:4px"></span>
-                        {{item && item[labelField] ? item[labelField] : item}}</p>
-                    <span j-sub-title *ngIf="item?.suffixIcon" class="{{item?.suffixIcon}}" style="font-size:12px; margin-top:3px"></span>
+                        <span class="jigsaw-list-lite-text-content">{{item && item[labelField] ? item[labelField] : item}}</span>
+                    </p>
+                    <p j-sub-title *ngIf="item?.suffixIcon">
+                        <i class="{{item?.suffixIcon}}"></i>
+                    </p>
                 </j-list-option>
             </j-list>
         </div>
