5c3be5caaded95bd796d639a593d53a8d88e9bc0
陈旭10045812 <chen.xu8@zte.com.cn>
2021-03-04 16:48:23 +0800

    [优化] time-section支持“每天”选项的实现优化，代码优化

--- src/app/demo/pc/time-section/demo-set.module.ts
+++ src/app/demo/pc/time-section/demo-set.module.ts
@@ -2,10 +2,15 @@ import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
 import {TimeSectionBasicDemoComponent} from "./basic/demo.component";
 import {TimeSectionBasicDemoModule} from "./basic/demo.module";
+import {TimeSectionOptionsDemoComponent} from "./time-section-options/demo.component";
+import {TimeSectionOptionsDemoModule} from "./time-section-options/demo.module";
 
 export const routerConfig = [
     {
         path: 'basic', component: TimeSectionBasicDemoComponent
+    },
+    {
+        path: 'time-section-options', component: TimeSectionOptionsDemoComponent
     },
 ];
 
@@ -13,6 +18,7 @@ export const routerConfig = [
     imports: [
         RouterModule.forChild(routerConfig),
         TimeSectionBasicDemoModule,
+        TimeSectionOptionsDemoModule
     ]
 })
 export class TimeSectionDemoModule {
--- src/app/demo/pc/time-section/time-section-options/demo.component.ts
+++ src/app/demo/pc/time-section/time-section-options/demo.component.ts
@@ -3,29 +3,7 @@ import {Component} from "@angular/core";
 @Component({
     templateUrl: './demo.component.html'
 })
-export class TimeSectionBasicDemoComponent {
-    multipleSelect1 = true;
-    timeSection = ['00:00-01:00', '05:00-06:00', '18:00-19:00'];
-
-    timeValueChange($event) {
-        console.log('time section picker change to ', $event);
-    }
-
-    multipleSelect2 = true;
-    weekSection = [{label: '周一', value: 1}, {label: '周三', value: 3}];
-
-    weekValueChange($event) {
-        console.log('week section picker change to ', $event);
-    }
-
-    multipleSelect3 = true;
-    daySection = [{label: 2, value: 2}, {label: 4, value: 4}];
-    showLastDay = true;
-
-    dayValueChange($event) {
-        console.log('day section picker change to ', $event);
-    }
-
+export class TimeSectionOptionsDemoComponent {
     timeSectionValue: any = {time: ['00:00-01:00', '05:00-06:00', '18:00-19:00'], date: [{label: 2, value: 2}, {label: 4, value: 4}]};
     layout;
     curTime = '2020-02';
