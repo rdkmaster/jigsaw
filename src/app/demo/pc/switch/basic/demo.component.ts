594c67ad8b063f8deb438b38d4158859eb658f13
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-03-25 19:08:28 +0800

    amend
    Change-Id: Idf6321641ba8e6a73c0c5588bbb55f36789aed95

--- src/app/demo/pc/switch/basic/demo.component.ts
+++ src/app/demo/pc/switch/basic/demo.component.ts
@@ -2,11 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     templateUrl: './demo.component.html',
-    styles: [`
-        jigsaw-switch {
-            margin-right: 20px;
-        }
-    `]
+    styleUrls:['./demo.component.scss']
 })
 export class SwitchBasicDemoComponent {
     checked: boolean;
