8de7237a3fb72bce72c94f47f2c439abb8252ca7
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-03-25 10:46:52 +0800

    amend
    Change-Id: I4ab878c8ff6a7fb8d4e3c238bb8d068a489883fd

---
+++ src/app/demo/pc/button-bar/theme/demo.component.ts
@@ -0,0 +1,41 @@
+import {AfterContentInit, Component} from "@angular/core";
+import {ArrayCollection} from "jigsaw/public_api";
+
+@Component({
+    templateUrl: './demo.component.html',
+    styleUrls: ["./demo.component.scss"]
+})
+export class ButtonBarThemeDemoComponent implements AfterContentInit {
+    multiple: boolean = false;
+
+    selectedCity: any[];
+    selectedCityStr: string;
+    cities = new ArrayCollection([
+        {label: "选中状态", id: 1},
+        {label: "正常状态并且特别的长", id: 2},
+        {label: "正常状态", id: 3},
+        {label: "禁用状态", id: 4, disabled: true},
+        {label: "正常状态2", id: 5},
+        {label: "禁用状态2", id: 6, disabled: true}
+    ]);
+    selectedTypes = [{id: 4}];
+
+    basicSelectChange(cityArr: ArrayCollection<any>) {
+        this.selectedCityStr = cityArr.map(city => city.label).join(',');
+    }
+
+    ngAfterContentInit() {
+        this.resetSelection();
+    }
+
+    resetSelection() {
+        this.selectedCity = [{label: "选中状态", id: 1}];
+        this.selectedCityStr = this.selectedCity.map(city => city.label).join(',');
+    }
+
+    // ====================================================================
+    // ignore the following lines, they are not important to this demo
+    // ====================================================================
+    summary: string = '';
+    description: string = '';
+}
---
+++ src/app/demo/pc/button-bar/theme/demo.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {JigsawButtonBarModule, JigsawRadioLiteModule, JigsawSwitchModule, JigsawHeaderModule} from "jigsaw/public_api";
+import { ButtonBarThemeDemoComponent } from './demo.component';
+import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
+
+@NgModule({
+    imports: [JigsawButtonBarModule, JigsawRadioLiteModule, JigsawDemoDescriptionModule, JigsawSwitchModule, JigsawHeaderModule],
+    declarations: [ButtonBarThemeDemoComponent],
+    exports: [ButtonBarThemeDemoComponent]
+})
+export class ButtonBarThemeDemoModule {
+}
