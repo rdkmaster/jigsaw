ee83e33cfca120ebd65074543ecf07117546d7ad
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-03-31 11:04:55 +0800

    amend
    Change-Id: Iaebda4c11fbd1008c8b513ba0f05f6580e17f02f

--- src/app/demo/pc/switch/demo-set.module.ts
+++ src/app/demo/pc/switch/demo-set.module.ts
@@ -3,17 +3,23 @@ import {RouterModule} from "@angular/router";
 import {SwitchBasicDemoModule} from "./basic/demo.module";
 
 import {SwitchBasicDemoComponent} from "./basic/demo.component";
+import { SwitchSizeDemoComponent } from './size/demo.component';
+import { SwitchSizeDemoModule } from './size/demo.module';
 
 export const routerConfig = [
     {
         path: 'basic', component: SwitchBasicDemoComponent
+    },
+    {
+        path: 'size', component: SwitchSizeDemoComponent
     },
 ];
 
 @NgModule({
     imports: [
         RouterModule.forChild(routerConfig),
-        SwitchBasicDemoModule
+        SwitchBasicDemoModule,
+        SwitchSizeDemoModule
     ]
 })
 export class SwitchDemoModule { }
