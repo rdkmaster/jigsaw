23f0080bee864c53883d83348f50d3e7e36455f0
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-03-31 11:09:16 +0800

    amend
    Change-Id: I9ef47b16fd8b31a775d43bf4f5877e507bc464c1

--- src/app/demo/pc/button-bar/theme/demo.component.ts
+++ src/app/demo/pc/button-bar/theme/demo.component.ts
@@ -1,8 +1,8 @@
-import {AfterContentInit, Component} from "@angular/core";
-import {ArrayCollection} from "jigsaw/public_api";
+import { AfterContentInit, Component } from "@angular/core";
+import { ArrayCollection } from "jigsaw/public_api";
 
 @Component({
-    templateUrl: './demo.component.html',
+    templateUrl: "./demo.component.html",
     styleUrls: ["./demo.component.scss"]
 })
 export class ButtonBarThemeDemoComponent implements AfterContentInit {
@@ -11,17 +11,17 @@ export class ButtonBarThemeDemoComponent implements AfterContentInit {
     selectedCity: any[];
     selectedCityStr: string;
     cities = new ArrayCollection([
-        {label: "选中状态", id: 1},
-        {label: "正常状态并且特别的长", id: 2},
-        {label: "正常状态", id: 3},
-        {label: "禁用状态", id: 4, disabled: true},
-        {label: "正常状态2", id: 5},
-        {label: "禁用状态2", id: 6, disabled: true}
+        { label: "选中状态", id: 1 },
+        { label: "正常状态并且特别的长", id: 2 },
+        { label: "正常状态", id: 3 },
+        { label: "禁用状态", id: 4, disabled: true },
+        { label: "正常状态2", id: 5 },
+        { label: "禁用状态2", id: 6, disabled: true }
     ]);
-    selectedTypes = [{id: 4}];
+    selectedTypes = [{ id: 4 }];
 
     basicSelectChange(cityArr: ArrayCollection<any>) {
-        this.selectedCityStr = cityArr.map(city => city.label).join(',');
+        this.selectedCityStr = cityArr.map(city => city.label).join(",");
     }
 
     ngAfterContentInit() {
@@ -29,13 +29,15 @@ export class ButtonBarThemeDemoComponent implements AfterContentInit {
     }
 
     resetSelection() {
-        this.selectedCity = [{label: "选中状态", id: 1}];
-        this.selectedCityStr = this.selectedCity.map(city => city.label).join(',');
+        this.selectedCity = [{ label: "选中状态", id: 1 }];
+        this.selectedCityStr = this.selectedCity
+            .map(city => city.label)
+            .join(",");
     }
 
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = '';
-    description: string = '';
+    summary: string = "";
+    description: string = "";
 }
--- src/app/demo/pc/button-bar/theme/demo.module.ts
+++ src/app/demo/pc/button-bar/theme/demo.module.ts
@@ -1,12 +1,22 @@
-import {NgModule} from '@angular/core';
-import {JigsawButtonBarModule, JigsawRadioLiteModule, JigsawSwitchModule, JigsawHeaderModule} from "jigsaw/public_api";
-import { ButtonBarThemeDemoComponent } from './demo.component';
-import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
+import { NgModule } from "@angular/core";
+import {
+    JigsawButtonBarModule,
+    JigsawRadioLiteModule,
+    JigsawSwitchModule,
+    JigsawHeaderModule
+} from "jigsaw/public_api";
+import { ButtonBarThemeDemoComponent } from "./demo.component";
+import { JigsawDemoDescriptionModule } from "app/demo-description/demo-description";
 
 @NgModule({
-    imports: [JigsawButtonBarModule, JigsawRadioLiteModule, JigsawDemoDescriptionModule, JigsawSwitchModule, JigsawHeaderModule],
+    imports: [
+        JigsawButtonBarModule,
+        JigsawRadioLiteModule,
+        JigsawDemoDescriptionModule,
+        JigsawSwitchModule,
+        JigsawHeaderModule
+    ],
     declarations: [ButtonBarThemeDemoComponent],
     exports: [ButtonBarThemeDemoComponent]
 })
-export class ButtonBarThemeDemoModule {
-}
+export class ButtonBarThemeDemoModule {}
