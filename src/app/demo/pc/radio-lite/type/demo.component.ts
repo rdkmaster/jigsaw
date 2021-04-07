10ee0abfdb6be7bf574268bbccbec0d4a522c393
陈旭10045812 <chen.xu8@zte.com.cn>
2021-04-07 17:02:34 +0800

    ...

--- src/app/demo/pc/radio-lite/type/demo.component.ts
+++ src/app/demo/pc/radio-lite/type/demo.component.ts
@@ -5,25 +5,29 @@ import { RadiosGroupValue } from 'jigsaw/public_api';
     templateUrl: "./demo.component.html"
 })
 export class RadioLiteTypeDemoComponent {
-    public selectedItem: string | RadiosGroupValue;
-    cities = [
-        "未选中1",
-        "未选中2",
-        "未选中3",
-        { label: "未选中禁用", disabled: true },
-        { label: "选中禁用", disabled: true }
+    public selectedItem: RadiosGroupValue | string;
+    public cities: (RadiosGroupValue | string)[] = [
+        "普通条目1",
+        "普通条目2",
+        "普通条目3",
+        { label: "禁用1", disabled: true },
+        { label: "禁用2", disabled: true }
     ];
 
     constructor() {
-        this.selectedItem = "选中禁用";
+        this.selectDisabledItem(1);
     }
 
     clearSelectedItem() {
-        this.selectedItem = null;
+        this.selectedItem = undefined;
     }
 
-    selectDisabledItem() {
-        this.selectedItem = { label: "选中禁用" };
+    selectDisabledItem(item) {
+        this.selectedItem = { label: "禁用" + item };
+    }
+
+    get value() {
+        return typeof this.selectedItem == 'object' ? this.selectedItem.label : this.selectedItem;
     }
 
     // ====================================================================
