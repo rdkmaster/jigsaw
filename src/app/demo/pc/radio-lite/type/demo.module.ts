626c3c0bcc24cc83a2ed324eb2d5b719c9d87a32
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2021-04-07 17:05:15 +0800

    [优化] radio组件新增四种状态 (#1364)

---
+++ src/app/demo/pc/radio-lite/type/demo.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {JigsawRadioLiteModule, JigsawHeaderModule} from "jigsaw/public_api";
+import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
+import { RadioLiteTypeDemoComponent } from "./demo.component";
+
+@NgModule({
+    declarations: [RadioLiteTypeDemoComponent],
+    exports: [RadioLiteTypeDemoComponent],
+    imports: [JigsawRadioLiteModule, CommonModule, JigsawDemoDescriptionModule, JigsawHeaderModule]
+})
+export class RadioLiteTypeDemoModule {
+
+}
