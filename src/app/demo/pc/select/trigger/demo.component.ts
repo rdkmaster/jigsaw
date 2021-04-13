d246010ad65914766ecba5f8243b28420d2f933d
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-04-13 08:49:13 +0800

    amend
    Change-Id: Id112d4d6ee2f0f35cb1b79ddbf28a8c2e1771146

--- src/app/demo/pc/select/trigger/demo.component.ts
+++ src/app/demo/pc/select/trigger/demo.component.ts
@@ -1,29 +1,32 @@
-import {Component} from "@angular/core";
-import {ArrayCollection} from "jigsaw/public_api";
+import { Component } from "@angular/core";
+import { ArrayCollection } from "jigsaw/public_api";
 
 @Component({
-  templateUrl: './demo.component.html',
+    templateUrl: "./demo.component.html"
 })
 export class SelectTriggerDemoComponent {
     selectedCityForSelect: any;
     cityListForSelect = new ArrayCollection([
-        {label: "北京"},
-        {label: "上海"},
-        {label: "南京"},
-        {label: "深圳"},
-        {label: "长沙"},
-        {label: "西安"}
+        { label: "北京" },
+        { label: "上海" },
+        { label: "南京" },
+        { label: "深圳" },
+        { label: "长沙" },
+        { label: "西安" }
     ]);
 
     selectedCityName: string;
 
-    public selectChange(selectedItem:any){
+    public selectChange(selectedItem: any) {
         this.selectedCityName = selectedItem.label;
     }
+
+    openTrigger = "mouseenter";
+    closeTrigger = "mouseleave";
 
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = '';
-    description: string = '';
+    summary: string = "";
+    description: string = "";
 }
