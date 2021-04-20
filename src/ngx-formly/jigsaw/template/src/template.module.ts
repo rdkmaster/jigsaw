42f4422f90ac87d19740237c22a8bd6b59e12448
周庆10229428 <zhou.qing4@zte.com.cn>
2021-04-20 10:55:17 +0800

    [新增]表单模块添加自定义模板类型，以支持template类型样式定义

---
+++ src/ngx-formly/jigsaw/template/src/template.module.ts
@@ -0,0 +1,29 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormlyModule} from '@ngx-formly/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormlyJigsawFormFieldModule} from "@ngx-formly/jigsaw/form-field";
+import {JigsawTrustedHtmlModule} from "@rdkmaster/jigsaw";
+
+import {FormlyTemplateFieldType} from "./template.type";
+
+@NgModule({
+    declarations: [FormlyTemplateFieldType],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        JigsawTrustedHtmlModule,
+        FormlyJigsawFormFieldModule,
+        FormlyModule.forChild({
+            types: [
+                {
+                    name: 'custom-template',
+                    component: FormlyTemplateFieldType,
+                    wrappers: ['form-field'],
+                }
+            ],
+        }),
+    ],
+})
+export class FormlyTemplateModule {
+}
