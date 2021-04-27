218ae380e8c87c66a258fc6c772afe499d05cdf5
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2021-04-27 18:34:11 +0800

    [优化] navigation与规范对齐，解决了@7706

--- src/app/demo/pc/menu/navigation/demo.component.ts
+++ src/app/demo/pc/menu/navigation/demo.component.ts
@@ -19,12 +19,12 @@ export class NavigationMenuNavDemo {
     constructor() {
         this.data1.fromXML(`
             <node>
-                <node label="当前告警" icon="iconfont iconfont-e9ad" isActive="true" selected="true">
-                    <node label="告警监控" selected="true" icon="iconfont iconfont-e5fc"></node>
+                <node label="当前告警" icon="iconfont iconfont-e5fd" isActive="true" selected="true">
+                    <node label="告警监控" selected="true" icon="iconfont iconfont-e2d8"></node>
                     <node label="告警统计"></node>
-                    <node label="定时导出" icon="iconfont iconfont-e0bd"></node>
+                    <node label="定时导出" icon="iconfont iconfont-e601"></node>
                     <node label="告警同步"></node>
-                    <node label="告警提示"></node>
+                    <node label="告警提示" icon="iconfont iconfont-e52a"></node>
                 </node>
                 <node label="历史告警" icon="iconfont iconfont-e5f7">
                     <node label="告警查询"></node>
@@ -37,7 +37,7 @@ export class NavigationMenuNavDemo {
         `);
         const xmlData = `
             <node>
-                <node label="当前告警" icon="iconfont iconfont-e9ad" selected="true"></node>
+                <node label="当前告警" icon="iconfont iconfont-e5fd" selected="true"></node>
                 <node label="历史告警" icon="iconfont iconfont-e5f7"></node>
                 <node label="通知" icon="iconfont iconfont-e605"></node>
                 <node label="告警设置" icon="iconfont iconfont-e36f"></node>
@@ -50,7 +50,7 @@ export class NavigationMenuNavDemo {
     updateMenu() {
         this.data1.fromXML(`
             <node>
-                <node label="一级菜单1" icon="iconfont iconfont-e9ad" isActive="true" selected="true">
+                <node label="一级菜单1" icon="iconfont iconfont-e5fd" isActive="true" selected="true">
                     <node label="二级菜单1" selected="true"></node>
                     <node label="二级菜单2"></node>
                     <node label="二级菜单3"></node>
