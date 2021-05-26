4cc7ee9b51cb3eb1339ef7f4c7b24ef9a4ff4e86
尤海鹏10210844 <you.haipeng@zte.com.cn>
2021-05-26 15:40:37 +0800

    [WIP][优化] 优化分页组件性能以适应万以上分页的场景，关联@7364
    Change-Id: Ic613ee21d7fb30b684e40ddc35259458fec37bdd

--- src/app/demo/pc/pagination/demo-set.module.ts
+++ src/app/demo/pc/pagination/demo-set.module.ts
@@ -5,6 +5,8 @@ import {PaginationBasicDemoModule} from "./basic/demo.module";
 
 import {PaginationBasicDemoComponent} from "./basic/demo.component";
 import {WithPagingInfoDemoComponent} from "./with-page-info/demo.component";
+import {PaginationBigDataDemoComponent} from "./big-data/demo.component";
+import {PaginationBigDataDemoModule} from "./big-data/demo.module";
 
 export const routerConfig = [
     {
@@ -13,13 +15,17 @@ export const routerConfig = [
     {
         path: 'with-page-info', component: WithPagingInfoDemoComponent
     },
+    {
+        path: 'big-data', component: PaginationBigDataDemoComponent
+    },
 ];
 
 @NgModule({
     imports: [
         RouterModule.forChild(routerConfig),
         PaginationBasicDemoModule,
-        WithPagingInfoDemoModule
+        WithPagingInfoDemoModule,
+        PaginationBigDataDemoModule
     ]
 })
 export class PaginationDemoModule { }
