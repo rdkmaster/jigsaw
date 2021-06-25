ab0a4512f674b662410d288429756824a6ac7429
周庆10229428 <zhou.qing4@zte.com.cn>
2021-06-25 14:40:35 +0800

    [新增] Steps步骤条组件新增currentChange事件和disabled配置，关联@7157

--- src/app/demo/pc/steps/context/demo.component.ts
+++ src/app/demo/pc/steps/context/demo.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
-import { InternalUtils } from "jigsaw/public_api";
+import {Component} from "@angular/core";
+import {InternalUtils} from "jigsaw/public_api";
 
 @Component({
     templateUrl: "./demo.component.html",
     styleUrls: ["./demo.component.scss"]
 })
-export class JigsawStepVerticalDemoComponent {
+export class JigsawStepContextDemoComponent {
     data = [];
 
     current = 0;
@@ -28,6 +28,7 @@ export class JigsawStepVerticalDemoComponent {
         };
     }
 
+    // 通过指定context属性到this，才能在steps节点上，直接调用当前组件上的成员方法
     public changeStatus(idx: number) {
         this.current = idx;
     }
@@ -39,9 +40,10 @@ export class JigsawStepVerticalDemoComponent {
         }
         this.data = tempData;
     }
+
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = "";
+    summary: string = "这个demo演示steps的数据中，context属性的用法";
     description: string = "";
 }
