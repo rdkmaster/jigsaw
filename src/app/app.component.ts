9bebee851bd4e76f957a09af5d3fed7516214909
尤海鹏10210844 <you.haipeng@zte.com.cn>
2021-07-01 11:37:08 +0800

    [优化] 修复切换皮肤时，demo标题的颜色与皮肤不匹配，解决了@7071
    Change-Id: I605d80e64d2e09a7da06610afa7dfbf3cd535171

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component, ViewEncapsulation} from "@angular/core";
+import {JigsawTheme} from "jigsaw/public_api";
 
 @Component({
     selector: 'app-root',
     template: `
         <jigsaw-root>
-            <div class="app-wrap">
+            <div class="app-wrap" [class.theme-dark]="theme=='dark'">
                 <router-outlet></router-outlet>
             </div>
         </jigsaw-root>
@@ -13,4 +14,7 @@ import {Component, ViewEncapsulation} from "@angular/core";
     encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+    get theme() {
+        return JigsawTheme.majorStyle
+    }
 }
