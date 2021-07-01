b61927f6e4602a64885611ff861abeaba4cf1599
You Haipeng <you.haipeng@zte.com.cn>
2021-07-01 15:01:36 +0800

    [优化] 修改部分demo以适配暗色皮肤，解决了@7055

--- src/app/demo/pc/tooltip/moving-tooltip/demo.module.ts
+++ src/app/demo/pc/tooltip/moving-tooltip/demo.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from "@angular/core";
-import {JigsawMovableModule, JigsawAlertModule, JigsawBadgeModule, JigsawSliderModule, JigsawTooltipModule} from "jigsaw/public_api";
+import {
+    JigsawMovableModule,
+    JigsawAlertModule,
+    JigsawBadgeModule,
+    JigsawSliderModule,
+    JigsawTooltipModule,
+    JigsawButtonModule
+} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {MovingTooltipDemoComponent} from "./demo.component";
 
 @NgModule({
     imports: [
-        JigsawMovableModule, JigsawTooltipModule, JigsawDemoDescriptionModule
+        JigsawMovableModule, JigsawTooltipModule, JigsawDemoDescriptionModule, JigsawButtonModule
     ],
     declarations: [MovingTooltipDemoComponent],
     exports: [MovingTooltipDemoComponent]
