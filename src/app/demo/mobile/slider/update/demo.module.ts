151248f2a443cff10fe535316343fcbdb3f20ae0
尤海鹏10210844 <you.haipeng@zte.com.cn>
2021-07-01 16:37:22 +0800

    native button to jigsaw button
    Change-Id: I8aa321458e1ab78208ef6b759257245a2dbc20d2

--- src/app/demo/mobile/slider/update/demo.module.ts
+++ src/app/demo/mobile/slider/update/demo.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
-import {JigsawMobileSliderModule, JigsawMobileSwitchModule} from "jigsaw/mobile_public_api";
+import {JigsawMobileSliderModule, JigsawMobileSwitchModule, JigsawMobileButtonModule} from "jigsaw/mobile_public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {SliderUpdateDemoComponent} from './demo.component';
 
 @NgModule({
-    imports: [JigsawMobileSwitchModule, JigsawMobileSliderModule, JigsawDemoDescriptionModule],
+    imports: [JigsawMobileSwitchModule, JigsawMobileSliderModule, JigsawDemoDescriptionModule, JigsawMobileButtonModule],
     declarations: [SliderUpdateDemoComponent],
     exports: [SliderUpdateDemoComponent]
 })
--- src/app/demo/pc/table/tree-table/demo.module.ts
+++ src/app/demo/pc/table/tree-table/demo.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {JigsawTableModule, JigsawPaginationModule} from "jigsaw/public_api";
+import {JigsawTableModule, JigsawPaginationModule, JigsawButtonModule} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {TreeTableDemoComponent} from './demo.component';
 
 @NgModule({
     imports: [
-        JigsawTableModule, CommonModule, JigsawDemoDescriptionModule, JigsawPaginationModule
+        JigsawTableModule, CommonModule, JigsawDemoDescriptionModule, JigsawPaginationModule, JigsawButtonModule
     ],
     declarations: [TreeTableDemoComponent],
     exports: [TreeTableDemoComponent]
