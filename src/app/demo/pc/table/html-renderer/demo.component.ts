91d5dacf052b483b69336930421b96004aac2e91
陈旭10045812 <chen.xu8@zte.com.cn>
2021-07-14 11:06:39 +0800

    [优化] 解决/table/html-renderer这个demo在深浅色系下标题渲染器的皮肤问题，解决了@7051

--- src/app/demo/pc/table/html-renderer/demo.component.ts
+++ src/app/demo/pc/table/html-renderer/demo.component.ts
@@ -1,15 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, ViewEncapsulation} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {
-    RawTableData, TableData, AdditionalColumnDefine, ColumnDefine,
-    CommonUtils, SortAs, SortOrder
+    AdditionalColumnDefine,
+    ColumnDefine,
+    CommonUtils,
+    JigsawTheme,
+    RawTableData,
+    SortAs,
+    SortOrder,
+    TableData
 } from "jigsaw/public_api";
 
 @Component({
-    templateUrl: './demo.component.html'
+    templateUrl: './demo.component.html',
+    encapsulation: ViewEncapsulation.None
 })
 export class TableHtmlRendererDemoComponent {
-
     tableData: TableData;
 
     constructor(http: HttpClient) {
@@ -28,6 +34,9 @@ export class TableHtmlRendererDemoComponent {
         }
     }
 
+    // 这里为了能让demo同时适配深浅色系才做的这么复杂，如果应用没有深浅色系前的需求，则无需搞这么复杂
+    readonly optionStyle = `background-color: ${JigsawTheme.majorStyle == 'dark' ? '#0f111a' : '#fff'}`;
+
     columns: ColumnDefine[] = [
         {
             target: 'desc',
@@ -40,9 +49,15 @@ export class TableHtmlRendererDemoComponent {
             header: {
                 renderer: 'html',
                 data: (data, col) => `${data.header[col]}
-                    <select onchange="changeUnit()" id="unitSelect">
-                        <option value="￥">￥</option>
-                        <option value="$">$</option>
+                    <select style="padding: 4px 8px;
+                                    margin-left: 4px;
+                                    font-size: 12px;
+                                    background: transparent;
+                                    -webkit-appearance: none;
+                                    border-radius: 4px;"
+                            onchange="changeUnit()" id="unitSelect">
+                        <option style="${this.optionStyle}" value="￥">￥</option>
+                        <option style="${this.optionStyle}" value="$">$</option>
                     </select>`,
                 innerHtmlContext: this,
                 sortable: true,
