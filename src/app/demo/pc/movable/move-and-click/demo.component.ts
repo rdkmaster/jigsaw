839312f01b1cd63d9aafe3a4bf5d8a3162216010
You Haipeng <you.haipeng@zte.com.cn>
2021-07-15 16:39:16 +0800

    [新增] 增加移动指令的定位类型和偏移的配置，关联@7020

--- src/app/demo/pc/movable/move-and-click/demo.component.ts
+++ src/app/demo/pc/movable/move-and-click/demo.component.ts
@@ -2,8 +2,7 @@ import {Component} from "@angular/core";
 import {JigsawInfoAlert} from "jigsaw/public_api";
 
 @Component({
-    templateUrl: 'demo.component.html',
-    styleUrls: ['./demo.component.css']
+    templateUrl: 'demo.component.html'
 })
 export class MoveAndClickDemoComponent {
     mouseStartPosition = {x: null, y: null};
