3e5092187018a3c9c3dfb271e03d798485faa8f5
周庆10229428 <zhou.qing4@zte.com.cn>
2021-07-16 18:17:59 +0800

    [优化] 上传结果组件在遇到类型或大小错误时，不显示重新上传按钮，解决了@7156

--- src/jigsaw/common/directive/upload/uploader-typings.ts
+++ src/jigsaw/common/directive/upload/uploader-typings.ts
@@ -8,7 +8,9 @@ export type UploadingFileLog = {
 export type UploadFileInfo = {
     name: string, url: string, file: File,
     state: 'pause' | 'loading' | 'success' | 'error',
-    progress?: number, log?: UploadingFileLog[], message?: string
+    progress?: number, log?: UploadingFileLog[], message?: string,
+    // 失败原因：类型错误，大小不符合 时，不让显示重新上传，因为没有意义
+    hideRetry?: boolean
 }
 
 export interface IUploader {
