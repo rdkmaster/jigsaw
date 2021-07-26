ba262260107234d83d296c43a61702aa9e079300
陈旭 <chen.xu8@zte.com.cn>
2021-07-26 16:24:48 +0800

    [新增] 给出如何处理slider多个值的demo

--- src/app/demo/pc/slider/handling-multi-value/demo.component.ts
+++ src/app/demo/pc/slider/handling-multi-value/demo.component.ts
@@ -5,25 +5,21 @@ import {ArrayCollection} from "jigsaw/public_api";
     templateUrl: './demo.component.html',
     styleUrls: ['./demo.component.css']
 })
-export class SliderUpdateDemoComponent {
-    rangeMax = 100;
-    rangeValue = new ArrayCollection([30, 50, 60]);
+export class SliderMultiValueComponent {
+    rangeValue1 = new ArrayCollection([20, 50, 80]);
+    rangeValue2 = new ArrayCollection([20, 50, 80]);
+
     get sortedValue(): string {
-        return [...this.rangeValue].sort((a, b) => a - b).join('-');
+        return [...this.rangeValue1].sort((a, b) => a - b).join('-');
     }
 
-    updateValue() {
-        // max是绑定变量，赋值给组件会有延迟，所以后面刷新视图需要加延迟
-        this.rangeMax = 200;
-        setTimeout(() => {
-            this.rangeValue.set(2, 200);
-            this.rangeValue.refresh();
-        })
+    handleChange() {
+        this.rangeValue2.sort((a, b) => a - b);
     }
 
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = '';
+    summary: string = '本demo主要给出如何处理slider多个值的情形';
     description: string = '';
 }
