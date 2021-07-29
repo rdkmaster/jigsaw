853be6bbb22b98b85c977350c722612e82e16cf9
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-07-29 19:15:41 +0800

    amd
    Change-Id: I3b88bce64b5179c65b58fd810e359b40b7e77a20

--- src/app/demo/pc/tree/icon/demo.component.ts
+++ src/app/demo/pc/tree/icon/demo.component.ts
@@ -8,6 +8,35 @@ export class ZtreeIconDemoComponent implements AfterViewInit {
     @ViewChild(JigsawTreeExt) treeExt: JigsawTreeExt;
 
     public data: SimpleTreeData;
+    public data2: SimpleTreeData;
+
+    public iconData: ZTreeIcon = {
+        edit: "iconfont-e166",
+        remove: "iconfont-e17b",
+        open: "iconfont-e2a2",
+        close: "iconfont-e19c",
+        document: "iconfont-e9dc",
+        checkboxChecked: "iconfont-ea38",
+        checkboxNotCheck: "iconfont-e9f1",
+        checkboxHalf: "iconfont-ea25",
+        nodeOpen: "iconfont-ea24",
+        nodeClose: "iconfont-ea57"
+    };
+
+    public iconData2: ZTreeIcon = {
+        edit: "iconfont-e166",
+        remove: "iconfont-e17b",
+        open: "iconfont-e9de",
+        close: "iconfont-e9db",
+        document: "iconfont-e9dc",
+        nodeOpen: "iconfont-e2b4",
+        nodeClose: "iconfont-e2b3"
+    };
+
+    public onClick(msg: any) {
+        console.log("click");
+        console.log(msg);
+    }
 
     constructor() {
         this.data = new SimpleTreeData();
@@ -75,25 +104,72 @@ export class ZtreeIconDemoComponent implements AfterViewInit {
             },
             { label: "父节点3 - 没有子节点", isParent: true }
         ]);
+        this.data2 = new SimpleTreeData();
+        this.data2.fromObject([
+            {
+                label: "父节点1 - 展开",
+                iconSkin: "iconfont-e722",
+                open: true,
+                nodes: [
+                    {
+                        label: "父节点11 - 折叠",
+                        iconSkin: "iconfont-e720",
+                        nodes: [
+                            { label: "叶子节点111", iconSkin: "iconfont-e721" },
+                            { label: "叶子节点112" },
+                            { label: "叶子节点113" },
+                            { label: "叶子节点114" }
+                        ]
+                    },
+                    {
+                        label: "父节点12 - 折叠",
+                        nodes: [
+                            { label: "叶子节点121" },
+                            { label: "叶子节点122" },
+                            { label: "叶子节点123" },
+                            { label: "叶子节点124" }
+                        ]
+                    },
+                    { label: "父节点13 - 没有子节点", isParent: true }
+                ]
+            },
+            {
+                label: "父节点2 - 折叠",
+                iconSkin: "iconfont-e722",
+                nodes: [
+                    {
+                        label: "父节点21 - 展开",
+                        open: true,
+                        nodes: [
+                            { label: "叶子节点211" },
+                            { label: "叶子节点212" },
+                            { label: "叶子节点213" },
+                            { label: "叶子节点214" }
+                        ]
+                    },
+                    {
+                        label: "父节点22 - 折叠",
+                        nodes: [
+                            { label: "叶子节点221" },
+                            { label: "叶子节点222" },
+                            { label: "叶子节点223" },
+                            { label: "叶子节点224" }
+                        ]
+                    },
+                    {
+                        label: "父节点23 - 折叠",
+                        nodes: [
+                            { label: "叶子节点231" },
+                            { label: "叶子节点232" },
+                            { label: "叶子节点233" },
+                            { label: "叶子节点234" }
+                        ]
+                    }
+                ]
+            },
+            { label: "父节点3 - 没有子节点", isParent: true }
+        ]);
     }
-
-    public onClick(msg: any) {
-        console.log("click");
-        console.log(msg);
-    }
-
-    public iconData: ZTreeIcon = {
-        edit: "iconfont-e166",
-        remove: "iconfont-e17b",
-        open: "iconfont-e9de",
-        close: "iconfont-e9db",
-        document: "iconfont-e9dc",
-        checkboxChecked: "iconfont-ea38",
-        checkboxNotCheck: "iconfont-e9f1",
-        checkboxHalf: "iconfont-ea25",
-        nodeOpen: "iconfont-ea24",
-        nodeClose: "iconfont-ea57"
-    };
 
     ngAfterViewInit() {
         if (this.treeExt && this.treeExt.ztree) {
