281bdd9ee4006adaa0a8269fd5b4cd7f090752f5
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-08-03 18:41:04 +0800

    amd
    Change-Id: I9399d5d59c8831bb4699246220d9602117529764

--- src/jigsaw/pc-components/input/auto-complete-input.ts
+++ src/jigsaw/pc-components/input/auto-complete-input.ts
@@ -14,8 +14,7 @@ import {
     Output,
     TemplateRef,
     ViewChild,
-    Injector,
-    ElementRef
+    Injector
 } from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule, NG_VALUE_ACCESSOR} from "@angular/forms";
