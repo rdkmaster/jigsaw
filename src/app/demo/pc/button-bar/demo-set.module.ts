6b2bef50c1341e873c362a41555262b3688b22e7
ZhouQing <42016535+10229428@users.noreply.github.com>
2021-08-25 14:20:13 +0800

    [新增] 按钮栏新增preSize属性，关联@6928

--- src/app/demo/pc/button-bar/demo-set.module.ts
+++ src/app/demo/pc/button-bar/demo-set.module.ts
@@ -2,8 +2,10 @@ import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
 import {ButtonBarBasicDemoComponent} from "./basic/demo.component";
 import {ButtonBarBasicDemoModule} from "./basic/demo.module";
-import { ButtonBarThemeDemoComponent } from './theme/demo.component';
-import { ButtonBarThemeDemoModule } from './theme/demo.module';
+import {ButtonBarThemeDemoComponent} from './theme/demo.component';
+import {ButtonBarThemeDemoModule} from './theme/demo.module';
+import {ButtonBarSizeDemoComponent} from "./size/demo.component";
+import {ButtonBarSizeDemoModule} from "./size/demo.module";
 
 export const routerConfig = [
     {
@@ -11,6 +13,9 @@ export const routerConfig = [
     },
     {
         path: 'theme', component: ButtonBarThemeDemoComponent
+    },
+    {
+        path: 'size', component: ButtonBarSizeDemoComponent
     }
 ];
 
@@ -18,7 +23,9 @@ export const routerConfig = [
     imports: [
         RouterModule.forChild(routerConfig),
         ButtonBarBasicDemoModule,
-        ButtonBarThemeDemoModule
+        ButtonBarThemeDemoModule,
+        ButtonBarSizeDemoModule
     ]
 })
-export class ButtonBarDemoModule { }
+export class ButtonBarDemoModule {
+}
