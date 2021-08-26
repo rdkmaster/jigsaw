c4d5c599fe0a93baccc4f2bf499d745739d959b6
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-08-26 10:41:26 +0800

    amd
    Change-Id: I512622af33c04392b47e0ea3b4045ef0fce5e5b7

--- src/jigsaw/pc-components/select/select-base.ts
+++ src/jigsaw/pc-components/select/select-base.ts
@@ -558,13 +558,6 @@ export abstract class JigsawSelectBase
         (<LocalPageableArray<any> | PageableArray>this.data).filter(filterKey, [this.labelField]);
         this._listScrollbar && this._listScrollbar.scrollToTop();
     }
-
-    /**
-     * @internal
-     */
-    public _$showNoDataHints(data):boolean{
-        return !!data.busy;
-    } 
 }
 
 export abstract class JigsawSelectGroupBase extends JigsawSelectBase {
