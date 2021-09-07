9a168e565606a8b25eb493ef8ee4971c41012ed8
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-09-07 15:17:24 +0800

    amd
    Change-Id: Ifd39fd1bb498a105b2905628400bd785d453034b

--- src/jigsaw/common/directive/upload/upload.directive.ts
+++ src/jigsaw/common/directive/upload/upload.directive.ts
@@ -217,15 +217,17 @@ export class JigsawUploadDirective extends AbstractJigsawComponent implements IU
         if (!this.multiple) {
             this.files.splice(0, this.files.length);
             files.splice(1, files.length);
+        } else {
+            fileInput.value = null;
         }
         this.files.push(...files);
-        fileInput.value = null;
         return this.files.length > 0;
     }
 
     public upload() {
         this.runAfterMicrotasks(() => {
             this._zone.run(()=>{
+                console.log(this._appendFiles(),this.files)
                 if (!this._appendFiles()) {
                     return;
                 }
