92b38f91aac0e4dd82717d46be257fbb650eefc7
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-09-13 17:24:44 +0800

    amd
    Change-Id: I2d269fe76b81c53c1b7c38ea43bf01b29bc190e9

--- src/app/demo/pc/status/basic/demo.module.ts
+++ src/app/demo/pc/status/basic/demo.module.ts
@@ -1,8 +1,7 @@
 import {NgModule} from "@angular/core";
-import {JigsawHeaderModule} from "jigsaw/public_api";
+import {JigsawHeaderModule, JigsawStatusModule} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {StatusBasicDemoComponent} from "./demo.component";
-import { JigsawStatusModule } from 'jigsaw/pc-components/status/status';
 
 @NgModule({
     declarations: [StatusBasicDemoComponent],
