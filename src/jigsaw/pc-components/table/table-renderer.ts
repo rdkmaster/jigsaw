49694aa25625738277c94ef7c1ea064e6b70db0b
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-09-30 10:59:40 +0800

    amd
    Change-Id: I7bcce2df2f64461815287390f7e44e0186f9920a

--- src/jigsaw/pc-components/table/table-renderer.ts
+++ src/jigsaw/pc-components/table/table-renderer.ts
@@ -653,7 +653,6 @@ export class TreeTableCellRenderer extends TableCellRendererBase {
     public tableData: PageableTreeTableData;
 
     public get indent(): string {
-        console.log((this.cellData.id.length - 1) * 20 + 'px')
         return (this.cellData.id.length - 1) * 20 + 'px';
     }
 
