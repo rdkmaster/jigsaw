78d702a1a029b2d264ee011ce28b33ba44e777fc
陈旭10045812 <chen.xu8@zte.com.cn>
2021-09-30 11:56:05 +0800

    ...

--- src/app/demo/pc/icon/status/demo.component.ts
+++ src/app/demo/pc/icon/status/demo.component.ts
@@ -2,15 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     templateUrl: './demo.component.html',
-    styles: [
-        `
-            .jigsaw-icon {
-                margin-right: 10px
-            }
-        `
-    ]
+    styles: [`
+        .icons {
+            margin-right: 10px;
+            margin-bottom: 16px;
+        }
+    `]
 })
 export class IconStatusDemoComponent {
+    fontSize = 12;
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
--- src/app/demo/pc/icon/status/demo.module.ts
+++ src/app/demo/pc/icon/status/demo.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {JigsawIconModule, JigsawHeaderModule} from "jigsaw/public_api";
+import {JigsawIconModule, JigsawHeaderModule, JigsawNumericInputModule} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {IconStatusDemoComponent} from "./demo.component";
 
 @NgModule({
     imports: [
-        JigsawIconModule, CommonModule, JigsawDemoDescriptionModule, JigsawHeaderModule
+        JigsawIconModule, CommonModule, JigsawDemoDescriptionModule, JigsawHeaderModule, JigsawNumericInputModule
     ],
     declarations: [IconStatusDemoComponent],
     exports: [IconStatusDemoComponent]
