fe826035fea5b49b297e89ee496bbe7c83761037
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-10-09 17:38:59 +0800

    amd
    Change-Id: I9b0031af064f86767172fbd8ddbc7aaefb923ed7

--- src/app/demo/pc/tree/fuzzy-search/demo.component.ts
+++ src/app/demo/pc/tree/fuzzy-search/demo.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from "@angular/core";
 import { SimpleTreeData, JigsawTreeExt } from "jigsaw/public_api";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     templateUrl: './demo.component.html'
@@ -9,41 +10,10 @@ export class ZtreeFuzzySearchComponent implements AfterViewInit {
 
     public data: SimpleTreeData;
 
-    constructor() {
+    constructor(public http: HttpClient) {
         this.data = new SimpleTreeData();
-        this.data.fromXML(`
-            <node>
-                <node label="父节点1 - 展开" open="true">
-                    <node label="父节点11 - 折叠">
-                        <node label="叶子节点111"></node>
-                        <node label="叶子节点112"></node>
-                        <node label="叶子节点113"></node>
-                        <node label="叶子节点114"></node>
-                    </node>
-                    <node label="父节点12 - 折叠">
-                        <node label="叶子节点121"></node>
-                        <node label="叶子节点122"></node>
-                        <node label="叶子节点123"></node>
-                        <node label="叶子节点124"></node>
-                    </node>
-                </node>
-                <node label="父节点2 - 展开" open="true">
-                    <node label="父节点21 - 展开" open="true">
-                        <node label="叶子节点111"></node>
-                        <node label="叶子节点112"></node>
-                        <node label="叶子节点113"></node>
-                        <node label="叶子节点114"></node>
-                    </node>
-                    <node label="父节点22 - 折叠">
-                        <node label="叶子节点121"></node>
-                        <node label="叶子节点122"></node>
-                        <node label="叶子节点123"></node>
-                        <node label="叶子节点124"></node>
-                    </node>
-                </node>
-                <node label="父节点3 - 没有子节点" isParent="true"></node>
-            </node>
-        `)
+        this.data.http = http;
+        this.data.fromAjax("mock-data/tree-data");
     }
 
     search(value: string): void {
