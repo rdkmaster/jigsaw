52b4c2fd5a1b2e793b6259a6a38dad3dc91e49f6
周庆10229428 <zhou.qing4@zte.com.cn>
2021-10-20 16:36:30 +0800

    [优化] 流程状态组件模块内部结构

--- src/app/demo/pc/process-status/status-multiline/demo.module.ts
+++ src/app/demo/pc/process-status/status-multiline/demo.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
-import {JigsawButtonModule, JigsawProcessStatusModule, JigsawProcessStatusMultilineModule} from "jigsaw/public_api";
+import {JigsawButtonModule, JigsawProcessStatusModule} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {ProcessStatusMultilineComponent} from './demo.component';
 
 @NgModule({
     imports: [
-        CommonModule, PerfectScrollbarModule, JigsawDemoDescriptionModule, JigsawProcessStatusModule, JigsawProcessStatusMultilineModule,
+        CommonModule, PerfectScrollbarModule, JigsawDemoDescriptionModule, JigsawProcessStatusModule,
         JigsawButtonModule
     ],
     declarations: [ProcessStatusMultilineComponent],
