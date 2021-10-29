1d94d0c77481f3fd99bc3dc55346fe22408a9829
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-10-29 17:49:31 +0800

    amd
    Change-Id: Id6416909bc46066eff87433734f171a6371ad532

--- src/jigsaw/common/directive/upload/upload.directive.ts
+++ src/jigsaw/common/directive/upload/upload.directive.ts
@@ -239,6 +239,7 @@ export class JigsawUploadDirective extends JigsawUploadBase implements IUploader
     public appendFiles(fileList) {
         const files = this._checkFiles(Array.from(fileList || []));
         this.files.push(...files);
+        this.change.emit(this.files);
     }
 
     public upload() {
