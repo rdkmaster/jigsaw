a62f1d6b0f94f6d926a42d11e0c1b5cbeb40cfb0
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-11-04 15:34:39 +0800

    amd
    Change-Id: Id2d0a581f1c59c0de2672270271b0121029be4d5

--- src/app/demo/pc/textarea/basic/demo.component.ts
+++ src/app/demo/pc/textarea/basic/demo.component.ts
@@ -6,7 +6,9 @@ import {JigsawTextarea} from "jigsaw/public_api";
 })
 export class TextareaBasicDemoComponent {
     textareaValue: any;
-    
+
+    @ViewChild('myTextarea') myTextarea: JigsawTextarea;
+
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
