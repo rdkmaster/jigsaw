b30354fb8af757fe4a5a435bcff65ca559deafea
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-12-02 11:11:11 +0800

    amd
    Change-Id: Ic411a0edb8973f690caf80cd237c4bf9160c3036

---
+++ src/jigsaw/pc-components/transfer/renderer/transfer-renderer.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+    template: './transfer-list.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TransferListRenderer {
+    
+}
