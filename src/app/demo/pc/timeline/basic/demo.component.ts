a14399f881c53fffee16b07666e6569b041ca861
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-12-22 15:06:00 +0800

    amd
    Change-Id: If0239d8c3918165333bacc6c02bb52884e64f207

--- src/app/demo/pc/timeline/basic/demo.component.ts
+++ src/app/demo/pc/timeline/basic/demo.component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
+import { TimelineNode } from 'jigsaw/pc-components/timeline/timeline';
 
 @Component({
     templateUrl: "./demo.component.html",
     styleUrls: ["./demo.component.css"]
 })
 export class JigsawTimelineBasicDemoComponent {
+    data: TimelineNode[] = [
+        { time: '2021-12-19 13:40:30', context: "1" },
+        { time: '2021-12-20 13:40:30', context: "123123123", title: "title" },
+        { time: '2021-11-20 11:40:30', context: "123123123", title: "title" }
+    ]
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
--- src/jigsaw/pc-components/timeline/timeline.ts
+++ src/jigsaw/pc-components/timeline/timeline.ts
@@ -2,20 +2,23 @@ import {
     Component,
     NgModule,
     ChangeDetectionStrategy,
-    Input
+    Input,
+    AfterViewInit
 } from "@angular/core";
 import { AbstractJigsawComponent } from "../../common/common";
 import { CommonModule } from "@angular/common";
+import { CommonUtils } from 'jigsaw/common/core/utils/common-utils';
+import { TimeGr, TimeService, TimeWeekStart } from "../../common/service/time.service";
 
 export type TimelineNode = {
     /**
      * 单个时间点的时间
      */
-    time: Date;
+    time: Date | string;
     /**
      * 单个时间点的内容
      */
-    context: string;
+    context: string | string[];
     /**
      * 单个时间点的状态
      */
@@ -38,7 +41,8 @@ export type TimelineNode = {
     },
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class JigsawTimeline extends AbstractJigsawComponent {
+export class JigsawTimeline extends AbstractJigsawComponent implements AfterViewInit {
+    private _data: TimelineNode[];
     /**
      * 时间点的数据
      *
@@ -46,7 +50,36 @@ export class JigsawTimeline extends AbstractJigsawComponent {
      *
      */
     @Input()
-    public data: TimelineNode[] = [{ time: new Date, context: "1" }, { time: new Date, context: "123123123", title: "title" }];
+    public get data(): TimelineNode[] {
+        return this._data;
+    }
+
+    public set data(value: TimelineNode[]) {
+        this._data = value;
+        this.update();
+    };
+
+    public update() {
+        if (CommonUtils.isUndefined(this.data) || this.data.length === 0) {
+            return;
+        }
+
+        this.data.forEach(node => {
+            console.log(typeof node.context === 'string')
+            if (typeof node.context === 'string') {
+                node.context = new Array(<string>node.context)
+            }
+        })
+
+        this.data.sort(function (a, b) {
+            return TimeService.getFormatDate(a.time, TimeGr.second) > TimeService.getFormatDate(b.time, TimeGr.second) ? 1 : -1;
+        })
+        console.log(this.data)
+    }
+
+    ngAfterViewInit() {
+        // this.update();
+    }
 }
 
 @NgModule({
