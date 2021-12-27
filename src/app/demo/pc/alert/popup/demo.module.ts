79f4ea1ba38eaa7092b733ccdcada9fbfb39cccd
谭子扬10293829 <tan.ziyang@zte.com.cn>
2021-12-27 15:50:49 +0800

    amd
    Change-Id: I196302ebd5032afdc565d9375c16d3af489b4df3

--- src/app/demo/pc/alert/popup/demo.module.ts
+++ src/app/demo/pc/alert/popup/demo.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from "@angular/core";
-import {JigsawAlertModule, JigsawButtonModule, JigsawInputModule} from "jigsaw/public_api";
+import {JigsawAlertModule, JigsawButtonModule, JigsawInputModule, JigsawHeaderModule, JigsawSliderModule} from "jigsaw/public_api";
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {AlertPopupDemoComponent} from "./demo.component";
 
 @NgModule({
     declarations: [AlertPopupDemoComponent],
     exports: [ AlertPopupDemoComponent ],
-    imports: [JigsawAlertModule, JigsawButtonModule, JigsawInputModule, JigsawDemoDescriptionModule]
+    imports: [JigsawAlertModule, JigsawButtonModule, JigsawInputModule, JigsawDemoDescriptionModule, JigsawHeaderModule, JigsawSliderModule]
 })
 export class AlertPopupDemoModule {
 }
