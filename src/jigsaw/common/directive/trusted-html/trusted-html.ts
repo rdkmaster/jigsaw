2a995dbfb27f0fc6ef17d033a38c39d00ff0e75f
谭子扬10293829 <tan.ziyang@zte.com.cn>
2022-03-09 13:41:13 +0800

    amd
    Change-Id: I136a245d575970fe0fccc03df41a3f4025225cc3

--- src/jigsaw/common/directive/trusted-html/trusted-html.ts
+++ src/jigsaw/common/directive/trusted-html/trusted-html.ts
@@ -7,7 +7,7 @@ type CallbackValues = { [callbackName: string]: HtmlCallback };
 
 export class JigsawTrustedHtmlBase {
     private static _callbacks = new Map<any, CallbackValues>();
-    public static _contexts: { context: any, counter: number }[] = [];
+    private static _contexts: { context: any, counter: number }[] = [];
     /**
      * @internal
      */
