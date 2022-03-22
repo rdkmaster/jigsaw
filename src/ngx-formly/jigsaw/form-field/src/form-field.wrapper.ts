28685f2df60ee5739a19c5e3ce3bb8b97f80c7fc
ZhouQing <42016535+10229428@users.noreply.github.com>
2022-03-22 19:31:28 +0800

    [优化] 对齐表单组件的label字体样式，关联了@6428

--- src/ngx-formly/jigsaw/form-field/src/form-field.wrapper.ts
+++ src/ngx-formly/jigsaw/form-field/src/form-field.wrapper.ts
@@ -31,6 +31,8 @@ import {FieldWrapper} from '@ngx-formly/core';
         }
 
         .jigsaw-formly-field-wrapper-label {
+            font-size: 14px;
+            color: #737373;
             min-height: 22px;
             display: flex;
             align-items: center;
@@ -54,6 +56,7 @@ import {FieldWrapper} from '@ngx-formly/core';
 
         .jigsaw-formly-field-required {
             color: red;
+            font-size: 12px;
         }
     `],
     changeDetection: ChangeDetectionStrategy.OnPush,
