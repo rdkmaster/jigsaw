d90a0d5cd66cc7ab9a8acc976596e24681d45b25
gloryoftan <916627606@qq.com>
2022-04-09 16:35:06 +0800

    amd

--- src/app/demo/pc/animation/knowledge/demo.component.ts
+++ src/app/demo/pc/animation/knowledge/demo.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
     templateUrl: "./demo.component.html",
-    styleUrls: ["./demo.component.css"]
+    styleUrls: ["./demo.component.css"],
+    encapsulation: ViewEncapsulation.None
 })
 export class JigsawAnimationKnowledgeDemoComponent {
     animationProperties = [
@@ -24,4 +25,31 @@ export class JigsawAnimationKnowledgeDemoComponent {
     description: string = "";
 }
 
+@Component({
+    selector: 'qa',
+    templateUrl: 'qa.html'
+})
+export class KnowledgeQA {
+    @Input()
+    index: number = 0;
 
+    animationQA = [
+        {
+            question: "一个CSS动画效果想要出现，必不可少的基本单元有哪些？",
+            answer: "动画名称，动画时间，animation属性和@keyframes规则。"
+        }
+    ]
+}
+
+@Component({
+    selector: 'tips',
+    templateUrl: 'tips.html',
+})
+export class KnowledgeTips {
+    @Input()
+    index: number = 0;
+
+    animationTips = [
+        { message: 'animation属性支持同时应用多个动画规则。所以实现多种动画效果时，正确的做法是分隔设置。' }
+    ]
+}
--- src/app/demo/pc/animation/knowledge/demo.module.ts
+++ src/app/demo/pc/animation/knowledge/demo.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { JigsawDemoDescriptionModule } from "app/demo-description/demo-description";
-import { JigsawAnimationKnowledgeDemoComponent } from "./demo.component";
+import { JigsawAnimationKnowledgeDemoComponent, KnowledgeQA, KnowledgeTips } from "./demo.component";
 import { JigsawHeaderModule } from "jigsaw/public_api";
-import { KnowledgeQA } from "./qa";
 
 @NgModule({
     imports: [CommonModule, JigsawDemoDescriptionModule, JigsawHeaderModule],
-    declarations: [JigsawAnimationKnowledgeDemoComponent, KnowledgeQA],
+    declarations: [JigsawAnimationKnowledgeDemoComponent, KnowledgeQA, KnowledgeTips],
     exports: [JigsawAnimationKnowledgeDemoComponent]
 })
 export class JigsawAnimationKnowledgeDemoModule { }
