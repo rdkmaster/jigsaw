cef5b41456f4bcc2d814fd3e185e73981d173057
gloryoftan <916627606@qq.com>
2022-04-10 09:44:21 +0800

    amd

--- src/app/demo/pc/animation/knowledge/demo.component.ts
+++ src/app/demo/pc/animation/knowledge/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from "@angular/core";
+import { AfterViewInit, Component, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
     templateUrl: "./demo.component.html",
@@ -6,15 +6,29 @@ import { Component, Input, ViewEncapsulation } from "@angular/core";
     encapsulation: ViewEncapsulation.None
 })
 export class JigsawAnimationKnowledgeDemoComponent {
-    animationProperties = [
-        { name: 'animation-name' },
-        { name: 'animation-duration' },
-        { name: 'animation-timing-function' },
-        { name: 'animation-delay' },
-        { name: 'animation-iteration-count' },
-        { name: 'animation-direction' },
-        { name: 'animation-fill-mode' },
-        { name: 'animation-play-state' }
+    animationPoints = [
+        {
+            point: 'animation-name',
+            exampleIndex: 0,
+            className: 'example-1'
+        },
+        { point: 'animation-duration' },
+        { point: 'animation-timing-function' },
+        { point: 'animation-delay' },
+        { point: 'animation-iteration-count' },
+        { point: 'animation-direction' },
+        { point: 'animation-fill-mode' },
+        { point: 'animation-play-state' }
+    ]
+
+    keyframePoints = [
+        { point: '起止关键帧可以不设置' },
+        { point: '关键帧列表可以合并' },
+        { point: '不同的关键帧选择器是无序的' },
+        { point: '重复定义的关键帧不是完全被覆盖的' },
+        { point: '关键帧中的样式可以不连续' },
+        { point: '!important无效' },
+        { point: '优先级最高' },
     ]
 
     // ====================================================================
@@ -55,4 +69,25 @@ export class KnowledgeTips {
     animationTips = [
         { message: 'animation属性支持同时应用多个动画规则。所以实现多种动画效果时，正确的做法是分隔设置。' }
     ]
+}
+
+@Component({
+    selector: 'points',
+    templateUrl: 'points.html',
+})
+export class KnowledgePoints {
+    @Input()
+    points: Array<any>;
+
+    handleAnimation(exampleIndex: number, className?: string) {
+        if (exampleIndex === undefined) {
+            return
+        }
+        const ele = <HTMLElement>document.querySelectorAll(".example-box")[exampleIndex];
+        ele.classList.remove(className);
+        ele.offsetWidth;
+        ele.classList.add(className)
+        console.log(ele)
+    }
+
 }
--- src/app/demo/pc/animation/knowledge/demo.module.ts
+++ src/app/demo/pc/animation/knowledge/demo.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { JigsawDemoDescriptionModule } from "app/demo-description/demo-description";
-import { JigsawAnimationKnowledgeDemoComponent, KnowledgeQA, KnowledgeTips } from "./demo.component";
+import { JigsawAnimationKnowledgeDemoComponent, KnowledgePoints, KnowledgeQA, KnowledgeTips } from "./demo.component";
 import { JigsawHeaderModule } from "jigsaw/public_api";
 
 @NgModule({
     imports: [CommonModule, JigsawDemoDescriptionModule, JigsawHeaderModule],
-    declarations: [JigsawAnimationKnowledgeDemoComponent, KnowledgeQA, KnowledgeTips],
+    declarations: [JigsawAnimationKnowledgeDemoComponent, KnowledgeQA, KnowledgeTips, KnowledgePoints],
     exports: [JigsawAnimationKnowledgeDemoComponent]
 })
 export class JigsawAnimationKnowledgeDemoModule { }
