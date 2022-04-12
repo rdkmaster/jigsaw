489dad3fb3a1873a4e03835fe89ebf310d2041c5
gloryoftan <916627606@qq.com>
2022-04-13 01:18:40 +0800

    amd

--- src/app/demo/pc/animation/knowledge/demo.component.ts
+++ src/app/demo/pc/animation/knowledge/demo.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, ViewEncapsulation } from "@angular/core";
+import { Component, Directive, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
     templateUrl: "./demo.component.html",
@@ -7,11 +7,7 @@ import { AfterViewInit, Component, Input, ViewEncapsulation } from "@angular/cor
 })
 export class JigsawAnimationKnowledgeDemoComponent {
     animationPoints = [
-        {
-            point: 'animation-name',
-            exampleIndex: 0,
-            className: 'example-1'
-        },
+        { point: 'animation-name' },
         { point: 'animation-duration' },
         { point: 'animation-timing-function' },
         { point: 'animation-delay' },
@@ -38,18 +34,37 @@ export class JigsawAnimationKnowledgeDemoComponent {
     description: string = "";
 }
 
+@Directive()
+export abstract class KnowledgeToolsBase {
+    handleAnimation(exampleIndex: number, classList?: string[]) {
+        if (exampleIndex === undefined) {
+            return
+        }
+        const ele = <HTMLElement>document.getElementById(`example-${exampleIndex}`);
+        ele.setAttribute("class", "")
+        ele.offsetWidth;
+        classList.forEach(name => {
+            ele.classList.add(name)
+        })
+        console.log(ele)
+    }
+}
+
 @Component({
     selector: 'qa',
     templateUrl: 'qa.html'
 })
-export class KnowledgeQA {
+export class KnowledgeQA extends KnowledgeToolsBase {
     @Input()
     index: number = 0;
+
+    _$showAnswer: boolean = false;
 
     animationQA = [
         {
             question: "一个CSS动画效果想要出现，必不可少的基本单元有哪些？",
-            answer: "动画名称，动画时间，animation属性和<code>@keyframes</code>规则。"
+            answer: "动画名称，动画时间，animation属性和<code>@keyframes</code>规则。",
+            action: { exampleIndex: 0, classList: ['example-1'] }
         },
         {
             question: `为什么<code>@keyframes</code>后面有个's'？`,
@@ -62,12 +77,15 @@ export class KnowledgeQA {
     selector: 'tips',
     templateUrl: 'tips.html',
 })
-export class KnowledgeTips {
+export class KnowledgeTips extends KnowledgeToolsBase {
     @Input()
     index: number = 0;
 
     animationTips = [
-        { message: 'animation属性支持同时应用多个动画规则。所以实现多种动画效果时，正确的做法是分隔设置。' }
+        {
+            message: `animation属性支持同时应用多个动画规则。<br/>实现多种动画效果时，正确的做法是分隔设置。`,
+            action: { exampleIndex: 1, classList: ['example-2'] }
+        }
     ]
 }
 
@@ -75,19 +93,7 @@ export class KnowledgeTips {
     selector: 'points',
     templateUrl: 'points.html',
 })
-export class KnowledgePoints {
+export class KnowledgePoints extends KnowledgeToolsBase {
     @Input()
     points: Array<any>;
-
-    handleAnimation(exampleIndex: number, className?: string) {
-        if (exampleIndex === undefined) {
-            return
-        }
-        const ele = <HTMLElement>document.querySelectorAll(".example-box")[exampleIndex];
-        ele.classList.remove(className);
-        ele.offsetWidth;
-        ele.classList.add(className)
-        console.log(ele)
-    }
-
 }
