f45e09489b54baaef78715fb257a07fd4730dfbb
周庆10229428 <zhou.qing4@zte.com.cn>
2022-05-10 11:07:57 +0800

    ...

--- src/app/demo/pc/tag/selectable/demo.module.ts
+++ src/app/demo/pc/tag/selectable/demo.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
-import {JigsawTagModule, JigsawButtonModule} from "jigsaw/public_api";
+import {JigsawTagModule, JigsawButtonModule, JigsawHeaderModule} from "jigsaw/public_api";
 import {TagSelectableDemoComponent} from './demo.component';
 import {JigsawDemoDescriptionModule} from "app/demo-description/demo-description";
 import {CommonModule} from "@angular/common";
 
 @NgModule({
-    imports: [JigsawTagModule, JigsawDemoDescriptionModule, JigsawButtonModule, CommonModule],
+    imports: [JigsawTagModule, JigsawDemoDescriptionModule, JigsawButtonModule, CommonModule, JigsawHeaderModule],
     declarations: [TagSelectableDemoComponent],
     exports: [TagSelectableDemoComponent]
 })
