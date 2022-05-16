be374a326deea65a0cf3b6a6f2f8ac9d6ebe116a
谭子扬10293829 <tan.ziyang@zte.com.cn>
2022-05-16 20:07:35 +0800

    amd
    Change-Id: Ia2a905ce163ac971ff8d6324327639e2bd55e431

--- src/app/demo/pc/novice-guide/basic/demo.component.ts
+++ src/app/demo/pc/novice-guide/basic/demo.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { SimpleTreeData } from 'jigsaw/public_api';
-import { noviceGuide } from 'novice-guide/src/novice-guide';
+import { SingularNoviceGuide } from 'novice-guide/src/novice-guide';
 
 @Component({
     templateUrl: "./demo.component.html",
     styleUrls: ["./demo.component.css"]
 })
-export class JigsawNoviceGuideBasicDemoComponent implements OnInit {
+export class JigsawNoviceGuideBasicDemoComponent implements OnInit, AfterViewInit {
     public navData: SimpleTreeData = new SimpleTreeData();
 
     constructor() {
@@ -27,23 +27,110 @@ export class JigsawNoviceGuideBasicDemoComponent implements OnInit {
         this.navData.fromXML(xmlData);
     }
 
-    guideData = [
-        { tagName: 'li', id: "guide1", notice: '这是一条新手指引' },
-        { tagName: 'div', property1: { property: 'innerText', value: '标准图标2' } }
-    ];
+    // singularGuideData: SingularNoviceGuide[] = [
+    //     { tagName: 'li', id: "guide1", notice: '这是一条新手指引', version: '0.0.1' },
+    //     { tagName: 'div', property1: { property: 'innerText', value: '标准图标2' }, notice: '这是一条新手指引', version: '0.0.1' }
+    // ];
+
+    singularGuideData: SingularNoviceGuide = { tagName: 'li', id: "guide1", notice: '这是一条新手指引', version: '0.0.1' };
+
+    singularGuideData2: SingularNoviceGuide = { tagName: 'div', property1: { property: 'innerText', value: '标准图标2' }, notice: '这是一条新手指引', version: '0.0.1' }
+
+    test(data) {
+        const tagName = data.tagName ? data.tagName.toUpperCase() : '';
+        const id = data.id ? '#' + data.id : '';
+        const classes = data.classes ? data.id : '';
+        const selector = `${tagName}${id}`;
+        const queryResult = document.body.querySelectorAll(selector);
+        console.log(queryResult)
+
+        if (queryResult.length === 1) {
+            const { left, top, width, height } = queryResult[0].getBoundingClientRect();
+            const centerX = left + width / 2;
+            const centerY = top + height / 2;
+            console.log(queryResult[0], left, top, width, height)
+            let guideEle = document.createElement('div');
+
+            document.body.appendChild(guideEle);
+            guideEle.style.position = "absolute";
+            guideEle.style.top = centerY + 'px';
+            guideEle.style.left = centerX + 'px';
+            guideEle.style.width = "40px";
+            guideEle.style.height = "40px";
+            guideEle.style.background = "cyan";
+
+            const observer = new IntersectionObserver(entries => {
+                console.log(entries);
+
+            })
+            observer.observe(queryResult[0])
+
+        } else {
+            const mutationObserver = new MutationObserver(entries => {
+
+                const addedNodes = entries.filter(m => m.addedNodes?.length > 0);
+                if (addedNodes.length == 0) {
+                    return;
+                }
+                const filterResult = addedNodes.filter(node => {
+                    const tagFilter = node.target.nodeName === tagName;
+                    const idFilter = node.target["id"] === id;
+                    const classFilter = node.target["classList"] === classes;
+                    const property1Filter = node.target[data.property1.property] === data.property1.value;
+                    // console.log(data.property1)
+                    return tagFilter && idFilter && property1Filter;
+                })
+                console.log(filterResult[0]);
+                console.log(filterResult[0].target);
+                // if (filterResult.length === 1) {
+                //     const { left, top, width, height } = filterResult[0].target.getBoundingClientRect();
+                //     const centerX = left + width / 2;
+                //     const centerY = top + height / 2;
+                // }
+            })
+            mutationObserver.observe(document.body, { childList: true, subtree: true })
+        }
+    }
 
     ngOnInit() {
-        // noviceGuide([])
-        this.guideData.forEach(guide => {
+        // console.log(document.body.querySelectorAll("div"))
+        // console.log(document.body.querySelectorAll("li"))
 
-        })
+        /* 在调用方法后，查询页面匹配元素 */
+        /* 拼接selector */
 
-        const mutationObserver = new MutationObserver(entries => {
-            console.log(entries);
-            
-        })
+        // 可以直接在获取数据之后处理
 
-        mutationObserver.observe(document.body, { childList: true, subtree: true })
+
+        // this.guideData.forEach(guide => {
+        //     /* 拼接selector */
+        //     const tagName = guide.tagName ? guide.tagName : '';
+        //     const id = guide.id ? guide.id : '';
+        //     const classes = guide.classes ? guide.id : '';
+        // })
+        this.test(this.singularGuideData);
+        this.test(this.singularGuideData2);
+
+        // window.onload = function () {
+        //     console.log(123123)
+        //     const queryResult = document.body.querySelectorAll('li#guide1');
+        //     const { left, top, width, height } = queryResult[0].getBoundingClientRect();
+        //     const centerX = left + width / 2;
+        //     const centerY = top + height / 2;
+        //     console.log(centerX, centerY)
+        // }
+    }
+
+    ngAfterViewInit() {
+        // this.xy()
+    }
+
+    xy() {
+        const queryResult = document.body.querySelectorAll('li#guide1');
+        const { left, top, width, height } = queryResult[0].getBoundingClientRect();
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+        console.log(queryResult[0], left, top, width, height)
     }
 
 
