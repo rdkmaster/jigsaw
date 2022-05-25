cd99c1030bc13b8c4db2988be3e7005711cebfca
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2022-05-25 15:23:01 +0800

    [优化] toast组件默认宽度对齐规范至256px；解决组件圆角有误的问题；关联了@6187

--- src/app/demo/pc/toast/full/demo.component.ts
+++ src/app/demo/pc/toast/full/demo.component.ts
@@ -10,7 +10,7 @@ export class ToastFullDemoComponent {
     message = '这是一个Toast提示框！';
     icon = undefined;
     timeout = 80;
-    width = 200;
+    width = 256;
 
     show() {
         JigsawToast.show(this.message, { icon: this.icon, timeout: this.timeout * 1000, width: this.width })
