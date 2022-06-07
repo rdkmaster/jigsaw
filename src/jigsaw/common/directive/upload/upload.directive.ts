9068e356c37733072338cec362b88955135f6efa
陈旭10045812 <chen.xu8@zte.com.cn>
2022-06-07 17:32:25 +0800

    ...

--- src/jigsaw/common/directive/upload/upload.directive.ts
+++ src/jigsaw/common/directive/upload/upload.directive.ts
@@ -206,8 +206,8 @@ export class JigsawUploadDirective extends JigsawUploadBase implements IUploader
         }
         this._fileInputElement.setAttribute('accept', this.fileType);
 
-        this._removeFileChangeEvent?.();
-        this._removeFileChangeEvent = this._renderer.listen(this._fileInputElement, 'change', () => {
+        this._removeFileChangeEvent = this._removeFileChangeEvent ? this._removeFileChangeEvent :
+            this._renderer.listen(this._fileInputElement, 'change', () => {
                 if (this.uploadImmediately) {
                     this.upload();
                 } else {
