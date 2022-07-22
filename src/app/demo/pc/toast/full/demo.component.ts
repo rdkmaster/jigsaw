c10d927c9c4fcc529dad2291134e1c179ff11837
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2022-07-22 09:38:16 +0800

    [优化] Toast组件支持显示多行文本，关联了@6040

--- src/app/demo/pc/toast/full/demo.component.ts
+++ src/app/demo/pc/toast/full/demo.component.ts
@@ -1,23 +1,40 @@
 import { Component } from '@angular/core';
-import { JigsawToast } from 'jigsaw/public_api';
+import {JigsawToast, ToastMessage} from 'jigsaw/public_api';
 
 @Component({
     templateUrl: './demo.component.html',
     styleUrls: ['./demo.component.css']
 })
 export class ToastFullDemoComponent {
-
+    selectedIcon = ['成功'];
     message = '这是一个Toast提示框！';
-    icon = undefined;
-    timeout = 80;
+    icon = 'iconfont iconfont-e076';
+    timeout = 4;
     width = 256;
 
     show() {
-        JigsawToast.show(this.message, { icon: this.icon, timeout: this.timeout * 1000, width: this.width })
+        const options: ToastMessage = { timeout: this.timeout * 1000, width: this.width };
+        if (this.selectedIcon[0] == '提示') {
+            JigsawToast.showInfo(this.message, options)
+        } else if (this.selectedIcon[0] == '成功') {
+            JigsawToast.showSuccess(this.message, options)
+        } else if (this.selectedIcon[0] == '失败') {
+            JigsawToast.showError(this.message, options)
+        } else if (this.selectedIcon[0] == '警告') {
+            JigsawToast.showWarn(this.message, options)
+        } else {
+            options.icon = this.icon;
+            JigsawToast.show(this.message, options);
+        }
     }
 
     makeLongMessage() {
         this.message = "这是一个有长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长文本的Toast提示框！"
+    }
+
+    makeSuperLongMessage() {
+        const long = '长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长';
+        this.message = `这是有${long}，${long}，${long}，${long}，${long}，${long}，${long}文本的Toast提示框！`;
     }
 
     // ====================================================================
