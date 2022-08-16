d351a14e9049bc13d28fd1615ffe9051ef8eba19
谭子扬10293829 <10293829@zte.intra>
2022-08-16 16:16:46 +0800

    amd
    Change-Id: I82c5ed9d76748789e7e97bf0d368fda68cb7188b

--- src/app/for-external/demo-template/demo-template.ts
+++ src/app/for-external/demo-template/demo-template.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, Input, Output, EventEmitter } from '@angular/core';
+import { Component, NgModule, Input, Output, EventEmitter, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JigsawMarkdownModule } from "../../libs/markdown/markdown";
 import { JigsawButtonBarModule, ArrayCollection } from "jigsaw/public_api";
@@ -11,22 +11,42 @@ import { JigsawButtonBarModule, ArrayCollection } from "jigsaw/public_api";
 })
 
 export class DemoTemplate {
+    constructor(private _renderer: Renderer2) {
+    }
+
+    @ViewChild('code')
+    code: ElementRef;
+
     @Input()
     public text: string = '';
+
     @Input()
     public data: object[] | '' = new ArrayCollection([
         { label: "小", size: "small" },
         { label: "中", size: "default" },
         { label: "大", size: "large" }
     ]);
+
     @Input()
     public selectedLabel: object = { label: "中", size: "default" };
+
     @Output()
     public selectedLabelChange = new EventEmitter<object>();
-    constructor() {
-    }
+
+    expand: boolean = false;
+
     onClick(size: object) {
         this.selectedLabelChange.emit(size);
+    }
+
+    showCode() {
+        this.expand = true;
+        this._renderer.setStyle(this.code.nativeElement, "height", this.code.nativeElement.scrollHeight + 'px')
+    }
+
+    hideCode() {
+        this.expand = false;
+        this._renderer.setStyle(this.code.nativeElement, "height", 0)
     }
 }
 
