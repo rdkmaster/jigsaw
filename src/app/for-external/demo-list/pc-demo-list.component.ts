3437dddbd338712a2d16d1ff05e5d8eed0f0cc03
谭子扬10293829 <10293829@zte.intra>
2022-08-18 16:26:48 +0800

    amd
    Change-Id: I65dde9d59bb5aa1e46db4f6d347ad760b1312a8f

--- src/app/for-external/demo-list/pc-demo-list.component.ts
+++ src/app/for-external/demo-list/pc-demo-list.component.ts
@@ -6,45 +6,12 @@ import { routerConfigPC, componentGroup } from "../router-config";
     templateUrl: './pc-demo-list.component.html',
     styleUrls: ['./pc-demo-list.component.scss']
 })
-export class PCDemoListComponent implements OnInit {
-    public floatOptions = { posType: PopupPositionType.fixed };
+export class PCDemoListComponent {
     public routerGroup: any[] = DemoListManager.fullRouterConfig;
-    public selectedItems: any[];
-    // public jComponents: string[] = this.routes.map(item => item.path);
-
-    // showHideDemos(selectedItems: string[]) {
-    //     this.routes.forEach(item => {
-    //         item.hidden = !selectedItems.find(component => component === item.path)
-    //     })
-    //     if (!selectedItems.length) {
-    //         this.routes.forEach(item => {
-    //             item.hidden = false
-    //         })
-    //     }
-    //     localStorage.setItem('jigsaw-demo-show-list', JSON.stringify([...selectedItems]));
-    // }
 
     getUrl(router): string {
         return `/${router.path}`;
     }
-
-    getDesc(childRouter): string {
-        return childRouter.hasOwnProperty('desc') ? childRouter.desc : childRouter.path;
-    }
-
-    ngOnInit(): void {
-        this.selectedItems = JSON.parse(localStorage.getItem('jigsaw-demo-show-list')) || [];
-        // this.showHideDemos(this.selectedItems);
-    }
-
-    // onOpenChange(open: boolean) {
-    //     if (open) {
-    //         return;
-    //     }
-    //     if (this.jComponents.length == this.selectedItems.length) {
-    //         this.selectedItems = [];
-    //     }
-    // }
 }
 
 export class DemoListManager {
@@ -60,13 +27,15 @@ export class DemoListManager {
             { groupName: componentGroup.schema, routers: [] },
             { groupName: componentGroup.other, routers: [] }
         ]
-        console.log(routerConfigPC)
         routerConfigPC.forEach(router => {
             if (router.group === undefined) {
                 routerGroup.find(item => item.groupName === componentGroup.other).routers.push(router);
             } else {
                 routerGroup.find(item => item.groupName === router.group).routers.push(router);
             }
+        })
+        routerGroup.forEach(group => {
+            group.routers.sort((item1, item2) => item1.path.localeCompare(item2.path))
         })
         return routerGroup;
     }
