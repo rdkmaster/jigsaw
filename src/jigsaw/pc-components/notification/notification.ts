e91cb37e7ebaa6f70e0cb96bd0b1305518ba0b26
陈旭 <chen.xu8@zte.com.cn>
2022-10-17 17:11:09 +0800

    [优化] notification的代码，去掉没必要的共有属性_popupInfo，改为私有属性

--- src/jigsaw/pc-components/notification/notification.ts
+++ src/jigsaw/pc-components/notification/notification.ts
@@ -253,26 +253,19 @@ export class JigsawNotification extends AbstractDialogComponentBase implements O
         this._position = NotificationPosition[v];
     }
 
-    private _popupInfoValue: PopupInfo;
+    private _popupInfo: PopupInfo;
 
-    /**
-     * @internal
-     */
-    get _popupInfo(): PopupInfo {
-        return this._popupInfoValue;
-    }
-
-    set _popupInfo(value: PopupInfo) {
+    private _setPopupInfo(value: PopupInfo): void {
         if (!value) {
             return;
         }
         // 如果有存在，则先取消旧的订阅
-        if (this._popupInfoValue?.answer) {
-            this._popupInfoValue.answer.complete();
+        if (this._popupInfo?.answer) {
+            this._popupInfo.answer.complete();
         }
 
-        this._popupInfoValue = value;
-        this._popupInfoValue.answer.subscribe(answer => this._$close(answer));
+        this._popupInfo = value;
+        this._popupInfo.answer.subscribe(answer => this._$close(answer));
         this._$onLeave();
     }
 
@@ -297,9 +290,9 @@ export class JigsawNotification extends AbstractDialogComponentBase implements O
      * @internal
      */
     public _$close(answer?: ButtonInfo) {
-        if (this._popupInfoValue) {
-            this._popupInfoValue.answer.unsubscribe();
-            this._popupInfoValue.dispose();
+        if (this._popupInfo) {
+            this._popupInfo.answer.unsubscribe();
+            this._popupInfo.dispose();
         }
 
         if (this._timeout) {
@@ -313,13 +306,13 @@ export class JigsawNotification extends AbstractDialogComponentBase implements O
         const removeListener = this.renderer.listen(this.elementRef.nativeElement, 'animationend',
             () => {
                 removeListener();
-                if (!this._popupInfoValue) {
+                if (!this._popupInfo) {
                     return;
                 }
 
                 JigsawNotification.reposition(this._position);
                 const instances = notificationInstances[NotificationPosition[this._position]];
-                const idx = instances.indexOf(this._popupInfoValue);
+                const idx = instances.indexOf(this._popupInfo);
                 if (idx != -1) {
                     instances.splice(idx, 1);
                 } else {
@@ -336,7 +329,7 @@ export class JigsawNotification extends AbstractDialogComponentBase implements O
                     JigsawNotification._removeResizeListener = null;
                 }
 
-                this._popupInfoValue = null;
+                this._popupInfo = null;
                 this._timeout = null;
                 this._callbackContext = null;
                 this._callback = null;
@@ -515,7 +508,7 @@ export class JigsawNotification extends AbstractDialogComponentBase implements O
             disposeOnRouterChanged: !!opt.disposeOnRouterChanged,
         };
         const popupInfo = PopupService.instance.popup(JigsawNotification, popupOptions, initData);
-        (<JigsawNotification>popupInfo.instance)._popupInfo = popupInfo;
+        (<JigsawNotification>popupInfo.instance)._setPopupInfo(popupInfo);
         notificationInstances[NotificationPosition[opt.position]].push(popupInfo);
 
         const onStableSubscription = InternalUtils.zone.onStable.asObservable().pipe(take(1)).subscribe(() => {
