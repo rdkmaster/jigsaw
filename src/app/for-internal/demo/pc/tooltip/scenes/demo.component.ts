7a3cf37f3aa8676d7f47c48767442eda150995b5
谭子扬10293829 <tan.ziyang@zte.com.cn>
2022-10-31 14:36:51 +0800

    amd

--- src/app/for-internal/demo/pc/tooltip/scenes/demo.component.ts
+++ src/app/for-internal/demo/pc/tooltip/scenes/demo.component.ts
@@ -1,9 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, TemplateRef } from "@angular/core";
+import { PopupInfo, PopupService } from "jigsaw/public_api";
 
 @Component({
     templateUrl: './demo.component.html'
 })
 export class TooltipScenesDemoComponent {
+    constructor(private popupService:PopupService){
+
+    }
+    public dialogInfo: PopupInfo;
+    public popupDialog(ele: TemplateRef<any>) {
+        this.dialogInfo = this.popupService.popup(ele);
+    }
+
+    public closeDialog(){
+        if (this.dialogInfo) {
+            this.dialogInfo.dispose();
+            this.dialogInfo = null;
+        }
+    }
 
     // ====================================================================
     // ignore the following lines, they are not important to this demo
