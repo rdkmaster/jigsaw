cb847743cda3d2e9b8f359e37ef505ad182cb8d9
陈旭10045812 <chen.xu8@zte.com.cn>
2022-11-01 19:01:03 +0800

    ...

--- build/publish.sh
+++ build/publish.sh
@@ -4,10 +4,11 @@ scriptDir=$(cd `dirname $0`; pwd);
 
 cd $scriptDir/..
 jigsawRepo=`pwd`
-cd $jigsawRepo/..
+cd $jigsawRepo/../ued-site-latest
 uedSiteRepo=`pwd`
 
 target=$1
+server=$2
 
 function checkRepo() {
     local repo=$1
@@ -17,11 +18,11 @@ function checkRepo() {
     fi
     cd $repo
     local code=0
-#    git status | grep "分支 master" > /dev/null
-#    if [ "$?" != "0" ]; then
-#        echo "Error: the repo $repo is not in master branch"
-#        exit 1
-#    fi
+    git status | grep "分支 master" > /dev/null
+    if [ "$?" != "0" ]; then
+        echo "Error: the repo $repo is not in master branch"
+        exit 1
+    fi
     git status | grep "干净的工作区" > /dev/null
     if [ "$?" != "0" ]; then
         echo "Error: the repo $repo is not clean"
@@ -35,16 +36,12 @@ function checkRepo() {
     echo "Great! the repo $repo is ready to work!"
 }
 
-function publishUed() {
-    checkRepo $jigsawRepo
-    checkRepo $uedSiteRepo
-
-    cd $jigsawRepo
-    node build/build.js jigsaw-app-external prod
-    if [ "$?" != "0" ]; then
-        echo "Error: failed to build jigsaw external app!"
+function publishToGitee() {
+    if [[ "$GITEE_USER" == "" || "$GITEE_PWD" == "" ]]; then
+        echo "Error: invalid gitee user or password, need this info to auto deploy ued site"
         exit 1
     fi
+    checkRepo $uedSiteRepo
 
     cd $uedSiteRepo
     git pull origin master
@@ -52,12 +49,55 @@ function publishUed() {
     cp -r $jigsawRepo/dist/* ./
     git add .
     git commit -m 'auto updated'
-#    git push origin master
+    git push origin master
     if [ "$?" != "0" ]; then
         echo "Error: failed to push built files to gitee.com!"
         exit 1
     fi
-    echo "Success to publish jigsaw external app to gitee.com!"
+    echo "Success to push files jigsaw external app to gitee.com!"
+
+    cd $jigsawRepo
+    node build/tools/deploy-ued-sites.js
+    if [ "$?" != "0" ]; then
+        echo "Error: failed to deploy jigsaw external web site!"
+        exit 1
+    fi
+    echo "Success to publish jigsaw external web site to gitee.com!"
+}
+
+function publishToRDK() {
+    if [ "$AWADE_ALL_WORK_DIR" == "" ]; then
+        echo "Error: invalid AWADE_ALL_WORK_DIR environment variable!"
+        exit 1
+    fi
+    local deployDir=$AWADE_ALL_WORK_DIR/ued-site/code-base-mirror/web-site/master
+    if [ ! -d $deployDir ]; then
+        echo "Error: invalid dir $deployDir!"
+        exit 1
+    fi
+    rm -fr $deployDir/*
+    cp -r $jigsawRepo/dist/* $deployDir/
+    echo "Success to publish jigsaw external web site to $deployDir!"
+}
+
+function publishUed() {
+    if [[ "$server" != "gitee" && "$server" != "rdk" ]]; then
+        echo "Error: invalid target server, need gitee/rdk"
+        exit 1
+    fi
+    checkRepo $jigsawRepo
+
+    cd $jigsawRepo
+    node build/build.js jigsaw-app-external prod
+    if [ "$?" != "0" ]; then
+        echo "Error: failed to build jigsaw external app!"
+        exit 1
+    fi
+    if [ "$server" == "gitee" ]; then
+        publishToGitee
+    else
+        publishToRDK
+    fi
 }
 
 function getNewVersion() {
@@ -79,6 +119,11 @@ function getNewVersion() {
 
 function publishNpm() {
     checkRepo $jigsawRepo
+    npm whoami
+    if [ "$?" != "0" ]; then
+        echo "Error: npm is not logged in, use npm login and tray again"
+        exit
+    fi
 
     cd $jigsawRepo
     ./node_modules/.bin/gulp build:jigsaw:clean
@@ -90,7 +135,7 @@ function publishNpm() {
     sed -i 's#"version":\s*".\+"#"version":"'$newVersion'"#' dist/@rdkmaster/jigsaw/package.json
 
     echo "publishing jigsaw lib to npm registry with new version $newVersion ..."
-#    npm publish --tag latest --access public dist/@rdkmaster/jigsaw
+    npm publish --tag latest --access public dist/@rdkmaster/jigsaw
     if [ "$?" != "0" ]; then
         echo "Error: failed to publish jigsaw lib to npm registry!"
         exit 1
@@ -103,7 +148,7 @@ if [ "$target" == "ued" ]; then
 elif [ "$target" == "npm" ]; then
     publishNpm
 else
-    echo "Error: invalid publish target, need used/npm"
+    echo "Error: invalid publish target, need ued/npm"
     exit 1
 fi
 
