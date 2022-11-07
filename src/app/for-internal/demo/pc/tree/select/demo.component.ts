fbc34cffd9edf2cc55c027e1c8858c488e60dbf4
谭子扬10293829 <tan.ziyang@zte.com.cn>
2022-11-07 14:11:28 +0800

    amd

--- src/app/for-internal/demo/pc/tree/select/demo.component.ts
+++ src/app/for-internal/demo/pc/tree/select/demo.component.ts
@@ -1,5 +1,5 @@
-import {AfterViewInit, Component, ViewChild} from "@angular/core";
-import {SimpleTreeData, JigsawTreeExt} from "jigsaw/public_api";
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { SimpleTreeData, JigsawTreeExt } from "jigsaw/public_api";
 
 @Component({
     templateUrl: './demo.component.html'
@@ -22,22 +22,22 @@ export class ZtreeSelectDemoComponent implements AfterViewInit {
                         label: "父节点11 - 展开",
                         open: true,
                         nodes: [
-                            {label: "叶子节点111", checked: true},
-                            {label: "叶子节点112", checked: true},
-                            {label: "叶子节点113"},
-                            {label: "叶子节点114"}
+                            { label: "叶子节点111", checked: true },
+                            { label: "叶子节点112", checked: true },
+                            { label: "叶子节点113" },
+                            { label: "叶子节点114" }
                         ]
                     },
                     {
                         label: "父节点12 - 折叠",
                         nodes: [
-                            {label: "叶子节点121"},
-                            {label: "叶子节点122"},
-                            {label: "叶子节点123"},
-                            {label: "叶子节点124"}
+                            { label: "叶子节点121" },
+                            { label: "叶子节点122" },
+                            { label: "叶子节点123" },
+                            { label: "叶子节点124" }
                         ]
                     },
-                    {label: "父节点13 - 没有子节点", isParent: true}
+                    { label: "父节点13 - 没有子节点", isParent: true }
                 ]
             },
             {
@@ -46,33 +46,33 @@ export class ZtreeSelectDemoComponent implements AfterViewInit {
                     {
                         label: "父节点21 - 展开", open: true,
                         nodes: [
-                            {label: "叶子节点211"},
-                            {label: "叶子节点212"},
-                            {label: "叶子节点213"},
-                            {label: "叶子节点214"}
+                            { label: "叶子节点211" },
+                            { label: "叶子节点212" },
+                            { label: "叶子节点213" },
+                            { label: "叶子节点214" }
                         ]
                     },
                     {
                         label: "父节点22 - 折叠",
                         nodes: [
-                            {label: "叶子节点221"},
-                            {label: "叶子节点222"},
-                            {label: "叶子节点223"},
-                            {label: "叶子节点224"}
+                            { label: "叶子节点221" },
+                            { label: "叶子节点222" },
+                            { label: "叶子节点223" },
+                            { label: "叶子节点224" }
                         ]
                     },
                     {
                         label: "父节点23 - 折叠",
                         nodes: [
-                            {label: "叶子节点231"},
-                            {label: "叶子节点232"},
-                            {label: "叶子节点233"},
-                            {label: "叶子节点234"}
+                            { label: "叶子节点231" },
+                            { label: "叶子节点232" },
+                            { label: "叶子节点233" },
+                            { label: "叶子节点234" }
                         ]
                     }
                 ]
             },
-            {label: "父节点3 - 没有子节点", isParent: true}
+            { label: "父节点3 - 没有子节点", isParent: true }
 
         ])
     }
@@ -84,7 +84,7 @@ export class ZtreeSelectDemoComponent implements AfterViewInit {
     }
 
     public cancelSelected(node) {
-        this.treeExt.checkNode(node, false, false);
+        this.treeExt.checkNode(node, false, true);
         this.selected = this.treeExt.getCheckedNodes(true);
     }
 
@@ -98,6 +98,7 @@ export class ZtreeSelectDemoComponent implements AfterViewInit {
         }
 
         this.selected = this.treeExt.getCheckedNodes(true);
+        console.log(this.selected)
     }
 
     // ====================================================================
