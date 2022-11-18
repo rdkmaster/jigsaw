864d4eff7e40cc4b7f2cb1b190b5fb78aab7a029
韩峥10322371 <han.zheng@zte.com.cn>
2022-11-18 17:46:36 +0800

    amd

--- src/app/for-external/demo/upload/profile-type/demo.component.ts
+++ src/app/for-external/demo/upload/profile-type/demo.component.ts
@@ -8,7 +8,7 @@ import {AsyncDescription} from "../../../template/demo-template/demo-template";
 export class UploadProfileTypeDemoComponent extends AsyncDescription {
     public demoPath = "demo/upload/profile-type";
 
-    public fileType = '.txt';
+    public urls: string[];
 
     public uploadStart($event) {
         console.log("uploadStart触发了", $event)
@@ -19,6 +19,9 @@ export class UploadProfileTypeDemoComponent extends AsyncDescription {
     }
 
     public uploadComplete($event) {
+        this.urls = $event
+            .filter(e => e.state == "success")
+            .map(f => f.url);
         console.log("uploadComplete触发了", $event)
     }
 }
