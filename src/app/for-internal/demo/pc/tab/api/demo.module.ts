4e754ec759cd0d976c35989ec3388d1b7f9000ab
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2022-11-22 16:55:13 +0800

    [新增] 根据用户使用场景新增一些复杂tab组件demo，解决了@5683

--- src/app/for-internal/demo/pc/tab/api/demo.module.ts
+++ src/app/for-internal/demo/pc/tab/api/demo.module.ts
@@ -4,6 +4,8 @@ import {JigsawButtonModule, JigsawInputModule, JigsawTabsModule} from "jigsaw/pu
 import {JigsawDemoDescriptionModule} from "app/for-internal/description/demo-description";
 import {DynamicTabDemoComponent} from './demo.component';
 import {TabContentModule} from "./tabContent/tab-content.module";
+import { TableContentModule } from './tableContent/table-content.module';
+import { TableInTabModule } from './tableInTabContent/table-in-tab.module';
 
 /* #for-live-demo-only#
 const routes = [{
@@ -13,7 +15,7 @@ const routes = [{
 @NgModule({
     imports: [
         TabContentModule, JigsawTabsModule, JigsawButtonModule, JigsawDemoDescriptionModule,
-        JigsawInputModule, RouterModule/* #for-live-demo-only# .forRoot(routes) */
+        JigsawInputModule, TableContentModule, TableInTabModule, RouterModule/* #for-live-demo-only# .forRoot(routes) */
     ],
     declarations: [DynamicTabDemoComponent],
     exports: [DynamicTabDemoComponent]
