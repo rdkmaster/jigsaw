a2d3a440b217a8e3b22a4753f60b639cc445f1d5
谭子扬10293829 <tan.ziyang@zte.com.cn>
2022-11-29 15:09:53 +0800

    amd

--- src/app/for-external/ued/icons/icons.ts
+++ src/app/for-external/ued/icons/icons.ts
@@ -16,6 +16,10 @@ export class IconsDemoComponent implements OnInit {
 
     public _$currentGroupIndex: number = -1;
 
+    private _commonIcon = ['e1fe', 'e2b0', 'e2b6', 'e179', 'e36f', 'e33a', 'e347', 'e14b', 'e1c7', 'e1c8', 'e4f7', 'e015', 'e2a8', 'e24c', 'e24f', 'e0a1',
+        'e177', 'e35d', 'e292', 'e231', 'e2f6', 'e35b', 'e366', 'e3dd', 'e4bc', 'e23f', 'e1a5', 'e142', 'e34a', 'e294', 'e295', 'e44f', 'e7f0',
+        'e7f1', 'e45e', 'e45f'];
+
     public _$clickGroupTag(i: number) {
         this._$currentGroupIndex = i;
         this._$searchIcons(this._$searchKeyword);
@@ -28,6 +32,8 @@ export class IconsDemoComponent implements OnInit {
             this._$currentIcons = this.icons.filter(icon => {
                 return icon.iconGroup == groupId
             })
+        } else if (this._$currentGroupIndex == -2) {
+            this._$currentIcons = this.icons.filter(icon => this._commonIcon.includes(icon.iconCode.toLowerCase()));
         } else {
             this._$currentIcons = this.icons;
         }
@@ -57,8 +63,19 @@ export class IconsDemoComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.iconGroup = iconfontInfo.group;
-        this.icons = iconfontInfo.icons;
+        const exclusiveGroup = [];
+        this.iconGroup = iconfontInfo.group.filter(g => {
+            if (g.groupName.indexOf("禁用") == -1) {
+                return true;
+            } else {
+                exclusiveGroup.push(g.id)
+            }
+        });
+
+        this.icons = iconfontInfo.icons.filter(icon => {
+            return !exclusiveGroup.includes(icon.iconGroup);
+        });
+        
         this._$searchIcons('');
         window.scrollTo(0, 0);
     }
