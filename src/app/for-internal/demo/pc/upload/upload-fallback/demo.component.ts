fc70452e75859975c391fb18a5c028694e0b38fc
谭子扬10293829 <tan.ziyang@zte.com.cn>
2023-01-12 15:14:40 +0800

    amd

---
+++ src/app/for-internal/demo/pc/upload/upload-fallback/demo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './demo.component.html'
+})
+export class UploadFallbackDemoComponent {
+
+    // ====================================================================
+    // ignore the following lines, they are not important to this demo
+    // ====================================================================
+    summary: string = 'fallback组件非必要不维护';
+    description: string = '';
+}
