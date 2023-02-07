05924ee402c74a6c3787650ff947f99f969c28d6
陈旭10045812 <chen.xu8@zte.com.cn>
2023-02-07 16:58:28 +0800

    ...

--- src/app/for-internal/demo/pc/menu/max-height/demo.component.ts
+++ src/app/for-internal/demo/pc/menu/max-height/demo.component.ts
@@ -6,89 +6,41 @@ import { SimpleTreeData, JigsawMenu } from "jigsaw/public_api";
     styleUrls: [`./demo.component.css`]
 })
 export class MenuMaxHeightDemo {
-    public menuData: any[] = this.initMenuData();
+    public menuData: SimpleTreeData = this.initMenuData();
 
-    private initMenuData(): any[] {
-        const data = [
-            {
-                "label": "文件", data: new SimpleTreeData()
-            },
-            {
-                "label": "编辑", data: new SimpleTreeData()
-            },
-            {
-                "label": "视图", data: new SimpleTreeData()
-            },
-            {
-                "label": "导航", data: new SimpleTreeData()
-            }
-        ];
-        data[0].data.fromXML(`
+    private initMenuData(): SimpleTreeData {
+        const data = new SimpleTreeData();
+        const lotsMenuItems = this._createMenuItems();
+        data.fromXML(`
             <node>
                 <node label="新建" iconUnicode="e9dd">
-                    <node label="工程..." subTitle="Ctrl+Shift+P"></node>
-                    <node></node>
-                    <node label="HTML文件" iconUnicode="e5a2" subTitle="Ctrl+Shift+H"></node>
-                    <node label="CSS文件" iconUnicode="e5a1" subTitle="Ctrl+Shift+C"></node>
-                    <node label="JS文件" iconUnicode="e6a6" subTitle="Ctrl+Shift+J"></node>
-                    <node></node>
-                    <node label="退出"></node>
+                    ${lotsMenuItems}
                 </node>
-                <node label="打开..." icon="iconfont iconfont-e4e4"></node>
                 <node label="打开" subTitle="最近文件">
-                    <node label="project1" icon="iconfont iconfont-e40d"></node>
-                    <node label="project2" icon="iconfont iconfont-e40d"></node>
-                    <node label="project3" icon="iconfont iconfont-e40d"></node>
-                    <node></node>
-                    <node label="file1.js" icon="iconfont iconfont-e6a6"></node>
-                    <node label="file2.html" icon="iconfont iconfont-e5a2"></node>
-                    <node label="file3.css" icon="iconfont iconfont-e5a1"></node>
+                    ${lotsMenuItems}
                 </node>
-                <node label="保存" icon="iconfont iconfont-ea2a" subTitle="Ctrl+S"></node>
-                <node label="另存为..." subTitle="Ctrl+Shift+S"></node>
+                <node label="保存" icon="iconfont iconfont-ea2a" subTitle="Ctrl+S">
+                    ${lotsMenuItems}
+                </node>
                 <node></node>
                 <node label="退出"></node>
             </node>
         `);
-        data[1].data.fromXML(`
-            <node>
-                <node label="撤回" icno="iconfont iconfont-e40f"></node>
-                <node label="重做" icno="iconfont iconfont-e341"></node>
-                <node></node>
-                <node label="剪切" icon="iconfont iconfont-e161" subTitle="Ctrl+X"></node>
-                <node label="拷贝" icon="iconfont iconfont-e9c1" subTitle="Ctrl+C"></node>
-                <node label="粘贴" subTitle="Ctrl+V"></node>
-                <node></node>
-                <node label="搜索" icon="iconfont iconfont-e91a"></node>
-                <node label="替换"></node>
-            </node>
-        `);
-        data[2].data.fromXML(`
-            <node>
-                <node label="窗口">
-                    <node label="工程" icon="iconfont iconfont-e140"></node>
-                    <node label="最爱"></node>
-                    <node label="Debug" icon="iconfont iconfont-e140"></node>
-                </node>
-                <node></node>
-                <node label="工具栏" icon="iconfont iconfont-e140"></node>
-                <node label="状态栏"></node>
-            </node>
-        `);
-        data[3].data.fromXML(`
-            <node>
-                <node label="类..."></node>
-                <node label="文件..."></node>
-                <node label="符号..."></node>
-                <node></node>
-                <node label="后退" icon="iconfont iconfont-e9c5"></node>
-                <node label="前进" icon="iconfont iconfont-e9c6"></node>
-                <node></node>
-                <node label="书签" icon="iconfont iconfont-e101"></node>
-            </node>
-        `);
-
         return data;
+    }
+
+    private _createMenuItems(level: number = 0): string {
+        let lotsMenuItems = '';
+        for (let i = 0; i < 32; i++) {
+            lotsMenuItems += `<node label="菜单${i}" iconUnicode="e5a2" subTitle="Ctrl+Shift+H"></node>`;
+            if (i % 5 == 4) {
+                if (level < 3) {
+                    lotsMenuItems += `<node label="多级菜单">${this._createMenuItems(level+1)}</node>`;
+                }
+                lotsMenuItems += '<node></node>';
+            }
+        }
+        return lotsMenuItems;
     }
 
     public menuSelect(node: SimpleTreeData) {
@@ -98,6 +50,6 @@ export class MenuMaxHeightDemo {
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
-    summary: string = '本demo主要用于展示菜单的各种典型用法，起到抛砖引玉的目的';
+    summary: string = '本demo主要用于展示菜单项很多的时候，如何通过max-height属性来规避菜单界面太高的问题';
     description: string = '';
 }
