78e979ec5e6d5c994da44a10881965f62f3e8590
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2023-02-14 15:15:29 +0800

    [优化] graph组件无数据添加“暂无数据”文字，关联@5344

--- src/jigsaw/pc-components/graph/index.ts
+++ src/jigsaw/pc-components/graph/index.ts
@@ -5,13 +5,26 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import { JigsawGraph } from './graph';
 import {JigsawGraphDownloadDirective} from "./graph-download.directive";
+import {TranslateHelper} from "../../common/core/utils/translate-helper";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, TranslateModule.forChild(),],
     exports: [JigsawGraph,JigsawGraphDownloadDirective],
     declarations: [JigsawGraph,JigsawGraphDownloadDirective],
 })
-export class JigsawGraphModule { }
+export class JigsawGraphModule { 
+    constructor() {
+        TranslateHelper.initI18n("jigsawGraph", {
+            zh: {
+                noData: "暂无数据"
+            },
+            en: {
+                noData: "NO DATA"
+            }
+        });
+    }
+}
 
 export * from './graph';
 export * from './graph-download.directive';
