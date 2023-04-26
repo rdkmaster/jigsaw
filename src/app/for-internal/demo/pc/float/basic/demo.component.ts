02d1f156e505bceede6fa6af2eef8d40ffb4c020
谭子扬10293829 <tan.ziyang@zte.com.cn>
2023-04-26 18:09:18 +0800

    amd

--- src/app/for-internal/demo/pc/float/basic/demo.component.ts
+++ src/app/for-internal/demo/pc/float/basic/demo.component.ts
@@ -2,20 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     templateUrl: './demo.component.html',
-    styles: [`
-        .iconfont-e9d8 {
-            margin: 20px 100px;
-        }
-
-        .jigsawFloatArea {
-            width: 150px;
-            height: 60px;
-            background: orange;
-            color: #fff;
-            text-align: center;
-            line-height: 60px;
-        }
-    `]
+    styleUrls: ['./demo.component.css']
 })
 export class FloatBasicDemo {
     // ====================================================================
