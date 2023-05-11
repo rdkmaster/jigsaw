28aea92af1a53546a3c9c9fd08f9780f4a1a2a23
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2023-05-11 19:26:35 +0800

    [优化] 优化级联菜单Demo，提供去除菜单不满足宿主宽度时右侧多余空白的示例，关联了@5184

--- src/app/for-internal/demo/pc/menu/options/demo.component.ts
+++ src/app/for-internal/demo/pc/menu/options/demo.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
 import {
     JigsawMenu, MenuTheme, SimpleNode, SimpleTreeData,
     PopupService
@@ -6,31 +6,7 @@ import {
 
 @Component({
     templateUrl: './demo.component.html',
-    styles: [`
-        .menu {
-            margin: 16px 0 8px 216px;
-            border: 1px solid #999;
-            padding: 2px 10px;
-            border-radius: 3px;
-            cursor: pointer;
-            display: inline-block;
-        }
-
-        .context-menu {
-            width: 500px;
-            height: 100px;
-            background-color: var(--bg-container);
-        }
-
-        .context-menu p {
-            text-align: center;
-            padding-top: 40px;
-        }
-
-        .message {
-            margin-bottom: 8px;
-        }
-    `]
+    styleUrls: ['./demo.component.css']
 })
 export class MenuOptionsDemo {
 
@@ -39,8 +15,11 @@ export class MenuOptionsDemo {
     public dataType: string[] = ['normal'];
     public width: number = 150;
     public height: number = 0;
+    public options = {};
+    public units = ['px', '%'];
+    public unit = 'px';
 
-    constructor( private ps: PopupService) {
+    constructor(private ps: PopupService) {
         this.data = new SimpleTreeData();
         this.changeData('normal');
     }
@@ -120,6 +99,15 @@ export class MenuOptionsDemo {
         }
     }
 
+    public autoTargetWidth: boolean = true;
+
+    public autoTargetWidthChange() {
+        this.options = this.autoTargetWidth ? {} : { size: { minWidth: null } }
+    }
+
+    public unitChange($event){
+        this.unit = $event;
+    }
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
