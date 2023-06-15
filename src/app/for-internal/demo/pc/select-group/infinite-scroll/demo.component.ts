585a8331667904612a2f4a6b8d6bfe40d49939bc
谭子扬10293829 <tan.ziyang@zte.com.cn>
2023-06-15 10:18:52 +0800

    amd

--- src/app/for-internal/demo/pc/select-group/infinite-scroll/demo.component.ts
+++ src/app/for-internal/demo/pc/select-group/infinite-scroll/demo.component.ts
@@ -1,20 +1,19 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Component } from "@angular/core";
-import { InfiniteScrollLocalPageableArray, InfiniteScrollPageableArray, TableData } from "jigsaw/public_api";
+import { HttpClientOptions, InfiniteScrollDirectPageableArray, InfiniteScrollLocalPageableArray, InfiniteScrollPageableArray, TableData } from "jigsaw/public_api";
 
 @Component({
     templateUrl: './demo.component.html',
     styleUrls: ['./demo.component.css']
 })
 export class SelectGroupInfiniteScrollDemoComponent {
-    public data: InfiniteScrollLocalPageableArray<any> | InfiniteScrollPageableArray;
+    public data: InfiniteScrollLocalPageableArray<any> | InfiniteScrollPageableArray | PluginInfiniteScrollDirectPageableArray;
     public value;
     public searchable = true;
     public disabled = false;
     public multipleSelect = false;
     public optionCount = 5;
-    public isLocal = true;
-    public dataType = [{ label: '本地', id: 1 }, { label: '服务器', id: 2 }];
+    public dataType = [{ label: '本地', id: 1 }, { label: '服务器', id: 2 }, { label: '自定义', id: 3 }];
     public selectedDataType = [{ label: '本地', id: 1 }];
 
     constructor(public http: HttpClient) {
@@ -26,7 +25,7 @@ export class SelectGroupInfiniteScrollDemoComponent {
     }
 
     public _$changeData() {
-        if (this.isLocal) {
+        if (this.selectedDataType[0].id == 1) {
             let array = [];
             for (let i = 1; i <= 100; i++) {
                 array.push({ name: "修改数据" + i });
@@ -34,21 +33,25 @@ export class SelectGroupInfiniteScrollDemoComponent {
             this.data.fromArray(array);
             return;
         }
-        this.data = new InfiniteScrollPageableArray(this.http, {
-            url: "mock-data/hr-list",
-            params: { aa: 11, bb: 22 },
-            method: 'post'
-        });
-        this.data.pagingInfo.pageSize = 20;
-        this.data.fromAjax();
-        this.data.dataReviser = (td) => {
-            return { data: TableData.toArray(td), paging : td.paging };
-        };
+
+        if (this.selectedDataType[0].id == 2) {
+            this.data = new InfiniteScrollPageableArray(this.http, {
+                url: "mock-data/hr-list",
+                params: { aa: 11, bb: 22 },
+                method: 'post'
+            });
+            this.data.pagingInfo.pageSize = 20;
+            this.data.fromAjax();
+            this.data.dataReviser = (td) => {
+                return { data: TableData.toArray(td), paging: td.paging };
+            };
+        }
     }
 
     public _$resetData() {
         this._$clearValue();
-        if (this.isLocal) {
+
+        if (this.selectedDataType[0].id == 1) {
             let array = [];
             for (let i = 1; i <= 200; i++) {
                 let gender = "其他分组";
@@ -66,28 +69,106 @@ export class SelectGroupInfiniteScrollDemoComponent {
             this.data.pagingInfo.pageSize = 15;
             return;
         }
-        this.data = new InfiniteScrollPageableArray(this.http, {
-            url: "mock-data/hr-list-full",
-            params: { aa: 11, bb: 22 },
-        });
-        this.data.pagingInfo.pageSize = 20;
-        this.data.fromAjax();
-        this.data.dataReviser = (td) => {
-            return { data: TableData.toArray(td), paging : td.paging };
-        };
+
+        if (this.selectedDataType[0].id == 2) {
+            this.data = new InfiniteScrollPageableArray(this.http, {
+                url: "mock-data/hr-list-full",
+                params: { aa: 11, bb: 22 },
+            });
+            this.data.pagingInfo.pageSize = 20;
+            this.data.fromAjax();
+            this.data.dataReviser = (td) => {
+                return { data: TableData.toArray(td), paging: td.paging };
+            };
+            return;
+        }
+
+        if (this.selectedDataType[0].id == 3) {
+            this.data = new PluginInfiniteScrollDirectPageableArray(this.http, {
+                url: "just-test",
+                method: 'get'
+            });
+            if (this.data instanceof PluginInfiniteScrollDirectPageableArray) {
+                this.data.paramGeneratorContext = this;
+                this.data.pagingInfo.pageSize = 20;
+                this.data.paramGenerator = () => ({
+                    test: 'some-test',
+                    paging: this.data.pagingInfo.valueOf(),
+                    filter: (this.data as PluginInfiniteScrollDirectPageableArray).filterInfo
+                });
+            }
+            this.data.fromAjax();
+            return;
+        }
     }
 
     public _$switchData() {
-        this.isLocal = !this.isLocal;
         this._$resetData();
     }
 
     public _$clearValue() {
         this.value = undefined;
     }
+
+    public _$getDisabledStatus() {
+        return this.selectedDataType[0].id == 3;
+    }
     // ====================================================================
     // ignore the following lines, they are not important to this demo
     // ====================================================================
     summary: string = '';
     description: string = '';
+}
+
+export class PluginInfiniteScrollDirectPageableArray extends InfiniteScrollDirectPageableArray {
+    paramGenerator: () => any;
+    bodyGenerator: () => any;
+    headersGenerator: () => any;
+    paramGeneratorContext: any;
+
+    protected _ajax(): void {
+        updateSourceRequestOptions(this.sourceRequestOptions, this.paramGenerator, this.bodyGenerator, this.headersGenerator, this.paramGeneratorContext);
+        super._ajax();
+    }
+}
+
+function updateSourceRequestOptions(sourceRequestOptions: HttpClientOptions, paramGenerator: () => any, bodyGenerator: () => any, headersGenerator: () => any, paramGeneratorContext: any): void {
+    let params, body, headers;
+    if (!!paramGenerator) {
+        try {
+            params = paramGenerator.apply(paramGeneratorContext);
+        } catch (e) {
+            params = {};
+            console.error("paramGenerator error! ", e);
+        }
+    }
+
+    if (!!params && Object.keys(params).length > 0){
+        sourceRequestOptions.params = params;
+    }
+
+    if (!!bodyGenerator) {
+        try {
+            body = bodyGenerator.apply(paramGeneratorContext);
+        } catch (e) {
+            body = {};
+            console.error("bodyGenerator error! ", e);
+        }
+    }
+
+    if (!!body && Object.keys(body).length > 0){
+        sourceRequestOptions.body = body;
+    }
+
+    if (!!headersGenerator) {
+        try {
+            headers = headersGenerator.apply(paramGeneratorContext);
+        } catch (e) {
+            headers = {};
+            console.error("headersGenerator error! ", e);
+        }
+    }
+    if (!!headers && Object.keys(headers).length > 0) {
+        sourceRequestOptions.headers = new HttpHeaders(headers);
+    }
 }
