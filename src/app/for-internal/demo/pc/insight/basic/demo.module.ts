6021155c6b4310752740fb7879fd3b112501a692
谭子扬10293829 <tan.ziyang@zte.com.cn>
2023-07-13 15:19:51 +0800

    amd

--- src/app/for-internal/demo/pc/insight/basic/demo.module.ts
+++ src/app/for-internal/demo/pc/insight/basic/demo.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { JigsawDemoDescriptionModule } from "app/for-internal/description/demo-description";
-import { JigsawHeaderModule } from "jigsaw/public_api";
+import { JigsawButtonModule, JigsawHeaderModule, JigsawInputModule } from "jigsaw/public_api";
 import { JigsawInsightBasicDemoComponent } from "./demo.component";
 import { PerfectScrollbarModule } from "ngx-perfect-scrollbar";
 
 @NgModule({
-    imports: [JigsawHeaderModule, CommonModule, JigsawDemoDescriptionModule, PerfectScrollbarModule],
+    imports: [JigsawHeaderModule, CommonModule, JigsawDemoDescriptionModule, PerfectScrollbarModule, JigsawInputModule, JigsawButtonModule],
     declarations: [JigsawInsightBasicDemoComponent],
     exports: [JigsawInsightBasicDemoComponent]
 })
-export class JigsawInsightBasicDemoModule {}
+export class JigsawInsightBasicDemoModule { }
