1ce33cd028ca99f450548e6d59ed65c667f08c58
imbnnn <111834476+imbnnn@users.noreply.github.com>
2023-08-10 20:04:09 +0800

    Update textarea.ts

--- src/jigsaw/pc-components/textarea/textarea.ts
+++ src/jigsaw/pc-components/textarea/textarea.ts
@@ -68,7 +68,7 @@ export class JigsawTextarea extends AbstractJigsawComponent implements IJigsawFo
      */
      @RequireMarkForCheck()
      @Input()
-     public showBorder: boolean = true;
+     public showBorder: boolean = false;
 
     private _resize: "both" | "horizontal" | "vertical" | "none" = "none";
     /**
