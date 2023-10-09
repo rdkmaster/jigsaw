e0b1d9ab386d25cc79c18e497fbcb216ab86ecdd
陈旭10045812 <chen.xu8@zte.com.cn>
2023-10-09 10:54:04 +0800

    ...

--- src/app/for-internal/demo/pc/table/no-data/demo.component.ts
+++ src/app/for-internal/demo/pc/table/no-data/demo.component.ts
@@ -15,13 +15,20 @@ export class TableNoDataDemoComponent {
             ["name", "position", "salary", "title1", "enroll-date", "office", "extn", "title2", "title3"],
             ["姓名", "职位", "薪资", "很长很长很长很长很长很长很长的用于测试的标题", "入职日期", "部门", "其他", "很长很长很长很长很长很长很长的用于测试的标题", "很长很长很长很长很长很长很长的用于测试的标题"]);
 
-        const field = this.generateStringArray('FIELD', 20);
-        const header = this.generateStringArray('HEADER', 20);
+        const field = this._generateStringArray('FIELD', 6);
+        const header = this._generateStringArray('HEADER', 6);
         this.columnDefines = [{target: field, width: 'byContent'}];
         this.tableData2 = new TableData([], field, header);
     }
 
-    private generateStringArray(label: string, n: number): string[] {
+    public moreColumns() {
+        const field = this._generateStringArray('FIELD', 26);
+        const header = this._generateStringArray('HEADER', 26);
+        this.columnDefines = [{target: field, width: 'byContent'}];
+        this.tableData2 = new TableData([], field, header);
+    }
+
+    private _generateStringArray(label: string, n: number): string[] {
         const result: string[] = [];
         for (let i = 0; i < n; i++) {
             result.push(`${label}${i}`);
