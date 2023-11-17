f9d6578a52d43ff3044cc5217e27f77114425d9b
10322371 <87427061+hanz6@users.noreply.github.com>
2023-11-17 16:45:24 +0800

    [新增] form-display组件新增数据更新的方法，关联了@4851

---
+++ src/app/for-internal/demo/pc/form-display/update/demo.component.ts
@@ -0,0 +1,52 @@
+import {Component, ViewChild} from "@angular/core";
+import {JigsawFormDisplayComponent, TableDataConfig} from "jigsaw/public_api";
+import {FormlyFieldConfig} from '@ngx-formly/core';
+
+@Component({
+    templateUrl: 'demo.component.html'
+})
+export class FormDisplayUpdateComponent {
+    @ViewChild('jigsawFormDisplayComponent')
+    jigsawFormDisplayComponent: JigsawFormDisplayComponent;
+
+    public get formData() {
+        return this.formComponentInstance.formio;
+    }
+
+    public formComponentInstance: { formio: FormlyFieldConfig[] } = {
+        formio: [
+            {
+                fieldGroup: [
+                    {type: 'auto-input', templateOptions: {label: "目的表字段", required: true, title: "edw_creat_date"}},
+                    {type: 'auto-input', templateOptions: {label: "类型", required: true, title: "string"}},
+                    {type: 'auto-input', templateOptions: {label: "字段长度", required: true, title: "33"}},
+                ]
+            },
+            {type: 'auto-input', templateOptions: {label: "字段描述", required: true, title: "很好"}},
+            {type: 'auto-input', templateOptions: {label: "业务主题", required: true, title: "AIGO"}},
+        ]
+    }
+
+    public styleOptions = {
+        trStyle: {'border-width': '1px'},
+        tdStyle: {'text-align': 'left', 'border-width': '1px', 'padding-left': '9px'},
+        columnWidths: [100, 200, 80]
+    }
+
+    public changeSource1() {
+        this.formComponentInstance.formio[0].fieldGroup = [];
+        this.formComponentInstance.formio.push({type: 'auto-input', templateOptions: {label: "字段33", title: "33"}});
+        this.jigsawFormDisplayComponent.update(<TableDataConfig[]>this.formComponentInstance.formio);
+        this.jigsawFormDisplayComponent.styleOptions = [
+            this.styleOptions,
+            {
+                trStyle: {'border-width': '2px'},
+                tdStyle: {'border-width': '2px', 'padding-left': '9px'},
+                columnWidths: [100, 150, 100]
+            }
+        ];
+    }
+
+    summary: string = "这个DEMO演示了已awade表单的root_fields做为数据源生成的form-display组件";
+    description: string = "";
+}
