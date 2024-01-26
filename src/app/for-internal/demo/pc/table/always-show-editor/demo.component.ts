cf92dc681d31188892b5fdeb84f395361698829b
谭子扬10293829 <tan.ziyang@zte.com.cn>
2024-01-26 13:52:09 +0800

    amd

---
+++ src/app/for-internal/demo/pc/table/always-show-editor/demo.component.ts
@@ -0,0 +1,213 @@
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import {
+    ColumnDefine,
+    TableCellSelectRenderer,
+    LocalPageableTableData
+} from "jigsaw/public_api";
+
+@Component({
+    templateUrl: './demo.component.html'
+})
+export class TableAlwaysShowEditorDemoComponent {
+    public pageable: LocalPageableTableData;
+
+    constructor(http: HttpClient) {
+        this.pageable = new LocalPageableTableData();
+        this.pageable.http = http;
+        this.pageable.pagingInfo.pageSize = 5;
+        this.pageable.fromObject({
+            data: [
+                [
+                    "空字符",
+                    "System Architect",
+                    "$320,00",
+                    "2011/04/25",
+                    "",
+                    "542"
+                ],
+                [
+                    "Garrett Winters1",
+                    "Accountant",
+                    "$170,7",
+                    "2011/07/25",
+                    "Tokyo",
+                    "8422"
+                ],
+                [
+                    "Tiger Nixon2",
+                    "System Architect",
+                    "$320,8000",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Garrett Winslters1",
+                    "Accountant",
+                    "$170,7",
+                    "2011/07/25",
+                    "",
+                    "8422"
+                ],
+                [
+                    "Tiger Nixon2",
+                    "System Architect",
+                    "$320,8000",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Garrett Winters1",
+                    "Accountant",
+                    "$170,7",
+                    "2011/07/25",
+                    "Tokyo",
+                    "8422"
+                ],
+                [
+                    "Tiger Nixon2",
+                    "System Architect",
+                    "$320,8000",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Garrett Winters1",
+                    "Accountant",
+                    "$170,7",
+                    "2011/07/25",
+                    "Tokyo",
+                    "8422"
+                ],
+                [
+                    "Tiger Nixon2",
+                    "System Architect",
+                    "$320,8000",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Garrett Wintsers2",
+                    "Accountant",
+                    "$170,50",
+                    "2011/07/25",
+                    "Tokyo",
+                    "8422"
+                ],
+                [
+                    "Tiger Nixon3",
+                    "System Architect",
+                    "$320,800",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Tiger Nixon3",
+                    "System Architect",
+                    "$3,800",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Tiger Nixon3",
+                    "System Architect",
+                    "$320,800",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Tiger Nixon1",
+                    "System Architect",
+                    "$320,80",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "542111"
+                ],
+                [
+                    "Garrett Winters1",
+                    "Accountant",
+                    "$170,750",
+                    "2011/07/25",
+                    "Tokyo",
+                    "84212"
+                ],
+                [
+                    "Tiger Nixon2",
+                    "System Architect",
+                    "$320,800",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Tigesr Nixon1",
+                    "System Architect",
+                    "$320,800",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ],
+                [
+                    "Garrett Winters1",
+                    "Accountant",
+                    "$170,750",
+                    "2011/07/25",
+                    "Tokyo",
+                    "8422"
+                ],
+                [
+                    "Tigers Nixon2",
+                    "System Architect",
+                    "$320,800",
+                    "2011/04/25",
+                    "Edinburgh",
+                    "5421"
+                ]
+            ],
+            field: ["name", "position", "salary", "enroll-date", "office", "extn"],
+            header: ["姓名", "职位", "薪资", "入职日期", "部门", "其他"]
+        })
+    }
+
+    columnDefines: ColumnDefine[] = [
+        {
+            target: "office",
+            cell: {
+                editorRenderer: TableCellSelectRenderer,
+                editorRendererInitData: {
+                    initData: (td, row, col) => {
+                        return [{ label: 'Edinburgh' }, { label: 'Tokyo' }, { label: '' }, { label: 'London' }];
+                    }
+                },
+                editable: true,
+                alwaysShowEditor: true
+            }
+        },
+    ];
+
+    onSearch(reg) {
+        console.log(reg);
+        // 这里需要特别注意，filter函数的执行是在服务端，而非在浏览器！
+        // 这里context变量是filter的执行上下文（即filter函数里的this所指向的对象），它将会一起传输给服务端，
+        // 因此这里需要注意控制context的值里只包含有用的数据，以加快前后端通信速度
+        const filter = function (item) {
+            return item[0].match(new RegExp(this.reg, 'g'));
+        };
+        const context = { reg };
+
+        this.pageable.filter(filter, context);
+    }
+
+    // ====================================================================
+    // ignore the following lines, they are not important to this demo
+    // ====================================================================
+    summary: string = '';
+    description: string = '';
+}
