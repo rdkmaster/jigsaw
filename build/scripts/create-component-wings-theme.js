65fcd900a3040c5767ac772f044f8ecec7bc2b59
Samari Tan <41236821+Gloryoftan@users.noreply.github.com>
2024-02-18 16:40:03 +0800

    [新增] auto-display组件默认支持深浅色卡片样式，关联了@4379

--- build/scripts/create-component-wings-theme.js
+++ build/scripts/create-component-wings-theme.js
@@ -13,7 +13,7 @@ const invalidComponents = [];
 const ignoredComponents = [
     'JigsawBox', 'JigsawBreadcrumbItem', 'JigsawTileOption', 'JigsawRadioOption', 'JigsawRoot', 'JigsawEditableBox',
     'JigsawTabPane', 'JigsawViewport', 'JigsawBlock', 'JigsawScrollbar', 'JigsawUploadFileInfoListFallback',
-    'JigsawCollapsePane', 'JigsawPieChartIcon', 'JigsawDonutChartIcon', 'JigsawLineChartIcon', 'JigsawBarChartIcon'
+    'JigsawCollapsePane', 'JigsawPieChartIcon', 'JigsawDonutChartIcon', 'JigsawLineChartIcon', 'JigsawBarChartIcon', 'JigsawAutoDisplay'
 ];
 let wingsThemeIds = [];
 tsFiles.forEach(file => {
