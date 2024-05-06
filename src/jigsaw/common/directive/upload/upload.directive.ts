97fbc189dd1a3631bfd2b708970a2859979a869e
孙丙奇6092003887 <sun.bingqi@zte.com.cn>
2024-05-06 14:55:08 +0800

    回复另起一个pr推送事件不触发的修改

--- src/jigsaw/common/directive/upload/upload.directive.ts
+++ src/jigsaw/common/directive/upload/upload.directive.ts
@@ -401,8 +401,8 @@ export class JigsawUploadDirective extends JigsawUploadBase implements IUploader
                 this.files.forEach(item => update(item));
             } else {
                 update(fileInfo);
+                this._afterCurFileUploaded(fileInfo);
             }
-            this._afterCurFileUploaded(fileInfo);
         }, (e) => {
             const update = (fileInfo: UploadFileInfo) => {
                 fileInfo.state = 'error';
@@ -412,9 +412,9 @@ export class JigsawUploadDirective extends JigsawUploadBase implements IUploader
             const message = this._translateService.instant(`upload.${e.statusText}`) || e.statusText;
             if (this.batchMode) {
                 this.files.filter(file => file.url == "").forEach((item: any) => update(item));
-            } else {
-                update(fileInfo);
+                return;
             }
+            update(fileInfo);
             this._afterCurFileUploaded(fileInfo);
         });
     }
@@ -452,13 +452,11 @@ export class JigsawUploadDirective extends JigsawUploadBase implements IUploader
             this.complete.emit(this.files);
             return;
         }
-        if (!this.batchMode) {
-            const waitingFile = this.files.find(f => f.state == 'pause');
-            if (waitingFile) {
-                this._sequenceUpload(waitingFile)
-            }
-        }
-        if (this._isAllFilesUploaded()) {
+
+        const waitingFile = this.files.find(f => f.state == 'pause');
+        if (waitingFile) {
+            this._sequenceUpload(waitingFile)
+        } else if (this._isAllFilesUploaded()) {
             this.complete.emit(this.files);
         }
         this._cdr.markForCheck();
